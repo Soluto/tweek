1d02a8dbae21f314e4f7559e15121ceab9a54d5b
yshay <yshayy@gmail.com>
2016-02-28 18:03:02 +0200

    using uint64 instead of bigint

--- Engine.Rules.ValueDistribution/ValueDistributor.fs
+++ Engine.Rules.ValueDistribution/ValueDistributor.fs
@@ -8,13 +8,13 @@ open System
 module ValueDistribution = 
 
     let private uniformCalc (hash) (choices:string[]) = 
-        let index  = (hash % (bigint choices.Length)) |> int
+        let index  = (hash % (choices.Length |> uint64)) |> int
         choices.[index]
 
     let scanWithFirstItem fold seq = seq |> Seq.skip 1 |> Seq.scan fold (Seq.head seq)
 
     let private weightedCalc (hash) (weighted:(string*int)[]) = 
-        let selectedItem = hash % (bigint (weighted |> Seq.sumBy snd )) |> int
+        let selectedItem = hash % (weighted |> Seq.sumBy snd |> uint64) |> int
 
         weighted
         |> scanWithFirstItem (fun (p,a) (x,n) -> (x, a+n))
@@ -26,7 +26,7 @@ module ValueDistribution =
 
     let private calc (json:JsonValue) (input: string) = 
         let sha1 = new SHA1CryptoServiceProvider(); 
-        let hash = bigint (sha1.ComputeHash (Encoding.UTF8.GetBytes input)) |> abs
+        let hash = BitConverter.ToUInt64(((sha1.ComputeHash (Encoding.UTF8.GetBytes input)).[0..15]), 0)
         match json.GetProperty("type").AsString()  with
         | "uniform" ->  uniformCalc hash (json.GetProperty("args").AsArray() |> Array.map (fun x-> x.AsString()))
         | "weighted" -> weightedCalc hash ( json.GetProperty("args").Properties() |> Array.map (fun (k,v)-> (k, v.AsInteger())) )
