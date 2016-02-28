50ea3d709e73bb3351a8387a56275c9d729695c9
yshay <yshayy@gmail.com>
2016-02-28 10:11:10 +0200

    add tests and fixes

--- Engine.Rules.ValueDistribution.Tests/ValueDistributor.Tests.fs
+++ Engine.Rules.ValueDistribution.Tests/ValueDistributor.Tests.fs
@@ -11,11 +11,28 @@ open Swensen.Unquote
 open FSharp.Data;
 open Microsoft.FSharp.Reflection;
 open Engine.Rules.ValueDistribution;
+open Newtonsoft.Json
+open FsCheck
+
+let generatedCalculatedScheme weights = weights |> Array.mapi (fun a b -> (a,b) )
+                                                |> dict 
+                                                |> JsonConvert.SerializeObject 
+                                                |> sprintf """{"type": "weighted","args": %s }""" 
+                                                |> ValueDistribution.CalculateValue
+
+let assertCalculated (weights:float[]) (numberOfUsers:int) (samplingError:int) (calcFunction:obj[]->string)  = 
+            let sumWeights = weights |> Array.sum
+            [|1..numberOfUsers|] 
+                        |> Array.map (fun x-> calcFunction([|x|]))
+                        |> Seq.countBy id
+                        |> Seq.sortBy fst
+                        |> Seq.zip weights
+                        |> Seq.map (fun (expectedWeight, (_,actualWeight) )->  actualWeight/numberOfUsers |> should (equalWithin samplingError) (expectedWeight/sumWeights) )
 
 [<Fact>]
 let ``Use uniform distrubtion with single value``() =
-    let calculator = ValueDistribution.CalculateValue """{"type": "uniform", "args": ["5"] }""" 
-    calculator [|"userName", 5|]  |> should equal "5";
+    let calculator = ValueDistribution.CalculateValue """{"type": "uniform", "args": ["abc"] }""" 
+    calculator [|"userName", 5|]  |> should equal "abc";
     
 [<Fact>]
 let ``Use weighted distrubtion with single value``() =
@@ -24,6 +41,36 @@ let ``Use weighted distrubtion with single value``() =
 
 [<Property>]
 let ``Use coin distrubtion should equal weighted``(userId:int) =
-    let calculatorWeighted = ValueDistribution.CalculateValue """{"type": "weighted","args": {"true": 60, "false": 40} }""" 
-    let calculatorCoin = ValueDistribution.CalculateValue """{"type": "coin","args": 0.6 }""" 
+    let calculatorWeighted = ValueDistribution.CalculateValue """{"type": "weighted","args": {"true": 40, "false": 60} }""" 
+    let calculatorCoin = ValueDistribution.CalculateValue """{"type": "coin","args": 0.4 }""" 
     calculatorWeighted [|userId|]  |> should equal (calculatorCoin [|userId|]);
+
+[<Fact>]
+let ``run single tests and verify similar values``()=
+    let weights = [|1.0;5.0;6.0|]
+    let calculatorWeighted = generatedCalculatedScheme weights
+    let totalUsers = 10
+    let samplingError = 1
+    assertCalculated weights totalUsers samplingError calculatorWeighted
+
+
+let rand = Gen.sequence ([Gen.elements([1..10]);
+                          Gen.elements([0..10]);
+                          Gen.elements([1..10])
+                          ]) |> Arb.fromGen
+
+(*    
+[<Property>]
+let ``run many tests and verify similar values``(ratio:int)=
+    let percent = if ratio = 0 then ratio else (1.0 / (ratio|>float) ) * 100.0 |> abs |> int
+    let calculatorWeighted = ValueDistribution.CalculateValue (sprintf """{"type": "weighted","args": {"true": %i, "false": %i} }""" percent (100-percent))
+    [|1..1000|] 
+            |> Array.map (fun x -> calculatorWeighted [|x|]) 
+            |> Seq.countBy (fun x -> x) 
+            |> dict
+            |> fun x -> (match x.Count with
+                        |1 -> if x.ContainsKey("true") then 100 else 0
+                        |2 -> (x.["true"] |>float) / ((x.["true"] + x.["false"]) |>float )  * 100.0 |> int
+                        )
+            |> should (equalWithin 6) percent
+ *)  
--- Engine.Rules.ValueDistribution/ValueDistributor.fs
+++ Engine.Rules.ValueDistribution/ValueDistributor.fs
@@ -14,26 +14,26 @@ module ValueDistribution =
     let scanWithFirstItem fold seq = seq |> Seq.skip 1 |> Seq.scan fold (Seq.head seq)
 
     let private weightedCalc (hash) (weighted:(string*int)[]) = 
-        let selectedItem = hash % (bigint (weighted |> Seq.fold (fun acc (_, w)->  acc + w) 0) ) |> int
+        let selectedItem = hash % (bigint (weighted |> Seq.sumBy snd )) |> int
+
         weighted
         |> scanWithFirstItem (fun (p,a) (x,n) -> (x, a+n))
-        |> Seq.skipWhile (fun (_, range)-> printf "%i" selectedItem ;selectedItem > range )  
-        |> Seq.map (fun (x, _) -> x )
+        |> Seq.skipWhile (fun (_, range)-> selectedItem > range )  
+        |> Seq.map fst
         |> Seq.head
 
     let floatToWeighted = (*) 100.0 >> int
 
     let private calc (json:JsonValue) (input: string) = 
         let sha1 = new SHA1CryptoServiceProvider(); 
-        let hash = bigint (sha1.ComputeHash (Encoding.UTF8.GetBytes input)).[0..15] |> abs
+        let hash = bigint (sha1.ComputeHash (Encoding.UTF8.GetBytes input)) |> abs
         match json.GetProperty("type").AsString()  with
-        | "weighted" -> weightedCalc hash ( json.GetProperty("args").Properties() |> Array.map (fun (k,v)-> (k, v.AsInteger())) )
         | "uniform" ->  uniformCalc hash (json.GetProperty("args").AsArray() |> Array.map (fun x-> x.AsString()))
+        | "weighted" -> weightedCalc hash ( json.GetProperty("args").Properties() |> Array.map (fun (k,v)-> (k, v.AsInteger())) )
         | "coin" -> weightedCalc hash (
-                                                 json.GetProperty("args").AsFloat() |>
-                                                 (fun(x)-> [|("true",x |> floatToWeighted);("false", (1.0 - x)|>floatToWeighted)|])
-                                                 ) 
+                                    json.GetProperty("args").AsFloat() |>
+                                    (fun(x)-> [|("true",x |> floatToWeighted);("false", (1.0 - x)|>floatToWeighted)|])) 
         | s -> raise (Exception("expected operator, found:"+s))
         
-    let CalculateValue (schema:string) ([<ParamArray>] hash : Object[]) = 
-        calc (schema |> JsonValue.Parse) (hash |> Seq.map string |>  String.concat ".");
+    let CalculateValue (schema:string) ([<ParamArray>] units : Object[]) = 
+        calc (schema |> JsonValue.Parse) (units |> Seq.map string |>  String.concat ".");
