4ea12cf4b18999d154c12696ce5f23eb42a6574f
yshay <yshayy@gmail.com>
2016-03-06 11:29:48 +0200

    add first test

--- Engine.Tests/EngineTests.cs
+++ Engine.Tests/EngineTests.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Engine.Context;
+using Engine.Core.DataTypes;
+using Engine.Core.Rules;
+using Engine.Rules;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Engine.Tests
@@ -7,9 +13,19 @@ namespace Engine.Tests
     public class EngineTests
     {
         [TestMethod]
-        public void Calculate()
+        public async Task Calculate()
         {
-            
+            var context = ContextByIdentityCreation.Convert(new Dictionary<string,string>(){{"AgentVersion", "10"}});
+            var engine = new Tweek(async (identity)=> context, path => new HashSet<ConfigurationPath>(){"abc/somepath"}, path => new List<IRule>(){new SingleVariantRule()
+            {
+                Matcher = x=> true,
+                Value = new ConfigurationValue("SomeValue"),
+            }});
+
+            var val = await engine.Calculate("abc", new HashSet<Identity>{new Identity("device", "1")});
+            Assert.AreEqual(val["somepath"].Value, "SomeValue");
+
+
         }
     }
 }
