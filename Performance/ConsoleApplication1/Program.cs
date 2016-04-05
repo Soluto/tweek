80e48286fa868846bb9a7f98a6d475ee9b98a10f
yshay <yshayy@gmail.com>
2016-04-06 00:40:37 +0300

    add simple benchmark project

---
+++ Performance/ConsoleApplication1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cassandra;
+using Engine;
+using Engine.Core.Context;
+using Engine.DataTypes;
+using Engine.Drivers.Cassandra;
+using Engine.Drivers.Rules.Git;
+using static LanguageExt.Prelude;
+
+namespace SimpleBenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var cluster = Cluster.Builder()
+                .WithQueryOptions(new QueryOptions().SetConsistencyLevel(ConsistencyLevel.All))
+                .AddContactPoints("dc0vm1tqwdso6zqj26c.eastus.cloudapp.azure.com",
+                    "dc0vm0tqwdso6zqj26c.eastus.cloudapp.azure.com")
+                .Build();
+
+            var session = cluster.Connect("tweek");
+
+            var contextDriver = new CassandraDriver(session);
+
+            var gitDriver = new GitDriver(
+                Path.Combine(Environment.CurrentDirectory, "tweek-rules" + Guid.NewGuid()),
+                new RemoteRepoSettings()
+                {
+                    url = "http://tweek-gogs-1.816587ba.cont.dockerapp.io/tweek/tweek-rules.git",
+                    Email = "tweek@soluto.com",
+                    UserName = "tweek",
+                    Password = "po09!@QW"
+                });
+
+            ITweek tweek = Task.Run(async () => await Engine.Tweek.Create(
+                contextDriver, gitDriver)).Result;
+            var query = ConfigurationPath.New("_");
+            
+            var dic = new Dictionary<string, string>()
+            {
+
+            };
+            GetLoadedContextByIdentityType ext_context =
+                (identityType) =>
+                    (key) => (identityType == "device" && key == "@CreationDate") ? Some("06/06/16") : None;
+            Console.WriteLine("Start running");
+            Console.WriteLine(Enumerable.Range(0, 1000)
+                .Select(_ =>
+                {
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    var data = tweek.Calculate(query, new HashSet<Identity>() {}, ext_context).Result;
+                    sw.Stop();
+                    return sw.ElapsedMilliseconds;
+                }).Average());
+            Console.ReadLine();
+
+        }
+    }
+}
