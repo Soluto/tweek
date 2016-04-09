182e9792e493911158f677a6503272ad468d2580
yshayy <yshayy@gmail.com>
2016-04-09 22:10:28 +0300

    add support for couchbase. made it default driver for api service.

---
+++ Drivers/Tweek.Drivers.Couchbase/CouchBaseDriver.cs
@@ -0,0 +1,93 @@
+﻿using Engine.Drivers.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.DataTypes;
+using Couchbase;
+using Couchbase.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+
+namespace Tweek.Drivers.CouchbaseDriver
+{
+    public class CouchBaseDriver : IContextDriver, IDisposable
+    {
+        readonly Cluster _cluster;
+        readonly string _bucketName;
+        private IBucket _bucket;
+
+        public CouchBaseDriver(Cluster cluster, string bucketName)
+        {
+            _bucketName = bucketName;
+            _cluster = cluster;
+
+            _bucket = null;
+        }
+
+        public string GetKey(Identity identity) => "identity_" + identity.Type + "_" + identity.Id;
+
+        private IBucket GetOrOpenBucket()
+        {
+            if (_bucket == null || !_cluster.IsOpen(_bucketName))
+            {
+                _bucket = _cluster.OpenBucket(_bucketName);
+            }
+            return _bucket;
+        }
+
+        public async Task AppendContext(Identity identity, Dictionary<string, string> context)
+        {
+            var key = GetKey(identity);
+            var bucket = GetOrOpenBucket();
+            
+            if (!await bucket.ExistsAsync(key))
+            {
+                var contextWithCreationDate  = context.ContainsKey("@CreationDate") ? context : context
+                    .Concat(new[] { new KeyValuePair<string, string>("@CreationDate", DateTimeOffset.UtcNow.ToString()) })
+                    .ToDictionary(x => x.Key, x => x.Value);
+                
+                var result = await bucket.UpsertAsync(new Document<Dictionary<string,string>>
+                    {
+                        
+                        Id = key,
+                        Content = contextWithCreationDate
+                });
+            }
+            else
+            {
+                    await context.Aggregate(
+                    bucket.MutateIn<dynamic>(key),
+                    (doc, item) => doc.Upsert(item.Key, item.Value)).ExecuteAsync();
+            }
+        }
+
+        public async Task<Dictionary<string, string>> GetContext(Identity identity)
+        {
+            var key = GetKey(identity);
+            using (var bucket = _cluster.OpenBucket(_bucketName))
+            {
+                var document = await bucket.GetDocumentAsync<Dictionary<string,string>>(key);
+                if (document.Status == Couchbase.IO.ResponseStatus.KeyNotFound)
+                {
+                    return new Dictionary<string, string>();
+                }
+                if (!document.Success)
+                    throw (document.Exception ?? new Exception(document.Message));
+                return document.Content;
+            }
+        }
+
+        public async Task RemoveIdentityContext(Identity identity)
+        {
+            await GetOrOpenBucket().RemoveAsync(GetKey(identity));
+        }
+
+        public void Dispose()
+        {
+            if (_bucket !=null) _cluster.CloseBucket(_bucket);
+        }
+    }
+}
---
+++ Engine.Tests/TestDrivers/CouchbaseTestDriver.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.DataTypes;
+using Engine.Drivers.Context;
+using Engine.Drivers.Rules;
+using Couchbase;
+using Tweek.Drivers.CouchbaseDriver;
+using Engine.Drivers.Rules.Git;
+using System.IO;
+using Couchbase.Core;
+
+namespace Engine.Tests.TestDrivers
+{
+    class CouchbaseTestDriver : ITestDriver
+    {
+        readonly Cluster _cluster;
+        readonly CouchBaseDriver _driver;
+        
+        public IContextDriver Context => _driver;
+        public Func<Task> cleanup = async ()=> {};
+
+        public CouchbaseTestDriver(Cluster cluster, string bucket)
+        {
+            _cluster = cluster;
+            _driver = new CouchBaseDriver(cluster, bucket);
+        }
+
+        async Task InsertRuleData(GitDriver driver, Dictionary<string, RuleDefinition> rules)
+        {
+            await
+                Task.WhenAll(
+                    rules.Select(
+                        x => driver.CommitRuleset(x.Key, x.Value, "tweekintegrationtests", "tweek@soluto.com", DateTimeOffset.UtcNow)));
+        }
+
+
+        async Task InsertContextRows(Dictionary<Identity, Dictionary<string, string>> contexts)
+        {
+            cleanup = () => Task.WhenAll(contexts.Select(x => x.Key).Select(_driver.RemoveIdentityContext));
+
+            await Task.WhenAll(
+                contexts.Map(x => _driver.AppendContext(x.Key, x.Value)
+                ));
+        }
+
+       
+        async Task Flush()
+        {
+            await cleanup();
+            _driver.Dispose();
+        }
+
+        public TestScope SetTestEnviornment(Dictionary<Identity, Dictionary<string, string>> contexts, string[] keys, Dictionary<string, RuleDefinition> rules)
+        {
+            var gitDriver = new GitDriver(Path.Combine(Environment.CurrentDirectory, "tweek-rules-tests" + Guid.NewGuid()));
+
+            return new TestScope(rules: gitDriver, context: Context, 
+                init: () => Task.WhenAll(InsertContextRows(contexts), InsertRuleData(gitDriver, rules)),
+                dispose: Flush);
+        }
+    }
+}
