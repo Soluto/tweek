6223c6d1e9936d70c779b6e34a1c8b8bd379b31c
yshay <yshayy@gmail.com>
2016-05-05 13:46:39 +0300

    add delete mutation

--- BackOffice/modules/utils/mutator.js
+++ BackOffice/modules/utils/mutator.js
@@ -14,7 +14,7 @@ export default class Mutator{
             return R.reduce((acc,x)=>acc[x], this.sourceTree, innerPath)[key];
         };
         
-    updateValue = (newValue) =>{
+    updateValue = newValue =>{
             console.log(`updating value:${this.path} to ${newValue}`);
             var clonedTree = R.clone(this._sourceTree);
             var [innerPath, [key]] = R.splitAt(-1,this.path);
@@ -23,7 +23,7 @@ export default class Mutator{
             this._callback(clonedTree);
             return new Mutator(clonedTree, this._callback, this.path);
         }
-     updateKey = (newKey) =>{
+     updateKey = newKey =>{
             console.log(`updating key:${this.path} to ${newKey}`);
             var [innerPath, [container, key]] = R.splitAt(-2,this.path);
             if (newKey === key) return this;
@@ -32,5 +32,14 @@ export default class Mutator{
             root[container] = R.fromPairs(R.toPairs(root[container]).map(([k,v])=>[k === key ? newKey : k, v]));
             this._callback(clonedTree);
             return new Mutator(clonedTree, this._callback, [...innerPath, container, newKey]);
+        }
+     delete =  () =>{
+         console.log(`deleting key:${this.path}`);
+         var [innerPath, [key]] = R.splitAt(-1,this.path);
+         var clonedTree = R.clone(this._sourceTree);
+         var container = R.reduce((acc,x)=>acc[x], clonedTree, innerPath);
+         delete container[key];
+         this._callback(clonedTree);
+         return new Mutator(clonedTree, this._callback, innerPath);
         }
 }
