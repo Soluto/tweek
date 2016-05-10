8531960a05769b865cfcf89fdc2b0ca081a1a857
yshay <yshayy@gmail.com>
2016-05-10 16:31:03 +0300

    fix autosuggest.

--- BackOffice/modules/pages/keys/components/JPadEditor/JPadEditor.js
+++ BackOffice/modules/pages/keys/components/JPadEditor/JPadEditor.js
@@ -6,11 +6,12 @@ import Paper from 'material-ui/Paper';
 import {List, ListItem} from 'material-ui/List';
 
 import {Editor as EditorStyle} from "./JPadEditor.css";
+let isBrowser = typeof(window) === "object";
 
 export default ({source, mutate})=>{
     if (!source) return (<div/>);
     var rules = JSON.parse(source);
-    return (<Paper className={EditorStyle}>
+    return isBrowser ? (<Paper className={EditorStyle}>
         {rules.map( (rule, i)=>(<ListItem disabled={true} key={rule.Id}><Rule mutate={mutate.in(i)} rule={rule} /></ListItem>))}
-    </Paper>)
+    </Paper>) : (<div>Loading rule...</div>);
 }
