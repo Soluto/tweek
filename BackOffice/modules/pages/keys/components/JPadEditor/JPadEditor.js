2fca4a6b18aecfb7dbefc0d98080c3e0fab2cf89
yshay <yshayy@gmail.com>
2016-07-13 18:32:41 +0300

    add ability to add cases

--- BackOffice/modules/pages/keys/components/JPadEditor/JPadEditor.js
+++ BackOffice/modules/pages/keys/components/JPadEditor/JPadEditor.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import Rule from './Rule';
 import { Editor as EditorStyle } from './JPadEditor.css';
+import Chance from 'chance';
 const isBrowser = typeof(window) === 'object';
+const chance = new Chance();
 
-export default ({ source, mutate }) => {
-  if (!source) return (<div/>);
-  const rules = JSON.parse(source);
+export default ({ cases, mutate }) => {
+  if (!cases) return (<div/>);
   return isBrowser ? (<div className={EditorStyle}>
-        {rules.map((rule, i) => (<div style={{ position: 'relative' }} disabled key={rule.Id}>
+        <button onClick={() =>
+            mutate.prepend({ Id: chance.guid(), Matcher: {}, Value: '', Type: 'SingleVariant' })
+        } >Add Case</button>
+        {cases.map((rule, i) => (<div style={{ position: 'relative' }} disabled key={rule.Id}>
             <div style={{ position: 'absolute',
                         top: 30,
                         right: 20 }} >
             {i > 0 ? <button onClick={() => mutate.replaceKeys(i, i - 1)}>&#8593;</button> : null}
-            {i < rules.length - 1 ? <button onClick={() => mutate.replaceKeys(i, i + 1)}>&#8595;</button> : null}
+            {i < cases.length - 1 ? <button onClick={() => mutate.replaceKeys(i, i + 1)}>&#8595;</button> : null}
             <button onClick={() => mutate.in(i).delete()}>X</button>
             </div>
-            <Rule mutate={mutate.in(i)} rule={rule} /></div>))}
-    </div>) : (<div>Loading rule...</div>);
+            <Rule key={rule.Id} mutate={mutate.in(i)} rule={rule} /></div>))}
+    </div>
+    ) : (<div>Loading rule...</div>);
 };
