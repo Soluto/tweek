a410e1889eb44e55a9c7f57e6cd14c7ed57d0615
yshay <yshayy@gmail.com>
2016-07-13 16:27:49 +0300

    extract slider, refactor ruleValue a bit

--- BackOffice/modules/pages/keys/components/JPadEditor/RuleValue.js
+++ BackOffice/modules/pages/keys/components/JPadEditor/RuleValue.js
@@ -1,71 +1,8 @@
 import React from 'react';
-import R from 'ramda';
-import Chance from 'chance';
-import ToolTip from 'react-portal-tooltip';
-import { withState, renderNothing } from 'recompose';
-import Draggable, { DraggableCore } from 'react-draggable';
-import Mutator from '../../../../utils/Mutator';
+import CustomSlider from '../../../../components/common/CustomSlider';
 
-const LegendStyle = {
-  Bar: { border: '2px solid #969696', padding: 6 },
-  Item: {},
-  Icon: (color) => ({ display: 'inline-block', backgroundColor: color, width: 24, height: 24 }),
-  Label: { display: 'inline-block' },
-};
-
-const CustomSlider = withState('hoverItem', 'setHoverItem', -1)(({ variants, hoverItem, setHoverItem, onUpdate, displayLegend = true }) => {
-  const chance = new Chance();
-  const colors = ['green', 'red', 'blue', 'yellow', 'black', 'grey'];
-  const items = R.zip(colors, R.toPairs(variants)).map(([color, [value, weight]]) => ({
-    value, weight, color,
-  }));
-  const tooltipHandlers = (index) => ({
-    onMouseEnter: () => setHoverItem(index),
-    omMouseMove: () => setHoverItem(index),
-  });
-  const tipElementPrefix = `t${chance.guid()}`;
-  const mutator = Mutator.stateless(() => variants, onUpdate);
-  return (<div onMouseLeave={() => setHoverItem(-1)} style={{ display: 'flex' }}>
-    {displayLegend ?
-    <div style={LegendStyle.Bar}>
-    {items.map(({ color, value, weight, tipId = `t${chance.guid()}` }, i) =>
-      <div {...tooltipHandlers(i)} style={LegendStyle.Item}>
-        <button onClick={() => mutator.apply(m => {
-          const itemToUpdate = items[i === 0 ? 1 : i - 1];
-          return m.in(value).delete().in(itemToUpdate.value).updateValue(itemToUpdate.weight + weight);
-        })}>-</button>
-        <div style={LegendStyle.Icon(color)} />
-        <input style={{ ...LegendStyle.Label, width: 150 }} onChange={e => mutator.in(value).updateKey(e.target.value)} value={value} />
-        <input style={{ ...LegendStyle.Label, width: 70 }} onChange={e => mutator.in(value).updateValue(parseFloat(e.target.value) || weight) } value={weight} />
-      </div>)}
-      <button onClick={() => mutator.insert(`variant #${items.length + 1}`, 0) }>+</button>
-    </div> : null}
-    <div style={{ alignSelf: 'center', padding: 10, display: 'flex' }}>
-      {items.map(({ color, weight, value }, i) =>
-      (<div style={{ display: 'flex', alignItems: 'center' }} >
-          <div {...tooltipHandlers(i)} id={`${tipElementPrefix}${i}`} style={{ height: 30, backgroundColor: color, width: parseFloat(weight) * 2 }} />
-          {i < (items.length - 1) ?
-          <DraggableCore onDrag={(_, data) =>
-            {
-            if (items[i + 1].weight - (data.deltaX * 0.5) < 0) return;
-            if (items[i].weight + (data.deltaX * 0.5) < 0) return;
-            mutator.apply(m => m.in(items[i + 1].value).updateValue(items[i + 1].weight - data.deltaX * 0.5)
-            .up()
-            .in(value).updateValue(weight + data.deltaX * 0.5));
-          }
-        } axis="x"
-          >
-          <div  {...tooltipHandlers(i)} style={{ backgroundColor: 'black', cursor: 'col-resize', width: 5, height: 40 }} />
-          </DraggableCore>
-          : null}
-        </div>
-      ))}
-    <ToolTip active={hoverItem !== -1} position="top" parent={`#${tipElementPrefix}${hoverItem}`}>
-            <div style={{ padding: 10 }}>{items[hoverItem] && (`${items[hoverItem].value} : ${items[hoverItem].weight}`)}</div>
-    </ToolTip>
-    </div>
-  </div>);
-});
+function replaceNaN(fallbackValue) {return isNaN(this) ? fallbackValue : this;}
+const parseNumericInput = (inputValue) => inputValue === '' ? 0 : parseInt(inputValue);
 
 let SingleVariantValue = ({ value, mutate }) => (
     (<div>
@@ -93,33 +30,37 @@ let SingleVariantValue = ({ value, mutate }) => (
     </div>)
 );
 
-let MultiVariantValue = ({ valueDistrubtion: { type, args }, mutate }) => {
+const WeightedValues = ({ onUpdate, variants }) =>
+  (<CustomSlider data={variants} onUpdate={onUpdate} />);
+
+const BernoulliTrial = ({ onUpdate, ratio }) => (
+  <div>
+  <div>Open to <input type="text" style={{ width: 40 }} value={ratio * 100} onChange={e =>
+      onUpdate((parseNumericInput(e.target.value) * 0.01)::replaceNaN(ratio))
+    } />%</div>
+    <CustomSlider displayLegend={false}
+      data={{ true: 1000 * ratio / 10, false: 100 - (1000 * ratio / 10) }}
+      onUpdate={x => onUpdate(x.true / 100) }
+    />
+  </div>
+);
+
+const MultiVariantValue = ({ valueDistrubtion: { type, args }, mutate }) => {
   if (type === 'weighted')
-    return (<div>
-        {
-            <CustomSlider variants={args} onUpdate={variants => {
-              if (Object.keys(variants).length === 1) {
-                const newValue = Object.keys(variants)[0];
-                mutate.apply(m => m.up()
+    return (<WeightedValues variants={args} onUpdate={variants => {
+      if (Object.keys(variants).length === 1) {
+        const newValue = Object.keys(variants)[0];
+        mutate.apply(m => m.up()
                                     .in('Value').updateValue(newValue).up()
                                     .in('Type').updateValue('SingleVariant').up()
                                     .in('ValueDistribution').delete());
-              } else {
-                mutate.in('args').updateValue(variants);
-              }
-            }} />
-            // R.toPairs(args).map(([value, weight]) => (<div>{`${value}:${weight}`}</div>))
-        }
-        </div>);
+      } else {
+        mutate.in('args').updateValue(variants);
+      }
+    }} />);
   if (type === 'bernoulliTrial') {
     return (<div>
-    <div>Open to <input type="text" value={args * 100} onChange={e =>
-      mutate.in('args').updateValue((parseInt(e.target.value) * 0.01) || args)
-    } />%</div>
-    <CustomSlider displayLegend={false}
-      variants={{ true: 1000 * parseFloat(args) / 10, false: 100 - (1000 * parseFloat(args) / 10) }}
-      onUpdate={x => mutate.in('args').updateValue(x.true / 100)}
-    />
+    <BernoulliTrial onUpdate={mutate.in('args').updateValue} ratio={args} />
     {(args === 1) ?
     <button onClick={() => mutate.apply(m =>
       m.up()
