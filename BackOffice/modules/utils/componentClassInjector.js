fb340d04f7db73ab93cba6fc828f584f0e5dafc9
Gil Mualem <gil@soluto.local>
2016-07-20 11:34:36 +0300

    - some ui changes - add tags/display name/description

--- BackOffice/modules/utils/componentClassInjector.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const componentClassInjector = (Comp) => ({ className, ...props }) =>
-(<div className={className}><Comp {...props} /></div>);
-
-export default componentClassInjector;
