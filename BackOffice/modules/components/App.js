a31dae3b6cadc305ca9c3a461734fde898e678ba
Gil Mualem <gil@soluto.local>
2016-08-02 10:30:32 +0300

    - improve application ui

--- BackOffice/modules/components/App.js
+++ BackOffice/modules/components/App.js
@@ -3,6 +3,9 @@ import { IndexLink, Link } from 'react-router';
 import Title from 'react-title-component';
 import styles from './App.css';
 import logo from './logo.png';
+import { Tabs } from 'react-tabs';
+
+Tabs.setUseDefaultStyles(false);
 
 export default React.createClass({
   render() {
