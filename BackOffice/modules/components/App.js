523dfd4a250fdeb8fa812dbf9b043430b46a865b
Gil Mualem <gil@soluto.local>
2016-08-03 17:40:28 +0300

    - some ui changes

--- BackOffice/modules/components/App.js
+++ BackOffice/modules/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { IndexLink, Link } from 'react-router';
 import Title from 'react-title-component';
-import styles from './App.css';
+import style from './App.css';
 import logo from './logo.png';
 import { Tabs } from 'react-tabs';
 import { Observable } from 'rxjs/Rx';
@@ -15,11 +15,11 @@ Tabs.setUseDefaultStyles(false);
 export default React.createClass({
   render() {
     return (
-      <div className={styles.app}>
-        <div className={styles.header}>
+      <div className={style['app']}>
+        <div className={style['header']}>
           <Title render="Tweek"/>
-          <IndexLink to="/"><img src={logo} /></IndexLink>
-          <ul className={styles.menu} >
+          <IndexLink to="/"><img className={style['logo']} src={logo} /></IndexLink>
+          <ul className={style['menu']} >
             <li><Link to="/keys">keys</Link></li>
           </ul>
         </div>
