b72fa28fc80c367652c9d92dd6f9f5c3e172347e
Gil Mualem <gil@soluto.local>
2016-08-24 15:10:37 +0300

    - rollback tags.json manualy

--- BackOffice/modules/pages/keys/ducks/tags.js
+++ BackOffice/modules/pages/keys/ducks/tags.js
@@ -1,11 +1,31 @@
 import { handleActions } from 'redux-actions';
+import { withJsonData } from './withJsonData';
+import R from 'ramda';
+
 const TAGS_DOWNLOADED = 'TAGS_DOWNLOADED';
+const TAGS_SAVED = 'TAGS_SAVED';
 
 export async function downloadTags() {
-  const tags = await (await fetch(`/api/tags`, { credentials: 'same-origin' })).json();
+  const tags = await (await fetch('/api/tags', { credentials: 'same-origin' })).json();
   return { type: TAGS_DOWNLOADED, payload: tags };
 }
 
+export const saveNewTags = (tagsToSave) => async function (dispatch, getState) {
+  const currentTags = getState().tags.map(x => x.name);
+  console.log(tagsToSave, currentTags);
+  return;
+  const newTagsDiff = R.symmetricDifference(tagsToSave, currentTags);
+
+  await fetch('/api/tags', {
+    credentials: 'same-origin',
+    method: 'put',
+    ...withJsonData(newTagsDiff),
+  });
+
+  dispatch({ type: TAGS_SAVED, payload: newTagsDiff });
+};
+
 export default handleActions({
   [TAGS_DOWNLOADED]: (state, { payload }) => payload,
+  [TAGS_SAVED]: (state, { payload }) => R.uniq([...state, ...(payload.map(x => ({ name: x })))]),
 }, []);
