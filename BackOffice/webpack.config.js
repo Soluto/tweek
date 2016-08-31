38224ff2aea9a60cd306d4842b2c1744637cd118
asafr <asafr@soluto.com>
2016-08-31 18:22:35 +0300

    Removed ts loaders and typings since we don't use ts

--- BackOffice/webpack.config.js
+++ BackOffice/webpack.config.js
@@ -1,24 +1,6 @@
 import { ServerConfig, ClientConfig } from 'react-project/webpack';
 
 const cssLoader = ClientConfig.module.loaders.find(x => x.loader.indexOf('style-loader!') !== -1);
-const nodeModulesLoader = {
-    ...cssLoader,
-    test: /node_modules.+\.css$/,
-    loader: 'style-loader!css-loader!postcss-loader',
-};
-
 cssLoader.exclude = /node_modules/;
-
-ClientConfig.module.loaders.push({
-    test: /.ts$/,
-    exclude: 'node_modules',
-    loader: 'babel!ts-loader'
-});
-
-ServerConfig.module.loaders.push({
-    test: /.ts$/,
-    exclude: 'node_modules',
-    loader: 'ts-loader'
-});
 
 export { ClientConfig, ServerConfig };
