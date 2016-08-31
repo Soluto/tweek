ab8cedc66b7ccc574a488b7248ed8e1108bef2a9
asafr <asafr@soluto.com>
2016-08-31 18:18:41 +0300

    Cleaned up unused files and some formatting issues in webpack.config

--- BackOffice/webpack.config.js
+++ BackOffice/webpack.config.js
@@ -1,18 +1,24 @@
 import { ServerConfig, ClientConfig } from 'react-project/webpack';
 
 const cssLoader = ClientConfig.module.loaders.find(x => x.loader.indexOf('style-loader!') !== -1);
-const nodeModulesLoader = { ...cssLoader,
+const nodeModulesLoader = {
+    ...cssLoader,
     test: /node_modules.+\.css$/,
     loader: 'style-loader!css-loader!postcss-loader',
 };
 
 cssLoader.exclude = /node_modules/;
-ClientConfig.module.loaders.push({ test: /.ts$/,
-    exclude: 'node_modules',
-    loader: 'babel!ts-loader' });
 
-ServerConfig.module.loaders.push({ test: /.ts$/,
+ClientConfig.module.loaders.push({
+    test: /.ts$/,
     exclude: 'node_modules',
-    loader: 'ts-loader' });
+    loader: 'babel!ts-loader'
+});
+
+ServerConfig.module.loaders.push({
+    test: /.ts$/,
+    exclude: 'node_modules',
+    loader: 'ts-loader'
+});
 
 export { ClientConfig, ServerConfig };
