077a68c81506798f9ce03645861bb286a0449e01
Asafr <asafr@soluto.com>
2016-09-05 17:34:41 +0300

    Added Tweek.ApiService.Tests

---
+++ Tweek.ApiService.Tests/IConfigurationsApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.DataTypes;
+using Newtonsoft.Json.Linq;
+using RestEase;
+
+namespace Tweek.ApiService.Tests
+{
+    public interface IConfigurationsApi
+    {
+        [Get("configurations/{keyPath}")]
+        Task<JToken> Get([Path] string keyPath,[QueryMap] Dictionary<string, string> context);
+    }
+}
