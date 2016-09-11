4f7ec72800225792f4a715a372928be072bfdec9
Asafr <asafr@soluto.com>
2016-09-11 10:48:18 +0300

    Added smoke tests for value distribution

--- Tweek.ApiService.Tests/ServingModuleTests.cs
+++ Tweek.ApiService.Tests/ServingModuleTests.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using RestEase;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Tweek.ApiService.Tests
 {
     public class ServingModuleTests
     {
+        private readonly ITestOutputHelper mOutput;
         private readonly Dictionary<string, string> mEmptyContext;
         private readonly IConfigurationsApi mConfigurationsApi;
 
-        public ServingModuleTests()
+        public ServingModuleTests(ITestOutputHelper output)
         {
+            mOutput = output;
             var targetBaseUrl = Environment.GetEnvironmentVariable("TWEEK_SMOKE_TARGET");
 
             if (string.IsNullOrEmpty(targetBaseUrl))
@@ -60,6 +65,66 @@ namespace Tweek.ApiService.Tests
             // Assert
             Assert.Equal(JTokenType.String, response.Type);
             Assert.Equal(expectedResult, response.ToString());
+        }
+
+        [Fact]
+        public async Task GetSingleKey_WithBernoulliTrialValues_ShouldReturnResultToSome()
+        {
+            // Arrange
+            const double probablityOfTestFailure = 0.001;
+            const double configurationKeyBernoulliTrialSplit = 0.3;
+            var attemptsForHighEnoughProbability = 
+                (int)
+                Math.Round(
+                    Math.Max(
+                        Math.Log(probablityOfTestFailure, configurationKeyBernoulliTrialSplit),
+                        Math.Log(probablityOfTestFailure, 1-configurationKeyBernoulliTrialSplit)
+                    )
+                );
+                 
+            // Act
+            var keyRequests = Enumerable.Range(0, attemptsForHighEnoughProbability)
+                .Select(async i => await mConfigurationsApi.Get("@tests/multivariantkeys/bernoulli", new Dictionary<string, string> {{"device", Guid.NewGuid().ToString()}}));
+
+            var results = await Task.WhenAll(keyRequests);
+            var returnedValues = results.Where(x => x.Type == JTokenType.String).Select(x => x.ToString()).ToList();
+            
+            // Assert
+            Assert.Contains("true", returnedValues);
+            Assert.Contains("false", returnedValues);
+            mOutput.WriteLine("Attempts: {0}", attemptsForHighEnoughProbability);
+            mOutput.WriteLine("Key Bernulli Thresold: {0}%", Math.Round(configurationKeyBernoulliTrialSplit * 100));
+            mOutput.WriteLine("Received \"true\" Rate: {0}%", Math.Round(returnedValues.Count(x => x == "true") * 100d / attemptsForHighEnoughProbability));
+            mOutput.WriteLine("Chance for a test fail due to statistics: {0}%", probablityOfTestFailure * 100);
+        }
+
+        [Fact]
+        public async Task GetSingleKey_WithMultiVariantsValues_ShouldReturnResultToSome()
+        {
+            // Arrange
+            // Using Coupon Collectors Problem to find out when we'll probably get all options
+            // Result for success in 99% for 4 equal options of the cases came from here: 
+            // http://www.distributome.org/js/calc/CouponCollectorCalculator.html
+            const int numberOfAttempts = 30;
+
+            // Act
+            var keyRequests = Enumerable.Range(0, numberOfAttempts)
+                .Select(async i => await mConfigurationsApi.Get("@tests/multivariantkeys/weighted_normalized", new Dictionary<string, string> { { "device", Guid.NewGuid().ToString() } }));
+
+            var results = await Task.WhenAll(keyRequests);
+            var returnedValues = results.Where(x => x.Type == JTokenType.String).Select(x => x.ToString()).ToList();
+
+            // Assert
+            mOutput.WriteLine("Attempts: {0}", numberOfAttempts);
+            mOutput.WriteLine("Received \"test1\" Rate: {0}%", Math.Round((returnedValues.Count(x => x == "test1") * 100d) / numberOfAttempts));
+            mOutput.WriteLine("Received \"test2\" Rate: {0}%", Math.Round((returnedValues.Count(x => x == "test2") * 100d) / numberOfAttempts));
+            mOutput.WriteLine("Received \"test3\" Rate: {0}%", Math.Round((returnedValues.Count(x => x == "test3") * 100d) / numberOfAttempts));
+            mOutput.WriteLine("Received \"test4\" Rate: {0}%", Math.Round((returnedValues.Count(x => x == "test4") * 100d) / numberOfAttempts));
+            Assert.Contains("test1", returnedValues);
+            Assert.Contains("test2", returnedValues);
+            Assert.Contains("test3", returnedValues);
+            Assert.Contains("test4", returnedValues);
+            
         }
     }
 }
