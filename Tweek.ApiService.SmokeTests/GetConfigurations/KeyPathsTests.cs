cca8f1f0e30a6ef8ff4847f68524de4634a94c70
Asafr <asafr@soluto.com>
2016-09-12 16:04:15 +0300

    Restructured smoke test project

---
+++ Tweek.ApiService.SmokeTests/GetConfigurations/KeyPathsTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Tweek.ApiService.SmokeTests.GetConfigurations
+{
+    public class KeyPathsTests
+    {
+        private readonly ITweekApi mTweekApi;
+
+        public KeyPathsTests()
+        {
+            mTweekApi = TweekApiServiceFactory.GetTweekApiClient();
+        }
+
+        [Fact(DisplayName = "Requesting a key should return its value as a string")]
+        public async Task GetSingleKey_KeyExists_ShouldReturnKeyValue()
+        {
+            // Act
+            var response = await mTweekApi.GetConfigurations("@tests/nested/key1", new Dictionary<string, string>());
+
+            // Assert
+            Assert.Equal(JTokenType.String, response.Type);
+            Assert.Equal("test", response.ToString());
+        }
+
+        [Fact(DisplayName = "Requesting a non-existant key should return null")]
+        public async Task GetSingleKey_KeyDoesntExists_ShouldReturnKeyValue()
+        {
+            // Act
+            var response = await mTweekApi.GetConfigurations("@tests/nested/nonexisting-key", new Dictionary<string, string>());
+
+            // Assert
+            Assert.Equal(JTokenType.Null, response.Type);
+        }
+
+        [Fact(DisplayName = "Requesting a key tree should return an object with the values for the child keys")]
+        public async Task GetKeyTree_KeysExistsInPath_ShouldReturnObjectWithValueForEachKey()
+        {
+            // Act
+            var response = await mTweekApi.GetConfigurations("@tests/nested/_", new Dictionary<string, string>());
+
+            // Assert
+            Assert.Equal(JTokenType.Object, response.Type);
+            Assert.Equal("test", response.Value<string>("key1"));
+            Assert.Equal("test", response.Value<string>("key2"));
+        }
+
+        [Fact(DisplayName = "Requesting a non-existant key tree should return an empty object")]
+        public async Task GetKeyTree_PathDoesntExist_ShouldReturnEmptyObject()
+        {
+            // Act
+            var response = await mTweekApi.GetConfigurations("@tests/nested/nonexisting-key-path/_", new Dictionary<string, string>());
+
+            // Assert
+            Assert.Equal(JTokenType.Object, response.Type);
+            Assert.Equal("{}", response.ToString());
+        }
+    }
+}
--- Tweek.ApiService.SmokeTests/GetConfigurations/Models/TestContext.cs
+++ Tweek.ApiService.SmokeTests/GetConfigurations/Models/TestContext.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
-namespace Tweek.ApiService.Tests.Models
+namespace Tweek.ApiService.SmokeTests.GetConfigurations.Models
 {
     public class TestContext
     {
