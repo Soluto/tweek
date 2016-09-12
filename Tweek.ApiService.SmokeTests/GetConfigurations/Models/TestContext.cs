4ebce5432f5b4deb683eacf2e835901ace28c687
Asafr <asafr@soluto.com>
2016-09-12 16:16:03 +0300

    Added display names to tests

--- Tweek.ApiService.SmokeTests/GetConfigurations/Models/TestContext.cs
+++ Tweek.ApiService.SmokeTests/GetConfigurations/Models/TestContext.cs
@@ -4,14 +4,14 @@ namespace Tweek.ApiService.SmokeTests.GetConfigurations.Models
 {
     public class TestContext
     {
-        public TestContext()
-        {
-            Context = new Dictionary<string, string>();
-        }
-
         public string TestName { get; set; }
         public string KeyName { get; set; }
         public string ExpectedValue { get; set; }
-        public Dictionary<string, string> Context { get; set; }
+        public Dictionary<string, string> Context { get; set; } = new Dictionary<string, string>();
+
+        public override string ToString()
+        {
+            return TestName;
+        }
     }
 }
---
+++ tests.ps1
@@ -0,0 +1,25 @@
+﻿$xunitConsoleRunner = "$env:APPVEYOR_BUILD_FOLDER\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe"
+
+# Start Local Service for Smoke Tests
+Start-Job -Name RunLocalDeployment -ScriptBlock { 
+    & 'C:\Program Files (x86)\IIS Express\iisexpress.exe' /port:1234 /path:"$env:APPVEYOR_BUILD_FOLDER\Tweek.ApiService"
+}
+
+Wait-Job -Name RunLocalDeployment -Timeout 5
+
+$Env:TWEEK_SMOKE_TARGET = "http://localhost:1234"
+
+# Run Smoke Tests
+$testAssemblies = @(
+    "$env:APPVEYOR_BUILD_FOLDER\Engine.Tests\bin\Release\Engine.IntegrationTests.dll",
+    "$env:APPVEYOR_BUILD_FOLDER\Engine.Core.Tests\bin\Release\Engine.Core.Tests.dll",
+    "$env:APPVEYOR_BUILD_FOLDER\Tweek.ApiService.Tests\bin\Release\Tweek.ApiService.Tests.dll",
+    "$env:APPVEYOR_BUILD_FOLDER\JPad\Tweek.JPad.Tests\bin\Release\Tweek.JPad.Tests.dll"
+)
+
+& $xunitConsoleRunner $testAssemblies
+
+# Stop Local Service
+Receive-Job -Name RunLocalDeployment | Write-Host
+Stop-Job -Name RunLocalDeployment 
+Remove-Job -Name RunLocalDeployment
