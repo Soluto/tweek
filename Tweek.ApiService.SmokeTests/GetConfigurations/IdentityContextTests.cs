4e950c68036cbc0bb35c487f1ec4d23c1133807c
Asafr <asafr@soluto.com>
2016-09-12 16:55:09 +0300

    Added stubs for context smoke tests

--- Tweek.ApiService.SmokeTests/GetConfigurations/IdentityContextTests.cs
+++ Tweek.ApiService.SmokeTests/GetConfigurations/IdentityContextTests.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Tweek.ApiService.SmokeTests.GetConfigurations
 {
-    class IdentityContextTests
+    public class IdentityContextTests
     {
+        [Fact(DisplayName = "Get key by identity", Skip = "Not Implemented Yet")]
+        public void GetKey_WithIdentityInContext_ReturnsValue()
+        {
+            
+        }
+
+        [Fact(DisplayName = "Get key with rules and identity", Skip = "Not Implemented Yet")]
+        public void GetKeyWithRules_WithIdentityInContext_ReturnsMatchValueForIdentityContext()
+        {
+
+        }
+
+        [Fact(DisplayName = "Get key with rules, identity, and a field in the context", Skip = "Not Implemented Yet")]
+        public void GetKeyWithRules_WithIdentityAndFieldInContext_FieldOverridesIdentityContext()
+        {
+
+        }
+
+        [Fact(DisplayName = "Get key with rules and an unknown identity", Skip = "Not Implemented Yet")]
+        public void GetKeyWithRules_WithUnkownIdentityInContext_ReturnsValueForNothingMatched()
+        {
+
+        }
+
     }
 }
