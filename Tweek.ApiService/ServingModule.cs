d05a136b937fd3874c3148d9eaa48667571a8c73
yshay <yshay@soluto.com>
2016-09-13 09:45:47 +0300

    fix $flatten behavior

--- Tweek.ApiService/ServingModule.cs
+++ Tweek.ApiService/ServingModule.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Engine;
 using Engine.Core.Context;
@@ -11,18 +12,31 @@ namespace Tweek.ApiService
 {
     public class ServingModule : NancyModule
     {
+        public static Tuple<IReadOnlyDictionary<TKey, TValue>, IReadOnlyDictionary<TKey, TValue>> PartitionByKey<TKey, TValue>(IDictionary<TKey,TValue> source, 
+            Predicate<TKey> predicate)
+        {
+            IReadOnlyDictionary<bool, IReadOnlyDictionary<TKey,TValue>> dict = source.GroupBy(x => predicate(x.Key))
+                .ToDictionary(p => p.Key, p => (IReadOnlyDictionary<TKey,TValue>)p.ToDictionary(x=>x.Key, x=>x.Value));
+
+            return Tuple.Create(dict.TryGetValue(true).IfNone(new Dictionary<TKey, TValue>()),
+                                dict.TryGetValue(false).IfNone(new Dictionary<TKey, TValue>()));
+        }
+
         private static readonly string PREFIX = "/configurations";
 
         public ServingModule(ITweek tweek) : base(PREFIX)
         {
             Get["{query*}", runAsync:true] = async (@params, ct) =>
             {
-                var isFlatten = @params["$flatten"] == true;
-                IReadOnlyDictionary<string,string> requestParams = ((DynamicDictionary) Request.Query).ToDictionary()
-                    .ToDictionary(x => x.Key, x => x.Value.ToString());
-                var identities = new HashSet<Identity>(requestParams.Where(x => !x.Key.Contains(".")).Select(x=>new Identity(x.Key, x.Value)));
+                var allParams = PartitionByKey(((DynamicDictionary) Request.Query).ToDictionary(), x => x.StartsWith("$"));
+                var modifiers = allParams.Item1;
+                var isFlatten = modifiers.TryGetValue("$flatten").Select(x=>bool.Parse(x.ToString())).IfNone(false);
+
+                IReadOnlyDictionary<string, string> contextParams = allParams.Item2.ToDictionary(x=>x.Key, x=>x.Value.ToString());
+
+                var identities = new HashSet<Identity>(contextParams.Where(x => !x.Key.Contains(".")).Select(x=>new Identity(x.Key, x.Value)));
                 GetLoadedContextByIdentityType contextProps =
-                    identityType => key => requestParams.TryGetValue($"{identityType}.{key}");
+                    identityType => key => contextParams.TryGetValue($"{identityType}.{key}");
                 
                 var query = ConfigurationPath.New(((string) @params.query));
                 var data = await tweek.Calculate(query, identities, contextProps);
