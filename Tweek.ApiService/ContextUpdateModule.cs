70eef71a9e29fa6baa612fa2958b23174d68fc6c
gil <gilm123@gmail.com>
2016-09-13 15:02:57 +0300

    - add get request to context update module

--- Tweek.ApiService/ContextUpdateModule.cs
+++ Tweek.ApiService/ContextUpdateModule.cs
@@ -18,6 +18,16 @@ namespace Tweek.ApiService
 
         public ContextUpdateModule(IContextDriver driver) : base(PREFIX)
         {
+            Get["/{identityType}/{identityId*}", runAsync: true] = async (@params, ct) =>
+            {
+                string identityType = @params.identityType;
+                string identityId = @params.identityId;
+                var identity = new Identity(identityType, identityId);
+
+                var identityContext = await driver.GetContext(identity);
+                return identityContext;
+            };
+
             Post["/{identityType}/{identityId*}", runAsync: true] = async (@params, ct) =>
             {
                 string identityType = @params.identityType;
