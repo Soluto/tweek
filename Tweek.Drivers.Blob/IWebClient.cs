6a34b037eb7ce8a122ff5a4536713ef177ed4ef9
gil <gilm123@gmail.com>
2016-10-30 15:20:29 +0200

    - add utf8 enocing for special characters - add unit tests

---
+++ Tweek.Drivers.Blob/IWebClient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tweek.Drivers.Blob
+{
+    public interface IWebClient : IDisposable
+    {
+        Encoding Encoding { get; set; }
+        Task<string> DownloadStringTaskAsync(Uri address);
+    }
+}
