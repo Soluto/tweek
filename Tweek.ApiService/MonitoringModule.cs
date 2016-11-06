73194c51481659543eea4f94d0ba7e9258776ba1
gil <gilm123@gmail.com>
2016-11-06 15:30:49 +0200

    test for deployment

---
+++ Tweek.ApiService/MonitoringModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Engine;
+using Engine.Core.Context;
+using Engine.Core.Utils;
+using Engine.DataTypes;
+using LanguageExt;
+using Nancy;
+using Newtonsoft.Json;
+
+namespace Tweek.ApiService
+{
+    public class MonitoringModule : NancyModule
+    {
+        private static readonly string PREFIX = "/monitor";
+
+        public MonitoringModule(ITweek tweek) : base(PREFIX)
+        {
+            Get["{query*}", runAsync: true] = async (@params, ct) =>
+             {
+                 return "pita";
+             };
+        }
+    }
+}
