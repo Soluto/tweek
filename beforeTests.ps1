224b0e1d948312fda6d89fcb8475174b01423157
gil <gilm123@gmail.com>
2016-11-06 18:14:21 +0200

    add tests requirement scripts

---
+++ beforeTests.ps1
@@ -0,0 +1,9 @@
+# Start Local Service for Smoke Tests
+Start-Job -Name RunLocalDeployment -ArgumentList (Resolve-Path "Tweek.ApiService").Path -ScriptBlock { 
+    Param($webservicePath)
+        & 'C:\\Program Files (x86)\\IIS Express\\iisexpress.exe' /port:1234 /path:"$webservicePath" /trace:warning
+}
+
+Wait-Job -Name RunLocalDeployment -Timeout 5
+
+$Env:TWEEK_SMOKE_TARGET = "http://localhost:1234"
