cada0dd35aca832ff6be9e2ba4116e3359627ae9
gil <gilm123@gmail.com>
2016-11-06 16:44:38 +0200

    - add isalive module - remove double slack notifications

---
+++ Tweek.ApiService/IsAliveModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Engine;
+using Engine.Core.Context;
+using Engine.Core.Utils;
+using Engine.DataTypes;
+using LanguageExt;
+using Nancy;
+using Newtonsoft.Json;
+
+namespace Tweek.ApiService
+{
+    public class IsAliveModule : NancyModule
+    {
+        public IsAliveModule()
+        {
+            Get["/isalive"] = _ =>
+            {
+                return HttpStatusCode.OK;
+            };
+        }
+    }
+}
