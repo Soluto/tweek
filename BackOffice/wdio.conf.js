1cec17c33e2d7a1f760ee5f2e365fe6f23be332a
gilios <gilm123@gamil.com>
2016-12-05 17:33:53 +0200

    - e2e try test

--- BackOffice/wdio.conf.js
+++ BackOffice/wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
   // directory is where your package.json resides, so `wdio` will be called from there.
   //
   specs: [
-    './spec/behavior/keys/*.js',
+    //'./spec/behavior/keys/*.js',
     './spec/behavior/tags/*.js',
   ],
   exclude: [
