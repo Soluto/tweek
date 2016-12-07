6785934c512727aa192591ffa7870c91e11c98c1
yshay <yshayy@gmail.com>
2016-12-07 10:53:27 +0200

    add async timeout

---
+++ Tweek.ApiService/Utils/NancyHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Tweek.ApiService.Utils
+{
+    public static class NancyHelpers
+    {
+        public static Func<dynamic, CancellationToken, Task<dynamic>> AsyncTimeout(
+            int timeout,
+            Func<dynamic, CancellationToken, Task<dynamic>> fn) =>
+                async (context, token) =>
+                {
+                    var newToken = new CancellationTokenSource();
+                    token.Register(() => newToken.Cancel());
+                    var delay = Task.Delay(timeout, newToken.Token);
+                    try
+                    {
+                        var result = await Task.WhenAny(delay, fn(context, newToken.Token));
+                        token.ThrowIfCancellationRequested();
+                        if (result == delay) throw new TimeoutException();
+                        return await result;
+                    }
+                    finally
+                    {
+                        newToken.Cancel();
+                    }
+                };
+    }
+}
