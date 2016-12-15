c60c5680af5aaae57cf597218dde5fbc0ff9a2ba
yshayy <yshayy@gmail.com>
2016-12-16 00:23:19 +0200

    make benchmark project easier

--- Performance/SimpleBenchmarks/Program.cs
+++ Performance/SimpleBenchmarks/Program.cs
@@ -10,7 +10,6 @@ using Engine.Core.Context;
 using Engine.DataTypes;
 using Engine.Drivers.Cassandra;
 using Engine.Drivers.Context;
-using Engine.Drivers.Rules.Git;
 using static LanguageExt.Prelude;
 using Tweek.JPad.Utils;
 using Tweek.JPad;
@@ -41,15 +40,9 @@ namespace SimpleBenchmarks
     {
         static void Main(string[] args)
         {
-            var gitDriver = new GitDriver(
-                Path.Combine(Environment.CurrentDirectory, "tweek-rules" + Guid.NewGuid()),
-                new RemoteRepoSettings()
-                {
-                    url = "http://tweek-gogs.c8940f48.svc.dockerapp.io/tweek/tweek.git",
-                    Email = "tweek@soluto.com",
-                    UserName = "tweek",
-                    Password = "po09!@QW"
-                });
+            var gitDriver = new Tweek.Drivers.Blob.BlobRulesDriver(
+                new Uri("https://tweek-management.mysoluto.com/ruleset/latest"),
+                new Tweek.Drivers.Blob.WebClient.SystemWebClientFactory());
 
             ITweek tweek = Task.Run(async () => await Engine.Tweek.Create(
                 new RamContextDriver(), gitDriver, JPadRulesParserAdapter.Convert(new JPadParser(new ParserSettings(
@@ -65,15 +58,23 @@ namespace SimpleBenchmarks
                     (key) => (identityType == "device" && key == "@CreationDate") ? Some("06/06/16") : None;
 
             Console.WriteLine("Start running");
-            Console.WriteLine(Enumerable.Range(0, 1000)
-                .Select(_ =>
+            Stopwatch swOverall = new Stopwatch();
+            swOverall.Start();
+            var results = Task.WhenAll(Enumerable.Range(0, 1000)
+                .Select(async _ =>
                 {
                     Stopwatch sw = new Stopwatch();
                     sw.Start();
-                    var data = tweek.Calculate(query, new HashSet<Identity>() { }, ext_context).Result;
+                    var data = await tweek.Calculate(query, new HashSet<Identity>() { }, ext_context).ConfigureAwait(false);
                     sw.Stop();
                     return sw.ElapsedMilliseconds;
-                }).Average());
+                })).Result;
+            swOverall.Stop();
+            Console.WriteLine("total:" + swOverall.ElapsedMilliseconds);
+            Console.WriteLine("avg:" + results.Average());
+            Console.WriteLine("max:" + results.Max());
+            Console.WriteLine("min:" + results.Min());
+
             Console.ReadLine();
 
 
