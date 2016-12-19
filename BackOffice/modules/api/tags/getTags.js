058f88cf3eec7fe6a8dd3d1d5fdd49768c27090e
asafr <asafr@soluto.com>
2016-12-19 14:44:42 +0200

    Removed pull from tags, since they're not important and just slow down requests

--- BackOffice/modules/api/tags/getTags.js
+++ BackOffice/modules/api/tags/getTags.js
@@ -2,8 +2,6 @@ import {getPathForTags} from "../../server/repositories/gitPathsUtils";
 export default async function (req, res, { gitTransactionManager }) {
 
   const tagsJson = await gitTransactionManager.transact(async gitRepo => {
-    await gitRepo.pull();
-
     return JSON.parse(await gitRepo.readFile(getPathForTags()));
   });
 
