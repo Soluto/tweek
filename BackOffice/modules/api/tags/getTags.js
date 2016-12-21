2b4d684ad790b8fd8d8d6110a966d3c78e1b2dd8
asafr <asafr@soluto.com>
2016-12-21 16:18:33 +0200

    Added 500 error code handling for api calls

--- BackOffice/modules/api/tags/getTags.js
+++ BackOffice/modules/api/tags/getTags.js
@@ -1,10 +1,15 @@
 import {getPathForTags} from "../../server/repositories/tweekPathsUtils";
 export default async function (req, res, { gitTransactionManager }) {
 
-  const tagsJson = await gitTransactionManager.transact(async gitRepo => {
-    return JSON.parse(await gitRepo.readFile(getPathForTags()));
-  });
+  try {
+    const tagsJson = await gitTransactionManager.transact(async gitRepo => {
+      return JSON.parse(await gitRepo.readFile(getPathForTags()));
+    });
 
-
-  res.json(tagsJson);
+    res.json(tagsJson);
+  }
+  catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 }
