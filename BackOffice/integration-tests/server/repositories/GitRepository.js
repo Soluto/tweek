076a81ae5cb177566fb2e3d74ecc715d80e0d518
asafr <asafr@soluto.com>
2016-12-25 11:34:14 +0200

    renamed GitRepository to git-repository

--- BackOffice/integration-tests/server/repositories/GitRepository.js
+++ BackOffice/integration-tests/server/repositories/GitRepository.js
@@ -1,6 +1,6 @@
 /* global jest, beforeEach, afterEach, describe, it, expect */
 
-import GitRepository from '../../../modules/server/repositories/GitRepository';
+import GitRepository from '../../../modules/server/repositories/git-repository';
 import { expect } from 'chai';
 import { Repository, Remote, Signature, Clone } from 'nodegit';
 import os from 'os';
