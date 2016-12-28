1ec74eaf7308770f43f37d3d37187927c0d6fa1b
Gil Mualem <gil@soluto.local>
2016-12-28 15:54:15 +0200

    - support readonly keys

--- BackOffice/modules/pages/keys/components/KeyPage/KeyEditPage/KeyPageActions/KeyPageActions.js
+++ BackOffice/modules/pages/keys/components/KeyPage/KeyEditPage/KeyPageActions/KeyPageActions.js
@@ -6,35 +6,50 @@ import * as keysActions from '../../../../ducks/selectedKey';
 import { deleteKey } from '../../../../ducks/keys';
 import { diff } from 'deep-diff';
 
+const DeleteButton = ({isSaving, selectedKey}) => (
+  <button disabled={isSaving}
+    className={style['delete-key-button']}
+    onClick={() => {
+      if (confirm('Are you sure?')) {
+        deleteKey(selectedKey.key);
+      }
+    } }>Delete key</button>
+);
+
+const SaveButton = ({selectedKey, isSaving, hasChanges}) => (
+  <button disabled={!hasChanges || isSaving}
+    data-state-has-changes={hasChanges}
+    data-state-is-saving={isSaving}
+    className={style['save-changes-button']}
+    onClick={() => saveKey(selectedKey.key)}>
+    {isSaving ? 'Saving...' : 'Save changes'}
+  </button>
+);
+
 const comp = compose(
   connect(
     state => ({ selectedKey: state.selectedKey }),
     { ...keysActions, deleteKey })
 )(
-  ({ selectedKey, isInAddMode, saveKey, deleteKey }) => {
+  ({ selectedKey, isInAddMode, saveKey, deleteKey, isReadonly }) => {
     const { local, remote, isSaving, isDeleting } = selectedKey;
     const changes = diff(local, remote);
     const hasChanges = (changes || []).length > 0;
+
     return (
-      <div className={style['key-action-buttons-wrapper']}>
-        {!isInAddMode ?
-          <button disabled={isSaving}
-            className={style['delete-key-button']}
-            onClick={() => {
-              if (confirm('Are you sure?')) {
-                deleteKey(selectedKey.key);
-              }
-            } }>
-            Delete key
-            </button>
+      <div>
+        {isReadonly ?
+          <div className={style['readonly-key-message']}>This key is readonly</div>
           : null}
-        <button disabled={!hasChanges || isSaving}
-          data-state-has-changes={hasChanges}
-          data-state-is-saving={isSaving}
-          className={style['save-changes-button']}
-          onClick={() => saveKey(selectedKey.key)}>
-          {isSaving ? 'Saving...' : 'Save changes'}
-        </button>
+        <div className={style['key-action-buttons-wrapper']}>
+          {!isInAddMode ?
+            <DeleteButton isSaving={isSaving}
+              selectedKey={selectedKey} />
+            : null}
+          <SaveButton selectedKey={selectedKey}
+            isSaving={isSaving}
+            hasChanges={hasChanges} />
+        </div>
       </div>
     );
   });
