64ed1fc0d3779b2611b5212b8ee5a3b21d633476
gilios <gilm123@gamil.com>
2017-01-05 13:18:43 +0200

    - change validations hint

--- BackOffice/modules/store/ducks/ducks-utils/key-name-validations.js
+++ BackOffice/modules/store/ducks/ducks-utils/key-name-validations.js
@@ -2,30 +2,30 @@ import { BLANK_KEY_NAME } from './blankKeyDefinition';
 
 const lengthValidation = {
   rule: ({value}) => value.length > 0,
-  hint: 'Empty key name'
+  hint: 'Empty key name',
 };
 
 const blankNameValidation = {
   rule: ({value}) => {
     return !value.startsWith(BLANK_KEY_NAME);
   },
-  hint: 'Invalid key name'
+  hint: 'Invalid key name',
 };
 
 const existingKeyValidation = {
   rule: ({value, keysList}) => keysList.indexOf(value) < 0,
-  hint: 'Key name already exists'
+  hint: 'Key name already exists',
 };
 
 const keyAsFolderNameValidation = {
   rule: ({value, keysList}) => !keysList.some(x => x.startsWith(value + '/')) &&
     !keysList.some(x => x.indexOf('/' + value + '/') > -1),
-  hint: 'Key name similar to existing category'
+  hint: 'Key name similar to existing category',
 };
 
 const invalidCharactersValidation = {
   rule: ({value}) => /(^[A-Za-z0-9#!@#$%^&()\-_=]+)(\/([A-Za-z0-9#!@#$%^&()\-_=\s])+)*$/.test(value),
-  hint: 'Invalid key name',
+  hint: 'Key name cannot include special characters',
 };
 
 let keyNameValidations = [lengthValidation, blankNameValidation, existingKeyValidation, keyAsFolderNameValidation, invalidCharactersValidation];
