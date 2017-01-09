b1adbee07d0c6622b07fa7f645d4c16b712ec3a7
gilios <gilm123@gamil.com>
2017-01-09 12:07:46 +0200

    - fix editabletextarea bug

--- BackOffice/modules/pages/keys/components/KeyPage/KeyEditPage/EditableTextArea/EditableTextArea.js
+++ BackOffice/modules/pages/keys/components/KeyPage/KeyEditPage/EditableTextArea/EditableTextArea.js
@@ -4,31 +4,32 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import style from './EditableTextArea.css';
 import classNames from 'classnames';
-import { withState } from 'recompose';
+import { compose, withState } from 'recompose';
 import TextareaAutosize from 'react-autosize-textarea';
 
-const EditableTextArea = withState('isInEditMode', 'setIsInEditMode', false)(
+const EditableTextArea = compose(
+  withState('isInEditMode', 'setIsInEditMode', false))(
   ({ value, placeHolder, title, maxLength, classNames: classes = {}, isInEditMode, setIsInEditMode, onTextChanged = () => { } }) => {
     return (
-      <div className={classNames(style['textarea-container'], classes.container) }>
+      <div className={classNames(style['textarea-container'], classes.container)}>
         <TextareaAutosize
           ref={(input) => {
             if (!input) return;
             input.refs.textarea.readOnly = !isInEditMode;
             if (isInEditMode) input.refs.textarea.focus();
           } }
-          onClick={() => setIsInEditMode(true) }
-          onChange={ (e) => onTextChanged(e.target.value) }
-          value = { value }
-          placeholder={ placeHolder }
-          title={ title }
-          className={classNames(style['textarea-input'], classes.input) }
+          onClick={() => setIsInEditMode(true)}
+          onChange={(e) => onTextChanged(e.target.value)}
+          value={value}
+          placeholder={placeHolder}
+          title={title}
+          className={classNames(style['textarea-input'], classes.input, { [style['read-only']]: !isInEditMode })}
           onBlur={() => {
             value = value.trim();
             setIsInEditMode(false);
             onTextChanged(value);
           } }
-          maxLength={ maxLength }
+          maxLength={maxLength}
           />
       </div>
     );
