1446c7170fcb78a6c823fcf50b43906f0eae4628
yshayy <yshayy@gmail.com>
2017-01-19 21:27:17 +0200

    revert revert

--- Engine.Core/Engine.cs
+++ Engine.Core/Engine.cs
@@ -5,6 +5,7 @@ using Engine.Core.Rules;
 using LanguageExt;
 using Engine.Core.Utils;
 using Engine.DataTypes;
+using FSharp.Data;
 
 namespace Engine.Core
 {
@@ -23,7 +24,7 @@ namespace Engine.Core
             GetRuleValue getRuleValue = null;
             GetContextValue recursiveContext = key =>
             {
-                if (!key.StartsWith("@@key")) return Option<string>.None;
+                if (!key.StartsWith("@@key")) return Option<JsonValue>.None;
                 var path = new ConfigurationPath(key.Split(':')[1]);
                 return getRuleValue(path).Map(x => x.Value);
             };
--- JPad/Tweek.JPad/Common.fs
+++ JPad/Tweek.JPad/Common.fs
@@ -1,9 +1,10 @@
 ﻿namespace Tweek.JPad
 open System;
+open FSharp.Data;
 
-type ComparerDelegate = delegate of string-> IComparable
-type Context = string-> Option<string>
-type ContextDelegate = delegate of string-> Option<string>
+type ComparerDelegate = delegate of string -> IComparable
+type Context = string-> Option<JsonValue>
+type ContextDelegate = delegate of string -> Option<JsonValue>
 type ParserSettings = {Comparers: System.Collections.Generic.IDictionary<string,ComparerDelegate>}
 exception ParseError of string
-type JPadEvaluate = Context -> Option<string>
+type JPadEvaluate = Context -> Option<JsonValue>
