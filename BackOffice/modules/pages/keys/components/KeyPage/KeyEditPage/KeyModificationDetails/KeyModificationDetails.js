359420262862309af22f4d5e6f4d1576e8573af8
Asafr <asafr@soluto.com>
2017-02-01 15:48:04 +0200

    Added handling of "unknown" dates in last modified

--- BackOffice/modules/pages/keys/components/KeyPage/KeyEditPage/KeyModificationDetails/KeyModificationDetails.js
+++ BackOffice/modules/pages/keys/components/KeyPage/KeyEditPage/KeyModificationDetails/KeyModificationDetails.js
@@ -3,19 +3,16 @@ import moment from 'moment';
 import wrapComponentWithClass from '../../../../../../hoc/wrap-component-with-class';
 import style from './KeyModificationDetails.css';
 
-const modifyDateFromat = 'DD/MM/YYYY HH:mm';
-
 const KeyModificationDetails = wrapComponentWithClass(({ modifyCompareUrl, modifyDate, modifyUser }) => {
-  const modifyDateFromNow = moment(modifyDate).fromNow();
-  const formatedModifyDate = 'Modify date: ' + moment(modifyDate).format(modifyDateFromat);
-
   return (
-    <div className={style['modification-details-text']} title={ formatedModifyDate }>
+    <div className={style['modification-details-text']}>
       <a href={modifyCompareUrl}
         target="_blank"
         >
         <label>Last modify: </label>
-        <label className={style['actual-sub-text']}>{modifyDateFromNow}, by {modifyUser}</label>
+        <label className={style['actual-sub-text']}>
+          { modifyDate == "unknown" ? "Unknown" : `${moment(modifyDate).fromNow()}, by ${modifyUser}`}
+        </label>
       </a>
     </div>
   );
