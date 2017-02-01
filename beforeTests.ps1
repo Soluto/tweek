eebfe009740861904f46b7d3b9bdea753e4fe7e5
Yshayy <yshayy@gmail.com>
2017-02-01 19:18:21 +0200

    Update beforeTests.ps1

--- beforeTests.ps1
+++ beforeTests.ps1
@@ -1,13 +1,11 @@
-# Start Local Service for Smoke Tests
-
 echo "Before tests"
 
 Start-Job -Name RunLocalDeployment -ArgumentList (Resolve-Path "Tweek.ApiService.NetCore").Path -ScriptBlock { 
     Param($webservicePath)
-        Start-Process -FilePath "C:\Program Files\dotnet\dotnet.exe" -ArgumentList "run" -WorkingDirectory $webservicePath
+        Start-Process -FilePath "$($webservicePath)/publish/Tweek.ApiService.NetCore.exe" -WorkingDirectory $webservicePath
 }
 
-Wait-Job -Name RunLocalDeployment -Timeout 10
+Wait-Job -Name RunLocalDeployment -Timeout 5
 
 $Env:TWEEK_SMOKE_TARGET = "http://localhost:5000"
 
