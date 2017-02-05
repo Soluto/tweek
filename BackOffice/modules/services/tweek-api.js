cbb3969f7caebe2d499d717685cc7bd0c9383bf4
asafr <asafr@soluto.com>
2017-02-05 11:58:06 +0200

    Added config reducer and moved the tweekApi target to it to allow setting environment variable at runtime

--- BackOffice/modules/services/tweek-api.js
+++
@@ -1,6 +0,0 @@
-const tweekApiHostname = env.TWEEK_API_HOSTNAME;
-
-export async function GetSchema(){
-  let response = await fetch(`${tweekApiHostname}/configurations/@tweek/context/_?$ignoreKeyTypes=false`);
-  return await response.json();
-}
--- BackOffice/webpack.config.js
+++ BackOffice/webpack.config.js
@@ -1,16 +1,6 @@
 import { ServerConfig, ClientConfig } from 'react-project/webpack';
-import {DefinePlugin} from 'webpack';
-const nconf = require('nconf');
-nconf.argv().env().file({ file: `${process.cwd()}/config.json` });
 
 const cssLoader = ClientConfig.module.loaders.find(x => x.loader.indexOf('style-loader!') !== -1);
 cssLoader.exclude = /node_modules/;
-
-let definePlugin = new DefinePlugin({
-  "env.TWEEK_API_HOSTNAME": "\"" + nconf.get("TWEEK_API_HOSTNAME") + "\""
-});
-
-ClientConfig.plugins.unshift(definePlugin);
-ServerConfig.plugins.unshift(definePlugin);
 
 export { ClientConfig, ServerConfig };
