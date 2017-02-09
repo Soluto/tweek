7b8cf9ed091e98597dcdabc9de44de1ed97ee9ef
yshay <yshay@soluto.com>
2017-02-09 16:47:56 +0200

    remvoed docker for ci

--- beforeTests.ps1
+++ beforeTests.ps1
@@ -2,7 +2,7 @@ echo "Before tests"
 
 Start-Job -Name RunLocalDeployment -ArgumentList (Resolve-Path "Tweek.ApiService.NetCore").Path -ScriptBlock { 
     Param($webservicePath)
-        Start-Process -FilePath "C:\Program Files\dotnet\dotnet" -ArgumentList "run" -WorkingDirectory $webservicePath
+        Start-Process -FilePath "C:\Program Files\dotnet\dotnet.exe" -ArgumentList "run" -WorkingDirectory $webservicePath
 }
 
 Wait-Job -Name RunLocalDeployment -Timeout 5
