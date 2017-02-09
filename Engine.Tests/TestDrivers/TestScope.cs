33dfaca347cb8d8142a3e0ee01c0b6a77fecdbec
yshay <yshay@soluto.com>
2017-02-09 20:50:09 +0200

    fix tests

--- Engine.Tests/TestDrivers/TestScope.cs
+++ Engine.Tests/TestDrivers/TestScope.cs
@@ -4,6 +4,8 @@ using Engine.Drivers.Context;
 using Engine.Drivers.Rules;
 using Tweek.JPad;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Microsoft.FSharp.Core;
 using Tweek.JPad.Utils;
 
 namespace Engine.Tests.TestDrivers
@@ -29,7 +31,15 @@ namespace Engine.Tests.TestDrivers
             try
             {
                 await _init();
-                var parserSettings = new ParserSettings(new Dictionary<string, ComparerDelegate>());
+                var parserSettings = new ParserSettings(FSharpOption<IDictionary<string, ComparerDelegate>>.Some(new Dictionary<string, ComparerDelegate>()), FSharpOption<Sha1Provider>.Some(
+                    (b) =>
+                    {
+                        using (var sha1 = SHA1.Create())
+                        {
+                            return sha1.ComputeHash(b);
+                        }
+                    } 
+                ));
                 var tweek = await Tweek.Create(_contextDriver, _rulesDriver, JPadRulesParserAdapter.Convert(new JPadParser(parserSettings)));
                 await test(tweek);
             }
