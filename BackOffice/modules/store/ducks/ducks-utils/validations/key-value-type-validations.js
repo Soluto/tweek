d5c503cbeb55022b546ae91fb03a90029276016a
asafr <asafr@soluto.com>
2017-02-13 15:46:48 +0200

    Fixed tests or key value type validation

--- BackOffice/modules/store/ducks/ducks-utils/validations/key-value-type-validations.js
+++ BackOffice/modules/store/ducks/ducks-utils/validations/key-value-type-validations.js
@@ -1,14 +1,10 @@
 import * as TypesService from '../../../../services/types-service';
 
-function isExistingType(type) {
-  return !!TypesService.types[type];
-}
-
 export default function (keyValueType) {
   if (!keyValueType) {
     return { isValid: false, hint: 'Empty key value type' };
   }
 
-  const isExists = isExistingType(keyValueType);
+  const isExists = !!TypesService.types[keyValueType];
   return { isValid: isExists, hint: !isExists ? 'Invalid type' : undefined };
 };
--- BackOffice/spec/unit/store/ducks/ducks-utils/key-value-type-validations-spec.js
+++ BackOffice/spec/unit/store/ducks/ducks-utils/key-value-type-validations-spec.js
@@ -1,40 +1,29 @@
 /* global jest, beforeEach, describe, it, expect */
 jest.unmock('../../../../../modules/store/ducks/ducks-utils/validations/key-value-type-validations');
-jest.mock('../../../../../modules/services/types-service', () => {
-  const types = {
-    mockType1: { type: "mock-type1" },
-    mockType2: { type: "mock-type1", typeAlias: "mock-type2" },
-  };
-  return {
-    types,
-    getTypeMeta: (type) =>
-      Object.keys(types).map(x => types[x]).find(x => x.typeAlias === type || x.type === type)
-  };
-});
+jest.mock('../../../../../modules/services/types-service', () => ({
+  types: {
+    string: { name: 'string' },
+    boolean: { name: 'boolean' },
+    number: { name: 'number' }
+  }
+}));
 
 import { assert } from 'chai';
 import keyValueTypeValidation from '../../../../../modules/store/ducks/ducks-utils/validations/key-value-type-validations';
 
 describe('key-value-type-validations', () => {
-  const testsDefenitions = [];
-  const setTestDefenition = (keyValueType, expectedResult) =>
-    testsDefenitions.push({
-      keyValueType,
-      expectedResult
-    });
 
-  setTestDefenition('', false);
-  setTestDefenition(null, false);
-  setTestDefenition(undefined, false);
-  setTestDefenition('mock-type1', true);
-  setTestDefenition('mock-type2', true);
-  setTestDefenition('unexisting-mock-type', false);
+  let checkTypeValidation = function (type, shouldBeValid) {
+    it('should return ' + (shouldBeValid ? "valid" : "invalid") + " for type " + type, () => {
+      expect(keyValueTypeValidation(type).isValid).toEqual(shouldBeValid);
+    })
+  };
 
-  testsDefenitions.forEach(x => it('should return correct validation result for type:' + x.keyValueType, () => {
-    // Act
-    const validationResult = keyValueTypeValidation(x.keyValueType);
-
-    // Assert
-    expect(validationResult.isValid).toEqual(x.expectedResult, 'validation result should be correct');
-  }));
+  checkTypeValidation('', false);
+  checkTypeValidation(null, false);
+  checkTypeValidation(undefined, false);
+  checkTypeValidation('string', true);
+  checkTypeValidation('boolean', true);
+  checkTypeValidation('number', true);
+  checkTypeValidation('non-existent-type', false);
 });
