a2d2d021e1a5e8fd526bb02f460dcbaa53fe3a0f
yshay <yshay@soluto.com>
2017-02-27 18:22:00 +0200

    migrate all tests to .net core. add docker ci

---
+++ Tweek.ApiService.NetCore/Diagnostics/EnvironmentDiagnosticsProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tweek.ApiService.NetCore.Diagnostics
+{
+    public class EnvironmentDiagnosticsProvider : IDiagnosticsProvider
+    {
+        public string Name => "EnvironmentDetails";
+
+        public string AppVersion =
+            Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationVersion;
+
+        public object GetDetails()
+        {
+            return new {Host=Environment.MachineName, Version = AppVersion};
+        }
+
+        public bool IsAlive()
+        {
+            return true;
+        }
+    }
+}
