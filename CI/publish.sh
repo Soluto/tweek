dc7c5d1fff45479f1502c4eb6b12bddd12932a8f
yshay <yshay@soluto.com>
2017-02-28 20:52:35 +0200

    attempt to add build number to version

--- CI/publish.sh
+++ CI/publish.sh
@@ -1,2 +1,2 @@
-dotnet publish ./Tweek.ApiService.NetCore/Tweek.ApiService.NetCore.csproj  -c Release -o ./obj/Docker/publish
+dotnet publish ./Tweek.ApiService.NetCore/Tweek.ApiService.NetCore.csproj  -c Release -o ./obj/Docker/publish --version-sufix $BUILD_NUMBER
 docker build -t soluto/tweek-api:latest ./Tweek.ApiService.NetCore
