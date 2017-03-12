62c340c1de4a1452094ca2773d4759c1d7c82c22
Yshay Yaacobi <yshay@soluto.com>
2017-03-12 18:55:07 +0200

    updated travis-ci and tweek-api version

--- CI/build.sh
+++
@@ -1,5 +0,0 @@
-set -e
-echo restoring packages
-dotnet restore Tweek.sln;
-echo building
-dotnet build Tweek.sln -c Release --version-suffix "ci-$BUILD_NUMBER";
--- CI/pack.sh
+++
@@ -1,3 +0,0 @@
-set -e
-dotnet publish ./Tweek.ApiService.NetCore/Tweek.ApiService.NetCore.csproj  -c Release -o ./obj/Docker/publish --version-suffix "ci-$BUILD_NUMBER"
-docker build -t soluto/tweek-api:candidate ./Tweek.ApiService.NetCore
