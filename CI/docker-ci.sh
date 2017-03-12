fd5beba81072d8008608e8ed8b168aca7e1a78ea
Yshay Yaacobi <yshay@soluto.com>
2017-03-12 19:55:29 +0200

    add ci version info

--- CI/docker-ci.sh
+++ CI/docker-ci.sh
@@ -2,7 +2,7 @@ set -e
 echo restoring packages
 dotnet restore Tweek.sln;
 echo building
-dotnet build Tweek.sln -c Release --version-suffix "ci-$BUILD_NUMBER";
+dotnet build Tweek.sln -c Release --version-suffix "$VERSION_SUFFIX";
 echo running tests
 find . -wholename '*.Tests.csproj' -print0 | xargs -0 -n 1 dotnet test
-dotnet publish ./Tweek.ApiService.NetCore/Tweek.ApiService.NetCore.csproj  -c Release -o ./obj/Docker/publish --version-suffix "ci-$BUILD_NUMBER"
+dotnet publish ./Tweek.ApiService.NetCore/Tweek.ApiService.NetCore.csproj  -c Release -o ./obj/Docker/publish --version-suffix "$VERSION_SUFFIX"
