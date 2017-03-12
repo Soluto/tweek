03cae792e3c084eb73786bfcb12cb6c8c137f9b5
Yshay Yaacobi <yshay@soluto.com>
2017-03-12 18:55:07 +0200

    updated travis-ci and tweek-api version

--- CI/ci.sh
+++ CI/ci.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
-
 set -e
-
+docker-compose run ci-build bash "./CI/docker-ci.sh"
+docker build -t soluto/tweek-api:candidate ./../Tweek.ApiService.NetCore
+echo running image;
+docker run --rm -d -p 5000:80 --name tweek-latest soluto/tweek-api:candidate;
+set +e
+curl --retry-delay 5 --retry 20 -v http://localhost:5000/status
+set -e
+echo running smoke tests;
+docker-compose run ci-build bash -c "dotnet restore Tweek.ApiService.SmokeTests/Tweek.ApiService.SmokeTests.csproj && dotnet test Tweek.ApiService.SmokeTests/Tweek.ApiService.SmokeTests.csproj -c Release --no-build";
 echo getting version number
-export TWEEK_VERSION=$(curl http://localhost:5000/status | jq '.EnvironmentDetails .Version' | grep -Po [0-9]+.[0-9]+.[0-9]+)
-echo $TWEEK_VERSION
-docker kill tweek-latest
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-
+TWEEK_VERSION=$(curl http://localhost:5000/status | jq '.EnvironmentDetails .Version' | grep -Po [0-9]+.[0-9]+.[0-9]+)
+docker rm -f tweek-latest
 function docker_tag_exists() {
     TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USERNAME}'", "password": "'${DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
     EXISTS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$2\"]" | grep -c true)
--- CI/test.sh
+++
@@ -1,2 +0,0 @@
-set -e
-find . -wholename '*.Tests.csproj' -print0 | xargs -0 -n 1 dotnet test
