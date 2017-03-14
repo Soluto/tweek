d94e7a695478cec4b7db158f967a08132cd61a56
Yshay Yaacobi <yshay@soluto.com>
2017-03-14 13:16:29 +0200

    updated tweek version

--- CI/ci.sh
+++ CI/ci.sh
@@ -10,12 +10,15 @@ echo running smoke tests;
 docker-compose run ci-build bash -c "dotnet restore Tweek.ApiService.SmokeTests/Tweek.ApiService.SmokeTests.csproj && dotnet test Tweek.ApiService.SmokeTests/Tweek.ApiService.SmokeTests.csproj -c Release --no-build";
 echo getting version number
 TWEEK_VERSION=$(curl http://localhost:5000/status | jq '.EnvironmentDetails .Version' | grep -Po [0-9]+.[0-9]+.[0-9]+)
+echo tweek version: $TWEEK_VERSION
 docker rm -f tweek-latest
 function docker_tag_exists() {
     TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USERNAME}'", "password": "'${DOCKER_PASSWORD}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
     EXISTS=$(curl -s -H "Authorization: JWT ${TOKEN}" https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$2\"]" | grep -c true)
     test $EXISTS = 1
 }
+
+
 
 if [ $TRAVIS_BRANCH != master ]; then
 	echo "no publish for non-master branches"
