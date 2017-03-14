096040111a415d0c3e73e9f59a3f95a8a1cff3ef
Nataly Shrits <nataly@soluto.com>
2017-03-14 14:39:49 +0200

    Added Partition selection to editor

--- BackOffice/modules/pages/keys/components/KeyPage/KeyEditPage/KeyRulesEditor/JPadEditor/JPadEditor.js
+++ BackOffice/modules/pages/keys/components/KeyPage/KeyEditPage/KeyRulesEditor/JPadEditor/JPadEditor.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PartitionsSelector from './Partition/PartitionsSelector';
 import RulesList from './RulesList/RulesList';
 import PartitionsList from './PartitionsList/PartitionsList';
 
@@ -8,10 +9,25 @@ export default ({valueType, mutate}) => {
   if (!isBrowser)
     return (<div>Loading rule...</div>);
 
-  const partitions = mutate.in('partitions').getValue();
+  const partitions = mutate.in("partitions").getValue();
 
-  if (partitions && partitions.length > 0)
-    return <PartitionsList partitions={partitions} valueType={valueType} mutate={mutate.in("rules")} />;
+  const updatePartitions = newPartitions => {
+    if (!confirm("If you change the partitions the rules will be reset.\nDo you want to continue?")) return;
+    mutate.apply(m => m.insert("partitions", newPartitions).insert("rules", newPartitions.length > 0 ? {"*": []} : []));
+  };
 
-  return (<RulesList valueType={valueType} mutate={mutate.in("rules")}/>)
+  return (
+    <div>
+      <PartitionsSelector
+        partitions={partitions}
+        onPartitionsChange={updatePartitions}
+      />
+
+      {
+        partitions && partitions.length > 0
+          ? <PartitionsList partitions={partitions} valueType={valueType} mutate={mutate.in("rules")}/>
+          : <RulesList valueType={valueType} mutate={mutate.in("rules")}/>
+      }
+    </div>
+  );
 }
