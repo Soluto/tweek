2281a30bd7243afe7812a6fb0c690d28dda8b775
asafr <asafr@soluto.com>
2017-03-15 17:47:34 +0200

    Some styling to partitions list

--- BackOffice/modules/components/Document.js
+++ BackOffice/modules/components/Document.js
@@ -29,7 +29,6 @@ const Document = React.createClass({
           <meta charSet="utf-8"/>
           <link rel="shortcut icon" href={favicon}/>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" />
-          <link rel="stylesheet" href="https://npmcdn.com/react-bootstrap-table/dist/react-bootstrap-table-all.min.css" />
           <title>{title}</title>
           {styles}
         </head>
