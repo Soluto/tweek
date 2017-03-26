0364050ea6be55499bd90f83de88356d1b1ca11c
Yshay Yaacobi <yshay@soluto.com>
2017-03-26 21:28:25 +0300

    first iteration, fix multiple autosuggest issues.

--- BackOffice/modules/components/common/ComboBox/ComboBox.js
+++ BackOffice/modules/components/common/ComboBox/ComboBox.js
@@ -1,7 +1,7 @@
 import {Typeahead} from 'react-bootstrap-typeahead';
 import React from 'react';
 import style from './ComboBox.css';
-import {compose, withState, mapProps} from 'recompose';
+import {compose, withState, mapProps, withPropsOnChange, mapPropsStream} from 'recompose';
 import R from 'ramda';
 import wrapComponentWithClass from '../../../hoc/wrap-component-with-class';
 
@@ -12,15 +12,28 @@ const comp = compose(
         [({label: currentInputValue, value: currentInputValue}), ...options] : options,
       currentInputValue, ...props,
     })
-  )
-)(({...props, autofocus, setCurrentInputValue}) => {
+  ),
+  mapPropsStream(props$=>{
+      return props$.scan((prev, next)=>{
+          if (prev.selected && prev.selected.length > 0 && next.selected && next.selected.length === 0){
+             return {
+                ...next,
+                currentInputValue : ""
+             }
+          }
+          else {
+            return next
+          }
+      });
+  })
+)(({...props, options, autofocus, setCurrentInputValue, currentInputValue}) => {
   const wrapperThemeClass = props.wrapperThemeClass ?
     props.wrapperThemeClass : style['combo-box-default-wrapper-theme-class'];
 
   return (
     <div className={wrapperThemeClass}>
       <Typeahead
-        {...props}
+        {...{...props, options}}
         onChange={selectedValues => {
           if (!props.onChange) return;
           if (selectedValues.length < 1) {
@@ -32,9 +45,24 @@ const comp = compose(
         }}
         onInputChange={text => {
           setCurrentInputValue(text);
+          let selectedItem = options.find((x)=> x === text || x.label === text);
+          console.log("selected", selectedItem);
+          if (selectedItem){
+             props.onChange(selectedItem);
+          }
           if (props.onInputChange) props.onInputChange(text);
         }}
-        ref={e => e && autofocus && e.refs.instance.focus()}
+        ref={e => 
+        {
+            e && autofocus && e.refs.instance.focus();
+            e && console.log("ref", currentInputValue);
+            if (currentInputValue ===""){
+              const value = e && e.refs.instance.refs.input && e.refs.instance.refs.input.props.value;
+              if (value && value !== "") {
+                 e.refs.instance.clear();
+              }
+            }
+        }}
       />
     </div>
   );
