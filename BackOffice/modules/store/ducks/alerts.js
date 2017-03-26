dfe5d3799df9c8e45ab9d7a2282df06d4e104de4
Nataly Shrits <nataly@soluto.com>
2017-03-26 17:52:48 +0300

    clean alerts ducks actions

--- BackOffice/modules/store/ducks/alerts.js
+++ BackOffice/modules/store/ducks/alerts.js
@@ -5,56 +5,61 @@ const chance = new Chance();
 const ADD_ALERT = 'ADD_ALERT';
 const REMOVE_ALERT = 'REMOVE_ALERT';
 
-const closeAlert = (dispatch, id, onClose) => (result) => {
-  dispatch({type: REMOVE_ALERT, id});
-  if (onClose) onClose(result);
-};
-
-export function showAlert({message}) {
-  return (dispatch) => {
-    const id = chance.guid();
-    const onClose = closeAlert(dispatch, id);
-    const alert = {
-      id,
-      message,
-      buttons: [{
-        value: 'OK',
-        onClick: onClose,
-        className: 'rodal-confirm-brn',
-      }],
-      onClose,
-      showCloseButton: true,
-    };
-    dispatch({type: ADD_ALERT, alert});
-  }
-}
-
-export function showConfirm({title, message, onResult, shouldShow = true}) {
+function addAlert({buttons, onResult, shouldShow = true, ...alertProps}) {
   if (!shouldShow) {
-    onResult(true);
+    if (onResult) onResult(true);
     return;
   }
 
   return (dispatch) => {
     const id = chance.guid();
-    const onClose = closeAlert(dispatch, id, onResult);
+    const onClose = (result) => {
+      dispatch({type: REMOVE_ALERT, id});
+      if (onResult) onResult(result);
+    };
+
     const alert = {
+      ...alertProps,
       id,
-      title,
-      message,
-      buttons: [{
-        value: 'OK',
-        onClick: () => onClose(true),
-        className: 'rodal-confirm-brn',
-      }, {
-        value: 'Cancel',
-        onClick: () => onClose(false),
-        className: 'rodal-cancel-brn',
-      }],
-      onClose: () => onClose(false),
+      onClose,
+      buttons: Array.isArray(buttons) ? buttons : buttons(onClose)
     };
     dispatch({type: ADD_ALERT, alert});
   }
+}
+
+export function showCustomAlert({buttons, ...alertProps}) {
+  return addAlert({
+    ...alertProps,
+    buttons: onClose => buttons.map(({value, ...props}) => ({...props, onClick: () => onClose(value)}))
+  });
+}
+
+export function showAlert(alertProps) {
+  return showCustomAlert({
+    ...alertProps,
+    showCloseButton: true,
+    buttons: [{
+      text: 'OK',
+      value: true,
+      className: 'rodal-confirm-brn',
+    }]
+  });
+}
+
+export function showConfirm(alertProps) {
+  return showCustomAlert({
+    ...alertProps,
+    buttons: [{
+      text: 'OK',
+      value: true,
+      className: 'rodal-confirm-brn',
+    }, {
+      text: 'Cancel',
+      value: false,
+      className: 'rodal-cancel-brn',
+    }]
+  });
 }
 
 export default handleActions({
