71066940af4f2977265af970ef938a507efb0c60
Nataly Shrits <nataly@soluto.com>
2017-03-27 16:13:39 +0300

    added function thet checks if auto partition is possible

--- BackOffice/modules/services/rules-service.js
+++ BackOffice/modules/services/rules-service.js
@@ -2,6 +2,34 @@ import R from 'ramda';
 import Chance from 'chance';
 const chance = new Chance();
 
+export function testAutoPartition(partition, rules, depth) {
+  if (depth == 0) {
+    const matchers = rules.map(r => r.Matcher[partition]).filter(p => p !== undefined);
+    const isValid = matchers.every(p => typeof p === 'string' || Object.keys(p).every(k => k == '$eq'));
+    if (!isValid) return {isValid};
+
+    const match = matchers.length;
+    return {
+      isValid,
+      match,
+      default: rules.length - match,
+    };
+  }
+
+  return Object.keys(rules).reduce((result, key) => {
+    if (!result.isValid) return result;
+
+    const test = testAutoPartition(partition, rules[key], depth - 1);
+    if (!test.isValid) return test;
+
+    return {
+      isValid: true,
+      match: test.match + result.match,
+      default: test.default + result.default
+    };
+  }, {isValid: true});
+}
+
 export function addPartition(partition, rules, depth) {
   if (depth == 0) {
     if (rules.length == 0) return {'*' : []};
