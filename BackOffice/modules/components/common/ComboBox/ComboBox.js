e74a0e8bd907fb39b68093ff54e71dc7f731cf75
Yshay Yaacobi <yshay@soluto.com>
2017-03-27 10:51:00 +0300

    fix combobox behavior

--- BackOffice/modules/components/common/ComboBox/ComboBox.js
+++ BackOffice/modules/components/common/ComboBox/ComboBox.js
@@ -1,67 +1,69 @@
-import {Typeahead} from 'react-bootstrap-typeahead';
+import { Typeahead } from 'react-bootstrap-typeahead';
 import React from 'react';
 import style from './ComboBox.css';
-import {compose, withState, mapProps, withPropsOnChange, mapPropsStream} from 'recompose';
+import { compose, withState, mapProps, mapPropsStream } from 'recompose';
 import R from 'ramda';
 import wrapComponentWithClass from '../../../hoc/wrap-component-with-class';
+const noop = () => {};
+const compareLowerCase = (a, b) =>
+            (a && a.toLowerCase && a.toLowerCase()) === (b && b.toLowerCase && b.toLowerCase());
 
 const comp = compose(
   withState('currentInputValue', 'setCurrentInputValue', ''),
-  mapProps(({options, currentInputValue, showValueInOptions, ...props}) => ({
-      options: currentInputValue && showValueInOptions && R.findIndex(x => x.label === currentInputValue)(options) < 0 ?
-        [({label: currentInputValue, value: currentInputValue}), ...options] : options,
-      currentInputValue, ...props,
-    })
-  ),
-  mapPropsStream(props$=>{
-      return props$.scan((prev, next)=>{
-          if (prev.selected && prev.selected.length > 0 && next.selected && next.selected.length === 0){
-             return {
-                ...next,
-                currentInputValue : ""
-             }
-          }
-          else {
-            return next
-          }
-      });
+  mapProps(({ options, currentInputValue, showValueInOptions, ...props }) => ({
+    options: currentInputValue && showValueInOptions && R.findIndex(x => x.label === currentInputValue)(options) < 0 ?
+        [({ label: currentInputValue, value: currentInputValue }), ...options] : options,
+    currentInputValue, ...props,
   })
-)(({...props, options, autofocus, setCurrentInputValue, currentInputValue}) => {
+  ),
+  mapPropsStream(props$ => props$.scan((prev, next) =>
+    (prev.selected && prev.selected.length > 0 && next.selected && next.selected.length === 0) ?
+    {
+      ...next,
+      currentInputValue: '',
+    } : next
+  )
+  )
+)(({ ...props, options,
+  selected = [],
+  autofocus,
+  setCurrentInputValue,
+  currentInputValue,
+  onChange = noop,
+  onInputChange = noop,
+}) => {
   const wrapperThemeClass = props.wrapperThemeClass ?
     props.wrapperThemeClass : style['combo-box-default-wrapper-theme-class'];
-
   return (
     <div className={wrapperThemeClass}>
       <Typeahead
-        {...{...props, options}}
+        {...{ ...props, options, selected }}
         onChange={selectedValues => {
-          if (!props.onChange) return;
+          if (onChange === noop) return;
           if (selectedValues.length < 1) {
-            if (props.clearButton) props.onChange();
+            if (props.clearButton) onChange();
             return;
           }
           setCurrentInputValue(selectedValues[0].label);
-          props.onChange(selectedValues[0]);
+          onChange(selectedValues[0]);
         }}
         onInputChange={text => {
           setCurrentInputValue(text);
-          let selectedItem = options.find((x)=> x === text || x.label === text);
-          console.log("selected", selectedItem);
-          if (selectedItem){
-             props.onChange(selectedItem);
+          const selectedItem = options.find((x) => compareLowerCase(x, text) || compareLowerCase(x.label, text));
+          if (selectedItem && onChange) {
+            onChange(selectedItem);
           }
-          if (props.onInputChange) props.onInputChange(text);
+          onInputChange(text);
         }}
-        ref={e => 
+        ref={e =>
         {
-            e && autofocus && e.refs.instance.focus();
-            e && console.log("ref", currentInputValue);
-            if (currentInputValue ===""){
-              const value = e && e.refs.instance.refs.input && e.refs.instance.refs.input.props.value;
-              if (value && value !== "") {
-                 e.refs.instance.clear();
-              }
+          e && autofocus && e.refs.instance.focus();
+          if (currentInputValue === '' && selected.length === 0) {
+            const value = e && e.refs.instance.refs.input && e.refs.instance.refs.input.props.value;
+            if (value && value !== '') {
+              e.refs.instance.clear();
             }
+          }
         }}
       />
     </div>
