eaa1c1ef51a7330e548c738df4bc81628625f570
Yshay Yaacobi <yshay@soluto.com>
2017-04-02 20:01:24 +0300

    fix travis path

--- CI/docker-ci.sh
+++ CI/docker-ci.sh
@@ -5,4 +5,4 @@ echo building
 dotnet build Tweek.sln -c Release --version-suffix "$VERSION_SUFFIX";
 echo running tests
 find . -wholename '*.Tests.csproj' -print0 | xargs -0 -n 1 dotnet test
-dotnet publish .services/api/Tweek.ApiService.NetCore/Tweek.ApiService.NetCore.csproj  -c Release -o ./obj/Docker/publish --version-suffix "$VERSION_SUFFIX"
+dotnet publish ./services/api/Tweek.ApiService.NetCore/Tweek.ApiService.NetCore.csproj  -c Release -o ./obj/Docker/publish --version-suffix "$VERSION_SUFFIX"
