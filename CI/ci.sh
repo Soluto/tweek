a1dcb96590d3523d0432f65cad9721194c4728be
Yshay Yaacobi <yshay@soluto.com>
2017-04-02 21:15:54 +0300

    attempt to fix travis ci

--- CI/ci.sh
+++ CI/ci.sh
@@ -7,7 +7,7 @@ docker network ls
 docker run -d -p 5000:80 --name tweek-latest --network ci_default --env RulesBlob.Url=http://tweek-management:3000/ruleset/latest  soluto/tweek-api:candidate;
 set +e
 curl --retry-delay 5 --retry 20 -v http://localhost:5000/status
-docker exec soluto/tweek-api:candidate curl --retry-delay 5 --retry 20 -v http://tweek-management:3000/ruleset/latest 
+docker exec tweek-latest --retry-delay 5 --retry 20 -v http://tweek-management:3000/ruleset/latest 
 set -e
 echo running smoke tests;
 docker-compose run ci-build bash -c "dotnet restore Tweek.ApiService.SmokeTests/Tweek.ApiService.SmokeTests.csproj && dotnet test Tweek.ApiService.SmokeTests/Tweek.ApiService.SmokeTests.csproj -c Release --no-build";
