89d967a9ed479742d3e0c8a9fbefff8c6a2d0141
Eyal Levy <eyal@soluto.com>
2017-04-05 16:08:42 +0300

    add searchBox component

---
+++ services/editor/modules/pages/context/components/ContextPage/ContextPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+import SearchBox from './SearchBox'
+
+export const ContextPage = () =>
+  <div >
+    <SearchBox />
+
+  </div>
+
+
+export default ContextPage;
