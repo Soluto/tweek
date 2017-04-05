6c2a6b47776ba5d769aa310149f52a73d4e2c817
Tal Glanzman <TalG@soluto.com>
2017-04-05 16:15:01 +0300

    Adding FixedConfiguration to the ContextPage

--- services/editor/modules/pages/context/components/ContextPage/ContextPage.js
+++ services/editor/modules/pages/context/components/ContextPage/ContextPage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Component } from 'react';
 
 import SearchBox from './SearchBox'
+import FixedConfiguration from '../FixedConfiguration/FixedConfiguration';
 
 export const ContextPage = () =>
   <div >
     <SearchBox />
-
+    <FixedConfiguration />
   </div>
 
 
