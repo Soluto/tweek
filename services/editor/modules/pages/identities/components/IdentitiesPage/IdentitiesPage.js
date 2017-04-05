e132639d81b2f53dedf75bb1d570b371c370c965
Or Rosenblatt <orr@soluto.com>
2017-04-05 17:53:37 +0300

    Added basic IdentitiesList component to IdentityPage

--- services/editor/modules/pages/identities/components/IdentitiesPage/IdentitiesPage.js
+++ services/editor/modules/pages/identities/components/IdentitiesPage/IdentitiesPage.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
 import { connect } from 'react-redux';
+import IdentitiesList from '../IdentitiesList/IdentitiesList';
 import style from './IdentitiesPage.css';
 import { compose } from 'recompose';
 import withLoading from '../../../../hoc/with-loading';
-import { refreshTypes } from '../../../../services/types-service';
 import { refreshSchema } from "../../../../services/context-service";
 
 const isNode=new Function("try {return this===global;}catch(e){return false;}");
 
 export default compose(
-  withLoading(() => null, isNode() ? Promise.resolve() : refreshTypes()),
   withLoading(() => null, isNode() ? Promise.resolve() : refreshSchema())
 )
 (class IdentitiesPage extends Component {
@@ -18,12 +17,20 @@ export default compose(
       super(props);
     }
 
-    componentDidMount() {
-    }
+    /*componentDidMount() {
+      if (!this.props.identities) {
+        this.props.getIdentities(['device','tech']);
+      }
+    }*/
 
     render() {
       return (
         <div className={style['identities-page-container']}>
+          <div key="IdentitiesList" className={style['identities-list']}>
+            <div className={style['identities-list-wrapper']}>
+              <IdentitiesList identities={[{name: 'device'},{name: 'tech'}]} />
+            </div>
+          </div>
         </div>
       );
     }
