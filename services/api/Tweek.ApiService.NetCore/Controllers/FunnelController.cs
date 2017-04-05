8b9978c72f6ec06fa8f197f43d6442d78d124ed1
Zahi Cohen <zahi@soluto.com>
2017-04-05 10:57:07 +0300

    added get to FunnelController and post now supports generic events

--- services/api/Tweek.ApiService.NetCore/Controllers/FunnelController.cs
+++ services/api/Tweek.ApiService.NetCore/Controllers/FunnelController.cs
@@ -1,30 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Cors;
-
-using System;
 using System.Linq;
-using Tweek.ApiService.NetCore.Security;
 using System.Collections.Generic;
 using Engine.DataTypes;
 using Microsoft.AspNetCore.Http;
 
 namespace Tweek.ApiService.NetCore.Controllers
 {
-	public class FunnelCounters
-	{
-		public int Start = 0;
-		public int Complete = 0;
-		public void IncrementStart() => Start++;
-		public void IncrementComplete() => Complete++;
-	}
-	
-	[Route("api/v1/funnel")]
+  [Route("api/v1/funnel")]
     [EnableCors("All")]
     public class FunnelController : Controller
     {
 		private static HashSet<string> Events = new HashSet<string>();
-		private static Dictionary<string, FunnelCounters> EventCounters = new Dictionary<string, FunnelCounters>();
+		private static Dictionary<string, Dictionary<string, long>> EventCounters = 
+			new Dictionary<string, Dictionary<string, long>>();
 
         private string _eventHash(string path, Identity identity, string eventName) => 
 			path + identity.Item1 + identity.Item2 + eventName;
@@ -38,18 +28,21 @@ namespace Tweek.ApiService.NetCore.Controllers
 
             if (identities.Count == 0) return BadRequest("At least one identity is mandatory for funnels");
 			if (identities.Count > 1) return BadRequest("Only one identity is currently supported");
-			if (eventName != "start" && eventName != "complete") return BadRequest("Only 'start' and 'complete' events are currently supported");
 			
 			var identity = identities.ElementAt(0);
 			var eventHash = _eventHash(path, identity, eventName);
 			if (Events.Contains(eventHash)) return Ok();
 			Events.Add(eventHash);
-			if (!EventCounters.ContainsKey(path)) EventCounters.Add(path, new FunnelCounters());
-			switch (eventName){
-				case "start": EventCounters[path].IncrementStart(); break;
-				case "complete": EventCounters[path].IncrementComplete(); break;
-			};
+			if (!EventCounters.ContainsKey(path)) EventCounters.Add(path, new Dictionary<string, long>{{eventName, 0}});
+			EventCounters[path][eventName] = EventCounters[path][eventName] + 1;
 			return Ok();
         }
+
+		[HttpGet("{*path}")]
+		public async Task<Dictionary<string, long>> Get([FromRoute] string path)
+		{
+			if (!EventCounters.ContainsKey(path)) return new Dictionary<string, long>();
+			return EventCounters[path];
+		}
     }
 }
