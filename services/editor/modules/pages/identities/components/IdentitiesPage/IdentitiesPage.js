6c92487369907839b52dcd97abd99b08227cf744
Or Rosenblatt <orr@soluto.com>
2017-04-05 13:19:50 +0300

    Added IdentitiesPage tab, linked to a page.

---
+++ services/editor/modules/pages/identities/components/IdentitiesPage/IdentitiesPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import withLoading from '../../../../hoc/with-loading';
+import { refreshTypes } from '../../../../services/types-service';
+import { refreshSchema } from "../../../../services/context-service";
+
+const isNode=new Function("try {return this===global;}catch(e){return false;}");
+
+export default compose(
+  withLoading(() => null, isNode() ? Promise.resolve() : refreshTypes()),
+  withLoading(() => null, isNode() ? Promise.resolve() : refreshSchema())
+)
+(class IdentitiesPage extends Component {
+    constructor(props) {
+      super(props);
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+      return (
+        <div>
+        </div>
+      );
+    }
+  });
