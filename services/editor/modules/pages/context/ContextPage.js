dbe7bebd7e606622eadba71561430d32e2db76d5
Eyal Levy <eyal@soluto.com>
2017-04-05 14:34:17 +0300

    add context page

---
+++ services/editor/modules/pages/context/ContextPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+
+export default compose(
+  connect(state => state),
+
+)
+(class ContextPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    if (!this.props.keys) {
+      this.props.getKeys([]);
+    }
+  }
+
+  render() {
+
+    return (
+      <div >
+            Hello world
+
+      </div>
+    );
+  }
+});
--- services/editor/modules/routes.js
+++ services/editor/modules/routes.js
@@ -4,6 +4,7 @@ import { Route, IndexRoute, Redirect } from 'react-router';
 import App from './components/App';
 import KeysPage from './pages/keys/components/KeysPage/KeysPage';
 import KeyPage from './pages/keys/components/KeyPage/KeyPage';
+import ContextPage from './pages/context/ContextPage';
 import NoMatch from './components/NoMatch';
 import style from './styles/styles.css';
 
@@ -14,6 +15,10 @@ export default serverRoutes => (
     <Redirect from="/" to="keys" />
     <Route path="/" component={App}>
       <Route path="keys" component={KeysPage} >
+        <IndexRoute component={() => <SelectKeyMessage />} />
+        <Route path="*" component={KeyPage} />
+      </Route>
+      <Route path="context" component={ContextPage} >
         <IndexRoute component={() => <SelectKeyMessage />} />
         <Route path="*" component={KeyPage} />
       </Route>
