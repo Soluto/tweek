3404f6d46ce89ad8894d670042c11ef3d7b04dbb
amir <amir@soluto.com>
2017-04-05 20:10:48 +0300

    support for multiple variants

--- services/analytics-api/Tweek.AnalyticsApiService.NetCore/Controllers/FunnelController.cs
+++ services/analytics-api/Tweek.AnalyticsApiService.NetCore/Controllers/FunnelController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using ServiceStack.Redis;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text.RegularExpressions;
 
 namespace Tweek.AnalyticsApiService.NetCore.Controllers
 {
@@ -12,8 +15,8 @@ namespace Tweek.AnalyticsApiService.NetCore.Controllers
     [EnableCors("All")]
     public class FunnelController : Controller
     {
-		private string _eventHash(string path, Identity identity, string eventName) => 
-			path + identity.Item1 + identity.Item2 + eventName;
+		private string _eventHash(string path, Identity identity, string eventName, string variant) => 
+			path + identity.Type + identity.Id + eventName + variant;
 
 		private T persistenceAction<T>(Func<IRedisClient, T> action)
 		{ 
@@ -23,7 +26,16 @@ namespace Tweek.AnalyticsApiService.NetCore.Controllers
 				// TODO: change to client with async api
 				return action(client);
 			}
-		}  
+		}
+
+		private async Task<string> GetVariant(string path, Identity identity){
+			var baseUrl = "http://localhost:4003/api/v1";
+			var url = baseUrl.TrimEnd('/') + "/keys/" + path.Trim('/') + $"?{identity.Type}={identity.Id}";
+			using (var httpClient = new HttpClient())
+			{
+				return JsonConvert.DeserializeObject<string>(await httpClient.GetStringAsync(url));				
+			}
+		}
 
         [HttpPost("{*path}")]
         public async Task<ActionResult> Post([FromRoute] string path, [FromQuery(Name = "event")] string eventName)
@@ -33,25 +45,39 @@ namespace Tweek.AnalyticsApiService.NetCore.Controllers
 					.Select(x => new Identity(x.Key, x.Value)));
 
             if (identities.Count == 0) return BadRequest("At least one identity is mandatory for funnels");
-			if (identities.Count > 1) return BadRequest("Only one identity is currently supported");
-			
+			if (identities.Count > 1) return BadRequest("Only one identity is currently supported");			
+
 			var identity = identities.ElementAt(0);
-			var eventHash = _eventHash(path, identity, eventName);
+			var variant = await GetVariant(path, identity);
+			if (variant == null) return BadRequest("Missing value for key");
+
+			var eventHash = _eventHash(path, identity, eventName, variant);
 			var didHappen = persistenceAction(client => client.Get<bool>(eventHash));
 			if (didHappen) return Ok();
-			persistenceAction(client => {
+			persistenceAction(client => 
+			{
 				client.Set(eventHash, true);
-				client.IncrementValueInHash(path, eventName, 1);
+				client.IncrementValueInHash(path, $"{variant}__{eventName}", 1);
 				return true;
 			});
 			return Ok();
         }
 
 		[HttpGet("{*path}")]
-		public async Task<Dictionary<string, long>> Get([FromRoute] string path)
+		public async Task<Dictionary<string, Dictionary<string, long>>> Get([FromRoute] string path)
 		{
-			var keyEvents = persistenceAction(client => client.GetAllEntriesFromHash(path));
-			return keyEvents.ToDictionary(x => x.Key, x => long.Parse(x.Value));
+			var variants = persistenceAction(client => client.GetAllEntriesFromHash(path));
+			return variants.Select(keyValuePair => 
+			{
+				var variantAndEvent = Regex.Split(keyValuePair.Key, "__");
+				return new {
+					variant = variantAndEvent[0],
+					eventName = variantAndEvent[1],
+					count = long.Parse(keyValuePair.Value)
+				};
+			})
+			.GroupBy(x => x.variant)
+			.ToDictionary(grouping => grouping.Key, grouping => grouping.ToDictionary(group => group.eventName, group => group.count));
 		}
     }
 
