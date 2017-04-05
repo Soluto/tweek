111a9d7464209456bb0cc4224db6d0da7e2a0b37
Eyal Levy <eyal@soluto.com>
2017-04-05 17:17:32 +0300

    send context id and context type to fix configuration component

--- services/editor/modules/pages/context/components/ContextPage/ContextPage.js
+++ services/editor/modules/pages/context/components/ContextPage/ContextPage.js
@@ -4,11 +4,30 @@ import { Component } from 'react';
 import SearchBox from './SearchBox'
 import FixedConfiguration from '../FixedConfiguration/FixedConfiguration';
 
-export const ContextPage = () =>
-  <div >
-    <SearchBox />
-    <FixedConfiguration />
-  </div>
+class ContextPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      contextType: '',
+      contextId: ''
+    }
+  }
+
+  onGetContext(contextType,contextId) {
+
+    this.setState({ contextType: contextType,  contextId: contextId});
+  }
 
 
+  render() {
+    return (
+      <div >
+        <SearchBox onGetContext={this.onGetContext.bind(this)}/>
+        <FixedConfiguration contextType={this.state.contextType} contextId={this.state.contextId}/>
+      </div>
+    );
+  }
+
+}
 export default ContextPage;
