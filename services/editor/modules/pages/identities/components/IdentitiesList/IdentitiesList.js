9a4d2e05eac5fd788d7281912d50a3a3bfbdd72b
Or Rosenblatt <orr@soluto.com>
2017-04-05 17:53:37 +0300

    Added basic IdentitiesList component to IdentityPage

---
+++ services/editor/modules/pages/identities/components/IdentitiesList/IdentitiesList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classNames from 'classnames';
+import { connect } from "react-redux";
+import { Link } from 'react-router';
+import { Observable } from 'rxjs/Rx';
+
+import style from './IdentitiesList.css';
+
+const IdentityItem = ({name}) => {
+  return (
+    <li className={classNames(style['identity-link'], { [style['selected']]: true })}>
+      {name}
+    </li>
+  );
+};
+
+const IdentitiesList = ({identities}) => {
+  const listItems = identities
+    .map((identitiy) => {
+      return (
+        <IdentityItem key={identitiy.name} name={identitiy.name} />
+      )});
+
+  return (
+      <div className={style['identities-list-container']}>
+        <ul>{listItems}</ul>
+      </div>
+    );
+};
+
+export default IdentitiesList;
