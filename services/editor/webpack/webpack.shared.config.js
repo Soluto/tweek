cc2c3232c0c65341c8cb8932e16510e0bbbb5bd9
Nataly Shrits <nataly@soluto.com>
2017-04-06 17:44:54 +0300

    fix prod build

--- services/editor/webpack/webpack.shared.config.js
+++ services/editor/webpack/webpack.shared.config.js
@@ -1,13 +1,15 @@
 import path from 'path';
+import nconf from 'nconf';
+nconf.argv().env().defaults({ PORT: 8080, DEV_HOST: 'localhost', DEV_PORT: 8081, NODE_ENV: 'development' });
 
 export const APP_PATH = process.cwd();
-export const PORT = process.env.PORT || 8080;
-export const DEV_HOST = process.env.DEV_HOST || 'localhost';
-export const DEV_PORT = process.env.DEV_PORT || 8081;
-export const PUBLIC_PATH = process.env.PUBLIC_PATH || '/';
+export const PORT = nconf.get('PORT');
+export const DEV_HOST = nconf.get('DEV_HOST');
+export const DEV_PORT = nconf.get('DEV_PORT');
+export const PUBLIC_PATH = nconf.get('PUBLIC_PATH') || '/';
 export const PUBLIC_DIR = path.join(APP_PATH, '.build');
-export const SERVER_RENDERING = process.env.SERVER_RENDERING === 'on';
-export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const SERVER_RENDERING = nconf.get('SERVER_RENDERING') === 'on';
+export const NODE_ENV = nconf.get('NODE_ENV');
 export const FONT_REGEX = /\.(otf|eot|svg|ttf|woff|woff2).*$/;
 export const IMAGE_REGEX = /\.(gif|jpe?g|png|ico)$/;
 export const CSS_REGEX = /\.css$/;
