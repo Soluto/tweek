e2585cfb3ab30d532524310c3d7049242a0ce7d4
Nataly Shrits <nataly@soluto.com>
2017-04-24 16:11:36 +0300

    aditor support for new meta format

--- services/editor/modules/api/keys.js
+++ services/editor/modules/api/keys.js
@@ -1,5 +1,54 @@
 import { UKNOWN_AUTHOR } from './unknownAuthor';
+import R from 'ramda';
 
+const DEPENDENT_KEY_PREFIX = '@@key:';
+
+function getDependenciesFromMatcher(matcher) {
+  return Object.keys(matcher).filter(x => x.startsWith(DEPENDENT_KEY_PREFIX)).map(x => x.substring(DEPENDENT_KEY_PREFIX.length));
+}
+
+function getDependencies(rules, depth) {
+  if (depth == 0) {
+    return R.chain(getDependenciesFromMatcher, rules.filter(r => r.Matcher).map(r => r.Matcher));
+  }
+  return Object.keys(rules).map(key => rules[key]).reduce((result, rule) => result.concat(getDependencies(rule, depth - 1)), []);
+}
+
+function generateMetaForKey(keyPath, {keyDef, meta}) {
+  const source = JSON.parse(keyDef.source);
+  const dependencies = R.uniq(getDependencies(source.rules, source.partitions.length));
+
+  return {
+    key_path: keyPath,
+    meta: {
+      name: meta.displayName,
+      tags: meta.tags,
+      description: meta.description,
+      archived: meta.archived,
+    },
+    implementation: {
+      type: 'file',
+      format: keyDef.type,
+    },
+    valueType: meta.valueType,
+    dependencies,
+    enabled: true,
+  };
+}
+
+function retrieveMetaForKey(meta) {
+  if (meta.meta == undefined) return meta;
+  console.log('dependencies', meta.dependencies);
+  const {meta: newMeta} = meta;
+  return {
+    displayName: newMeta.name,
+    description: newMeta.description,
+    tags: newMeta.tags,
+    valueType: meta.valueType,
+    archived: newMeta.archived,
+    enabled: meta.enabled,
+  }
+}
 
 let injectAuthor = (fn) => function (req, res, deps, ...rest) {
   return this::fn(req, res, {
@@ -9,33 +58,32 @@ let injectAuthor = (fn) => function (req, res, deps, ...rest) {
     }) || UKNOWN_AUTHOR,
     ...deps
   }, ...rest);
-}
+};
 
 export async function getKey(req, res, { keysRepository }, { params }) {
   const keyPath = params.splat;
   const revision = req.query.revision;
-  let keyDetails;
   try {
-    keyDetails = await keysRepository.getKeyDetails(keyPath, { revision });
+    const keyDetails = await keysRepository.getKeyDetails(keyPath, { revision });
+    res.json({...keyDetails, meta: retrieveMetaForKey(keyDetails.meta)});
   } catch (exp) {
     res.sendStatus(404);
   }
-
-  res.json(keyDetails);
 }
 
 export const saveKey = injectAuthor(async function (req, res, { keysRepository, author }, { params }) {
   const keyPath = params.splat;
 
   let keyRulesSource = req.body.keyDef.source;
-  let keyMetaSource = JSON.stringify(req.body.meta, null, 4);
+  const meta = generateMetaForKey(keyPath, req.body);
+  let keyMetaSource = JSON.stringify(meta, null, 4);
   await keysRepository.updateKey(keyPath, keyMetaSource, keyRulesSource, author);
 
   res.send('OK');
-})
+});
 
 export const deleteKey = injectAuthor(async function (req, res, { keysRepository, author }, { params }) {
   const keyPath = params.splat;
   await keysRepository.deleteKey(keyPath, author);
   res.send('OK');
-})
+});
