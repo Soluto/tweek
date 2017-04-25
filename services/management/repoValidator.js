51b30d2ae24e82f922c29e8e07f305daba270ab6
Nataly Shrits <nataly@soluto.com>
2017-04-25 10:54:07 +0300

    small optimizations in management endpoint

--- services/management/repoValidator.js
+++ services/management/repoValidator.js
@@ -1,5 +1,5 @@
 const _ = require('lodash');
-const JSZip = require("jszip");
+const JSZip = require('jszip');
 const fetch = require('node-fetch');
 const Promise = require('bluebird');
 const logger = require('./logger');
@@ -9,14 +9,6 @@ nconf.argv().env().file({ file: `${process.cwd()}/config.json` });
 const validationUrl = nconf.get('VALIDATION_URL');
 const DEPENDENT_KEY_PREFIX = '@@key:';
 
-function normalizeFileName(name) {
-    return name.replace(/\\/g, '/').replace(/.j(pad|son)/g, '');
-}
-
-function getValues(obj) {
-    return Object.keys(obj).map(k => obj[k]);
-}
-
 function getDependenciesFromMatcher(matcher) {
   return Object.keys(matcher).filter(x => x.startsWith(DEPENDENT_KEY_PREFIX)).map(x => x.substring(DEPENDENT_KEY_PREFIX.length));
 }
@@ -25,11 +17,12 @@ function calculateDependencies(rules, depth) {
   if (depth == 0) {
       return _.flatMap(rules.filter(r => r.Matcher).map(r => r.Matcher), getDependenciesFromMatcher);
   }
-  return getValues(rules).reduce((result, rule) => result.concat(calculateDependencies(rule, depth - 1)), []);
+  return Object.keys(rules).map(k => rules[k]).reduce((result, rule) => result.concat(calculateDependencies(rule, depth - 1)), []);
 }
 
 function getDependencies(meta, rule) {
-    if (meta && meta.meta) return meta.dependencies;
+    if (!meta) return [];
+    if (meta.meta) return meta.dependencies;
     const rulesObj = JSON.parse(rule);
     return calculateDependencies(rulesObj.rules || rulesObj, rulesObj.partitions? rulesObj.partitions.length : 0);
 }
@@ -47,32 +40,30 @@ module.exports = function (data) {
                 .filter(file => file.name.startsWith('rules') || file.name.startsWith('meta'))
                 .filter(file => !file.dir)
                 .map(file => {
-                    const name = normalizeFileName(file.name);
+                    const name = file.name.replace(/\\/g, '/').replace(/.j(pad|son)/g, '');
                     const index = name.indexOf('/');
-                    const type = name.substring(0, index);
                     return {
-                        name,
                         key: name.substring(index + 1),
-                        type,
-                        file: file.async("string").then(contents => [type, contents]),
+                        type: name.substring(0, index),
+                        file,
                     };
                 });
 
             const groupedFiles = _.groupBy(filteredFiles, 'key');
 
-            return getValues(groupedFiles)
-                .map(files => files.reduce((result, file) => Object.assign({}, result, {[file.type]: file}), {}))
-                .filter(files => files.rules)
-                .map(files => 
-                    Promise.all(getValues(files).map(f => f.file))
+            return Object.keys(groupedFiles)
+                .filter(key => groupedFiles[key].some(x => x.type == 'rules'))
+                .map(key => ({ key, files: groupedFiles[key].map(x => x.file.async('string').then(contents => [x.type, contents])) }))
+                .map(x => 
+                    Promise.all(x.files)
                         .then(pairs => _.fromPairs(pairs))
-                        .then(result => [files.rules.name, { format: 'jpad', payload: result.rules, dependencies: getDependencies(result.meta, result.rules)}])
+                        .then(result => [x.key, { format: 'jpad', payload: result.rules, dependencies: getDependencies(result.meta, result.rules)}])
                 );
         })
         .then(promises => Promise.all(promises))
         .then(pairs => _.fromPairs(pairs))
         .then(ruleset => {
-            logger.info("new ruleset was bundled");
+            logger.info('new ruleset was bundled');
             return ruleset;
         })
         .then(ruleset => {
@@ -81,7 +72,7 @@ module.exports = function (data) {
         })
         .then(ruleset => fetch(validationUrl, { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(ruleset) }))
         .then(ruleset => {
-            logger.info("finished request to validate rules", { timeToFetch: Date.now() - fetchStartTime });
+            logger.info('finished request to validate rules', { timeToFetch: Date.now() - fetchStartTime });
             return ruleset;
         })
         .then(checkStatus)
