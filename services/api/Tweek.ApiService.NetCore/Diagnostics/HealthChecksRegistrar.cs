8b919c9fb8fbca8b52eeabc16fa7b45027caa1b8
Michael Kruglos <michaelk@soluto.com>
2017-04-25 16:04:57 +0300

    Added code to automatically adapt all IDiagnosticsProvider singletons to HealthChecks

--- services/api/Tweek.ApiService.NetCore/Diagnostics/HealthChecksRegistrar.cs
+++ services/api/Tweek.ApiService.NetCore/Diagnostics/HealthChecksRegistrar.cs
@@ -1,38 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using App.Metrics;
 using App.Metrics.Health;
 using App.Metrics.Health.Abstractions;
-using Tweek.ApiService.Addons;
 
 namespace Tweek.ApiService.NetCore.Diagnostics
 {
     public class HealthChecksRegistrar
     {
-        private readonly IEnumerable<IDiagnosticsProvider> mDiagnosticsProviders;
         private readonly string mRulesBlobUrl;
 
-        public HealthChecksRegistrar(IEnumerable<IDiagnosticsProvider> diagnosticsProviders, string rulesBlobUrl)
+        public HealthChecksRegistrar(string rulesBlobUrl)
         {
-            mDiagnosticsProviders = diagnosticsProviders;
             mRulesBlobUrl = rulesBlobUrl;
         }
         public void RegisterHelthChecks(IHealthCheckFactory factory)
         {
-            foreach (var diagnosticsProvider in mDiagnosticsProviders)
-            {
-                var healthy = diagnosticsProvider.GetDetails().ToString()
-                    .Replace("{", "")
-                    .Replace("}", "")
-                    .Trim();
-
-                factory.Register(diagnosticsProvider.Name,
-                    () => Task.FromResult(diagnosticsProvider.IsAlive()
-                        ? HealthCheckResult.Healthy(healthy)
-                        : HealthCheckResult.Unhealthy($"Health check failed for {diagnosticsProvider.Name}")));
-            }
-
             var rulesBlobUri = new Uri(mRulesBlobUrl).GetComponents(UriComponents.Scheme | UriComponents.StrongAuthority, UriFormat.Unescaped);
 
             factory.RegisterHttpGetHealthCheck("ManagementIsAlive", new Uri($"{rulesBlobUri}/isAlive"), TimeSpan.FromSeconds(5));
