a5d3ab5b1ecd1818a589a04ac091240ddacc0955
Yshay Yaacobi <yshay@soluto.com>
2017-04-30 13:44:11 +0300

    update driver sampling to seconds

--- services/api/Tweek.ApiService.NetCore/Metrics/TimedRulesDriver.cs
+++ services/api/Tweek.ApiService.NetCore/Metrics/TimedRulesDriver.cs
@@ -24,7 +24,7 @@ namespace Tweek.ApiService.NetCore.Metrics
                 Name = "GetAll",
                 MeasurementUnit = Unit.None,
                 DurationUnit = TimeUnit.Milliseconds,
-                RateUnit = TimeUnit.Milliseconds
+                RateUnit = TimeUnit.Seconds,
             };
         }
 
