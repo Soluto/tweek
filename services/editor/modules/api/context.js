76402a344b716ae6794dc99f22fdf7a002af59e9
Nataly Shrits <nataly@soluto.com>
2017-05-08 13:43:32 +0300

    use authenticated endpoint for context

--- services/editor/modules/api/context.js
+++ services/editor/modules/api/context.js
@@ -1,38 +1,38 @@
-import changeCase from "change-case";
+import changeCase from 'change-case';
 import R from 'ramda';
 import authenticatedClient from '../server/auth/authenticatedClient';
 
 const mapKeys = R.curry((fn, obj) =>
   R.fromPairs(R.map(R.adjust(fn, 0), R.toPairs(obj))));
 
-export async function getContextSchema(req, res, {tweekApiHostname}) {
-  const tweekApiClient = await authenticatedClient({baseURL: tweekApiHostname});
+export async function getContextSchema(req, res, { tweekApiHostname }) {
+  const tweekApiClient = await authenticatedClient({ baseURL: tweekApiHostname });
   const response = await tweekApiClient.get('/api/v1/keys/@tweek/context/_?$ignoreKeyTypes=false');
   const schemaDetails = response.data;
-  const processedSchemaDetails = {};
-
-  for (let identity of Object.keys(schemaDetails)) {
-    processedSchemaDetails[identity] = mapKeys(changeCase.pascalCase, schemaDetails[identity]);
-  }
+  const processedSchemaDetails =
+    Object.keys(schemaDetails)
+      .reduce((result, identity) => ({
+        ...result,
+        [identity]: mapKeys(changeCase.pascalCase, schemaDetails[identity]),
+      }), {});
 
   res.json(processedSchemaDetails);
 }
 
-export async function getContext(req, res, { tweekApiHostname }, { params }){
+export async function getContext(req, res, { tweekApiHostname }, { params }) {
   const tweekApiClient = await authenticatedClient({ baseURL: tweekApiHostname });
-  const response = await tweekApiClient.get(`context/${params.contextType}/${params.contextId}`);
+  const response = await tweekApiClient.get(`api/v1/context/${params.contextType}/${encodeURIComponent(params.contextId)}`);
   res.json(response.data);
 }
 
-export async function updateContext(req, res, { tweekApiHostname }, { params }){
+export async function updateContext(req, res, { tweekApiHostname }, { params }) {
   const tweekApiClient = await authenticatedClient({ baseURL: tweekApiHostname });
-  await tweekApiClient.post(`context/${params.contextType}/${params.contextId}`, req.body);
+  await tweekApiClient.post(`api/v1/context/${params.contextType}/${encodeURIComponent(params.contextId)}`, req.body);
   res.sendStatus(200);
 }
 
 export async function deleteContextKey(req, res, { tweekApiHostname }, { params }) {
-  console.log("ke", params.key)
   const tweekApiClient = await authenticatedClient({ baseURL: tweekApiHostname });
-  await tweekApiClient.delete(`context/${params.contextType}/${params.contextId}/${params.key}`);
+  await tweekApiClient.delete(`api/v1/context/${params.contextType}/${encodeURIComponent(params.contextId)}/${params.key}`);
   res.sendStatus(200);
 }
