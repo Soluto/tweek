c289e943efeae78b55b985b32a730098d5921d54
Nataly Shrits <nataly@soluto.com>
2017-05-08 13:44:08 +0300

    show notification on error

--- services/editor/modules/store/ducks/context.js
+++ services/editor/modules/store/ducks/context.js
@@ -1,67 +1,75 @@
 import { handleActions } from 'redux-actions';
+import fetch from '../../utils/fetch';
+import { showError } from './notifications';
 
-const GET_CONTEXT = "GET_CONTEXT";
-const CONTEXT_RECEIVED = "CONTEXT_RECEIVED";
+const GET_CONTEXT = 'GET_CONTEXT';
+const CONTEXT_RECEIVED = 'CONTEXT_RECEIVED';
 
-const UPDATE_CONTEXT = "UPDATE_CONTEXT";
-const CONTEXT_UPDATED = "CONTEXT_UPDATED";
+const UPDATE_CONTEXT = 'UPDATE_CONTEXT';
+const CONTEXT_UPDATED = 'CONTEXT_UPDATED';
 
 export const getContext = ({ contextType, contextId }) => async function (dispatch) {
-    dispatch({ type: GET_CONTEXT });
-    let response = await fetch(`/api/context/${contextType}/${contextId}`, { credentials: 'same-origin' });
-    const contextData = await response.json()
-    console.log({ contextData });
+  dispatch({ type: GET_CONTEXT });
+  try {
+    const response = await fetch(`/api/context/${contextType}/${encodeURIComponent(contextId)}`, { credentials: 'same-origin' });
+    const contextData = await response.json();
     dispatch({ type: CONTEXT_RECEIVED, payload: { contextData } });
+  } catch (error) {
+    dispatch(showError({ title: 'Failed to retrieve context', error }));
+  }
 };
 
-export const updateContext = ({ contextType, contextId, updatedContextData, deletedContextKeys }) => async function(dispatch){
-    dispatch({ type: UPDATE_CONTEXT });
+export const updateContext = ({ contextType, contextId, updatedContextData, deletedContextKeys }) => async function (dispatch) {
+  dispatch({ type: UPDATE_CONTEXT });
+  const encodedContextId = encodeURIComponent(contextId);
 
-    console.log({updatedContextData, deletedContextKeys});
-
+  try {
     await Promise.all([
       ...deletedContextKeys
-          .map(encodeURIComponent)
-          .map(key => fetch(`/api/context/${contextType}/${contextId}/${key}`, {
-        credentials: 'same-origin',
-        method: 'DELETE',
-      })),
+        .map(encodeURIComponent)
+        .map(key => fetch(`/api/context/${contextType}/${encodedContextId}/${key}`, {
+          credentials: 'same-origin',
+          method: 'DELETE',
+        })),
 
-      fetch(`/api/context/${contextType}/${contextId}`, {
+      fetch(`/api/context/${contextType}/${encodedContextId}`, {
         credentials: 'same-origin',
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(updatedContextData)
-      })
+        body: JSON.stringify(updatedContextData),
+      }),
     ]);
 
     dispatch({ type: CONTEXT_UPDATED });
     dispatch(getContext({ contextType, contextId }));
-}
+  } catch (error) {
+    dispatch(showError({ title: 'Failed to update context', error }));
+  }
+};
 
 export default handleActions({
-    [GET_CONTEXT]: (state, action) => ({
-        ...state,
-        isGettingContext: true        
-    }),
-    [CONTEXT_RECEIVED]: (state, action) => ({
-        ...state,
-        contextData: action.payload.contextData,
-        isGettingContext: false
-    }),
-
-    [UPDATE_CONTEXT]: (state, action) => ({
-        ...state,
-        isUpdatingContext: true
-    }),
-
-    [CONTEXT_UPDATED]: (state, action) => ({
-        ...state,
-        isUpdatingContext: false
-    })
-}, {
+  [GET_CONTEXT]: (state, action) => ({
+    ...state,
+    isGettingContext: true,
+  }),
+  [CONTEXT_RECEIVED]: (state, action) => ({
+    ...state,
+    contextData: action.payload.contextData,
     isGettingContext: false,
-    contextData: {},
+  }),
 
-    isUpdatingContext: false
+  [UPDATE_CONTEXT]: (state, action) => ({
+    ...state,
+    isUpdatingContext: true,
+  }),
+
+  [CONTEXT_UPDATED]: (state, action) => ({
+    ...state,
+    isUpdatingContext: false,
+  }),
+}, {
+  isGettingContext: false,
+  contextData: {},
+
+  isUpdatingContext: false,
 });
