8b3233c6c893db6dcd1da2321a8d516250058851
Nataly Shrits <nataly@soluto.com>
2017-05-09 10:23:30 +0300

    restructure folders

---
+++ services/editor/modules/pages/context/components/ContextPage/FixedKeys/FixedKeys.js
@@ -0,0 +1,80 @@
+import React, { PropTypes } from 'react';
+import { compose, mapProps, lifecycle } from 'recompose';
+import { connect } from 'react-redux';
+import filteredPropsValues from '../../../utils/filteredPropsValues';
+import transformProps from '../../../utils/transformProps';
+import { getContext, updateContext } from '../../../../../store/ducks/context';
+import FixedConfigurationTable from './FixedKeysList/FixedKeysList';
+
+const trimString = value => value.trim();
+const padWithFixedPrefix = prop => `@fixed:${prop}`;
+const filterFixedConfigurationProps = filteredPropsValues(prop => prop.startsWith('@fixed:'));
+const removeFixedPrefix = transformProps(prop => prop.replace('@fixed:', ''));
+const addFixedPrefix = transformProps(padWithFixedPrefix);
+const trimSpaces = transformProps(trimString);
+
+const style = {
+  container: {
+    marginLeft: '10px',
+  },
+};
+
+const FixedConfiguration = ({ fixedConfigurations, updateContext }) => (
+  <div style={style.container}>
+    <div>
+      <h3 style={{ marginBottom: '1em' }}>Fixed Configuration</h3>
+      <div />
+    </div>
+    <FixedConfigurationTable
+      onSave={({ updatedConfiguration, deletedKeys }) => updateContext({
+        updatedConfiguration,
+        deletedKeys,
+      })}
+      fixedConfigurations={fixedConfigurations}
+    />
+  </div>
+  );
+
+FixedConfiguration.propTypes = {
+  fixedConfigurations: PropTypes.object,
+  updateContext: PropTypes.func.isRequired,
+  contextType: PropTypes.string.isRequired,
+  contextId: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = state => ({
+  contextData: state.context.contextData,
+});
+
+const mapDispatchToProps = (dispatch, props) => ({
+  getContext: () => dispatch(getContext({
+    contextType: props.contextType,
+    contextId: props.contextId,
+  })),
+  updateContext: ({ updatedConfiguration, deletedKeys }) => dispatch(updateContext({
+    contextType: props.contextType,
+    contextId: props.contextId,
+    updatedContextData: addFixedPrefix(trimSpaces(updatedConfiguration)),
+    deletedContextKeys: deletedKeys.map(trimString).map(padWithFixedPrefix),
+  })),
+});
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  lifecycle({
+    componentWillMount() {
+      this.props.getContext();
+    },
+
+    componentDidUpdate(prev) {
+      const { props } = this;
+      if (props.contextId !== prev.contextId || props.contextType !== prev.contextType) {
+        props.getContext();
+      }
+    },
+  }),
+  mapProps(props => ({
+    ...props,
+    fixedConfigurations: removeFixedPrefix(filterFixedConfigurationProps(props.contextData || {})),
+  })),
+)(FixedConfiguration);
