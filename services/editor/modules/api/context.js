ea4be18ff8d080b4b0256136a7b768cf9b10e148
Yshay Yaacobi <yshay@soluto.com>
2017-05-14 15:03:11 +0300

    moved logic to client side

--- services/editor/modules/api/context.js
+++ services/editor/modules/api/context.js
@@ -13,7 +13,7 @@ export async function getContextSchema(req, res, { tweekApiHostname }) {
     Object.keys(schemaDetails)
       .reduce((result, identity) => ({
         ...result,
-        [identity]: {...mapKeys(changeCase.pascalCase, schemaDetails[identity]), "@@id": "string"},
+        [identity]: mapKeys(changeCase.pascalCase, schemaDetails[identity])
       }), {});
 
   res.json(processedSchemaDetails);
