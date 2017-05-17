d60186bd86b292f81103caa9ed86262eca032595
Nataly Shrits <nataly@soluto.com>
2017-05-17 15:12:10 +0300

    use rx to get suggestions

--- services/editor/modules/components/common/Input/AutoSuggest.js
+++ services/editor/modules/components/common/Input/AutoSuggest.js
@@ -1,21 +1,48 @@
-import React, { PropTypes } from 'react';
-import { withState } from 'recompose';
+import React, { Component, PropTypes } from 'react';
+import Rx from 'rxjs';
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 import style from './AutoSuggest.css';
 
-const AutoSuggest = ({ suggestions, setSuggestions, getSuggestions, onChange, value, className, ...props }) => (
-  <div className={className}>
-    <AsyncTypeahead
-      {...props}
-      onSearch={query => Promise.resolve(getSuggestions(query)).then(setSuggestions)}
-      onChange={selectedValues => onChange(selectedValues[0])}
-      onInputChange={onChange}
-      options={suggestions}
-      filterBy={() => true}
-      selected={value === undefined ? [] : [value]}
-    />
-  </div>
-);
+class AutoSuggest extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      suggestions: [],
+    };
+  }
+
+  componentWillMount() {
+    this.onSearch$ = new Rx.Subject();
+    this.onSearch$
+      .debounceTime(500)
+      .switchMap(query => Promise.resolve(this.props.getSuggestions(query)))
+      .subscribe(suggestions => this.setState({ suggestions }));
+  }
+
+  componentWillUnmount() {
+    this.onSearch$.complete();
+  }
+
+  render() {
+    const { suggestions } = this.state;
+    const { onChange, value, className, ...props } = this.props;
+
+    return (
+      <div className={className}>
+        <AsyncTypeahead
+          {...props}
+          onSearch={query => this.onSearch$.next(query)}
+          onChange={selectedValues => onChange(selectedValues[0])}
+          onInputChange={onChange}
+          options={suggestions}
+          filterBy={() => true}
+          selected={value === undefined ? [] : [value]}
+        />
+      </div>
+    );
+  }
+}
 
 AutoSuggest.propTypes = {
   value: PropTypes.any,
@@ -29,4 +56,4 @@ AutoSuggest.defaultProps = {
   className: style['autosuggest-default-wrapper-theme-class'],
 };
 
-export default withState('suggestions', 'setSuggestions')(AutoSuggest);
+export default AutoSuggest;
