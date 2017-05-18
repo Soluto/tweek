012d73dd5828b637f5a3d5d762810ab769d47845
Nataly Shrits <nataly@soluto.com>
2017-05-18 19:28:21 +0300

    fix property type

--- services/editor/modules/pages/context/components/IdentityProperties/IdentityProperties.js
+++ services/editor/modules/pages/context/components/IdentityProperties/IdentityProperties.js
@@ -5,27 +5,30 @@ import TypedInput from '../../../../components/common/Input/TypedInput';
 import { getPropertyTypeDetails } from '../../../../services/context-service';
 import style from './IdentityProperties.css';
 
-const getPropertyValueType = property => getPropertyTypeDetails(property).name;
+const getPropertyValueType = (identityName, property) => {
+  const details = getPropertyTypeDetails(`${identityName}.${property}`);
+  return details.name;
+};
 
-const Property = ({ property, value }) => (
+const Property = ({ identityName, property, value }) => (
   <div className={style['property-wrapper']}>
     <Input className={style['property-input']} value={property} disabled />
     <TypedInput
       className={style['property-input']}
       value={value}
-      valueType={getPropertyValueType(property)}
+      valueType={getPropertyValueType(identityName, property)}
       placeholder="(no value)"
       disabled
     />
   </div>
 );
 
-const IdentityProperties = ({ className, properties }) => (
+const IdentityProperties = ({ className, identityName, properties }) => (
   <div className={classnames(style['context-properties-container'], className)}>
     <div className={style['context-properties-title']}>Properties</div>
     <div className={style['property-list']}>
       {
-        Object.keys(properties).map(prop => <Property property={prop} value={properties[prop]} />)
+        Object.keys(properties).map(prop => <Property key={prop} identityName={identityName} property={prop} value={properties[prop]} />)
       }
     </div>
   </div>
