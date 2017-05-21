a638f7063b489870e320a31e04efc964d2cfee42
Yshay Yaacobi <yshay@soluto.com>
2017-05-21 10:00:16 +0300

    change @@key to keys context

--- services/editor/modules/pages/keys/components/KeyPage/KeyEditPage/KeyRulesEditor/JPadEditor/Matcher/Properties/PropertyComboBox.js
+++ services/editor/modules/pages/keys/components/KeyPage/KeyEditPage/KeyRulesEditor/JPadEditor/Matcher/Properties/PropertyComboBox.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import R from 'ramda';
-import style from './styles.css';
-import ComboBox from '../../../../../../../../../components/common/ComboBox/ComboBox';
+import Chance from 'chance';
 import { withState } from 'recompose';
 import Highlighter from 'react-highlight-words';
 import ReactTooltip from 'react-tooltip';
+import style from './styles.css';
+import ComboBox from '../../../../../../../../../components/common/ComboBox/ComboBox';
 import * as ContextService from '../../../../../../../../../services/context-service';
+
+const chance = new Chance();
 
 const HIGHLIGHTED_TEXT_INLINE_STYLE = {
   fontWeight: 800,
@@ -13,21 +16,23 @@ const HIGHLIGHTED_TEXT_INLINE_STYLE = {
   color: 'gray',
 };
 
-const PropertyTooltip = ({propName, description, propType, identityType}) =>
+const PropertyTooltip = ({ propName, description, propType, identityType }) =>
   <div>
     <div style={{ fontSize: 18 }}><span>{identityType}</span>.<span>{propName}</span></div>
-    <div style={{ display: "flex", marginTop: 10 }}>
+    <div style={{ display: 'flex', marginTop: 10 }}>
       <div style={{ minWidth: 200, maxWidth: 400 }}>{description}</div>
-      <div style={{
-        textAlign: "center",
-        height: 60,
-        marginLeft: 40,
-        borderLeftColor: "#ffffff",
-        borderLeftStyle: "solid",
-        borderLeftWidth: 1,
-        paddingLeft: 18,
-        fontSize: 14
-      }}>
+      <div
+        style={{
+          textAlign: 'center',
+          height: 60,
+          marginLeft: 40,
+          borderLeftColor: '#ffffff',
+          borderLeftStyle: 'solid',
+          borderLeftWidth: 1,
+          paddingLeft: 18,
+          fontSize: 14,
+        }}
+      >
         <div style={{ marginBottom: 10 }}>Property Type</div>
         <div>
           {propType}
@@ -36,8 +41,8 @@ const PropertyTooltip = ({propName, description, propType, identityType}) =>
     </div>
   </div>;
 
-const PropertySuggestion = ({suggestion, textToMark}) => {
-  if (suggestion.value.startsWith('@@key:')) {
+const PropertySuggestion = ({ suggestion, textToMark }) => {
+  if (suggestion.value.startsWith('keys.')) {
     return (
       <div className={style['property-suggestion-wrapper']}>
         <div className={style['suggestion-identity']}>
@@ -56,8 +61,10 @@ const PropertySuggestion = ({suggestion, textToMark}) => {
   const tooltipId = chance.guid();
 
   return (
-    <div data-tip data-for={tooltipId} className={style['property-suggestion-wrapper']}
-         data-field-type={typeDetails.name}>
+    <div
+      data-tip data-for={tooltipId} className={style['property-suggestion-wrapper']}
+      data-field-type={typeDetails.name}
+    >
       <i />
       <Highlighter
         highlightClassName={style['suggestion-label']}
@@ -71,24 +78,27 @@ const PropertySuggestion = ({suggestion, textToMark}) => {
         searchWords={[textToMark]}
         textToHighlight={identity}
       />)</span>
-      <ReactTooltip id={tooltipId}
-                    place="right"
-                    type="dark"
-                    effect="solid"
-                    delayShow={1000}
-                    delayHide={1000}>
-        <PropertyTooltip propName={prop}
-                         identityType={identity}
-                         propType={typeDetails.name}
-                         description={typeDetails.description || ""} />
+      <ReactTooltip
+        id={tooltipId}
+        place="right"
+        type="dark"
+        effect="solid"
+        delayShow={1000}
+        delayHide={1000}
+      >
+        <PropertyTooltip
+          propName={prop}
+          identityType={identity}
+          propType={typeDetails.name}
+          description={typeDetails.description || ''}
+        />
       </ReactTooltip>
     </div>
   );
 };
 
 export default withState('currentInputValue', 'setCurrentInputValue', '')(
-  ({property, suggestedValues, onPropertyChange, autofocus, currentInputValue, setCurrentInputValue}) => {
-
+  ({ property, suggestedValues, onPropertyChange, autofocus, currentInputValue, setCurrentInputValue }) => {
     if (!!property && !suggestedValues.some(x => x.value === property)) {
       suggestedValues = [...suggestedValues, { label: property, value: property }];
     }
@@ -101,14 +111,14 @@ export default withState('currentInputValue', 'setCurrentInputValue', '')(
         onChange={onPropertyChange}
         placeholder="Property"
         selected={suggestedValues.filter(x => x.value === property)}
-        onInputChange={text => {
+        onInputChange={(text) => {
           setCurrentInputValue(text);
-          if (text.startsWith('@@key:')) {
-            onPropertyChange({ value: text });
+          if (text.startsWith('@@key:') || text.startsWith('keys.')) {
+            onPropertyChange({ value: text.replace('@@key:', 'keys.') });
           }
         }}
         filterBy={option => option.value.toLowerCase().includes(currentInputValue.toLowerCase())}
-        renderMenuItemChildren={(suggestion) => (
+        renderMenuItemChildren={suggestion => (
           <PropertySuggestion suggestion={suggestion} textToMark={currentInputValue} />
         )}
         autofocus={autofocus}
