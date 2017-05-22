6d4baf8a4cad4eb2a0782cea3bdc5aeb0856faab
YshayY <yshayy@gmail.com>
2017-05-22 11:45:05 +0300

    fix

--- services/editor/modules/server.js
+++ services/editor/modules/server.js
@@ -75,7 +75,7 @@ function getApp(req, res, requestCallback) {
 function addDirectoryTraversalProtection(server) {
   const DANGEROUS_PATH_PATTERN = /(?:^|[\\/])\.\.(?:[\\/]|$)/;
   server.use('*', (req, res, next) => {
-    if (req.path.contains('\0') || DANGEROUS_PATH_PATTERN.test(req.path)) {
+    if (req.path.includes('\0') || DANGEROUS_PATH_PATTERN.test(req.path)) {
       return res.status(400).send({ error: 'Dangerous path' });
     }
     return next();
