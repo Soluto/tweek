a5199bb229316a744fb47c08259540fef6de80e5
Nataly Shrits <nataly@soluto.com>
2017-05-24 16:34:26 +0300

    Operator component uses new combobox

--- services/editor/modules/pages/keys/components/KeyPage/KeyEditPage/KeyRulesEditor/JPadEditor/Matcher/Properties/Operator.js
+++ services/editor/modules/pages/keys/components/KeyPage/KeyEditPage/KeyRulesEditor/JPadEditor/Matcher/Properties/Operator.js
@@ -1,24 +1,18 @@
-import R from 'ramda';
 import React from 'react';
 import style from './styles.css';
-import ComboBox from '../../../../../../../../../components/common/ComboBox/ComboBox';
+import ComboBox from '../../../../../../../../../components/common/ComboBox/NewComboBox';
 
-export const Operator = ({ selectedOperator, onUpdate, supportedOperators }) => {
-  const props = {};
-  if (!!selectedOperator) props['selected'] = [{
-    label: selectedOperator.label,
-    value: selectedOperator.operatorValue,
-  }];
+// className={style['matcher-operator']}
 
-  return (
-    <ComboBox
-      options={supportedOperators.map(op => ({ value: op.operatorValue, label: op.label }))}
-      wrapperThemeClass={style['matcher-operator']}
-      onChange={(selected) => {
-        if (selected.value === '') return;
-        onUpdate(supportedOperators.find(x => x.operatorValue === selected.value));
-      }}
-      {...props}
-    />
-  );
-};
+const Operator = ({ selectedOperator, onUpdate, supportedOperators }) => (
+  <ComboBox
+    suggestions={supportedOperators.map(op => ({ value: op.operatorValue, label: op.label }))}
+    onChange={(_, selected) => {
+      if (!selected || selected.value === '') return;
+      onUpdate(supportedOperators.find(x => x.operatorValue === selected.value));
+    }}
+    value={selectedOperator && ({ label: selectedOperator.label, value: selectedOperator.operatorValue })}
+  />
+);
+
+export default Operator;
