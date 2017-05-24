d384d0919e2fada247bbc0118c9075f0636548b0
Nataly Shrits <nataly@soluto.com>
2017-05-24 16:17:05 +0300

    SearchBox uses new combobox

--- services/editor/modules/pages/context/components/ContextPage/SearchBox/SearchBox.js
+++ services/editor/modules/pages/context/components/ContextPage/SearchBox/SearchBox.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import changeCase from 'change-case';
 import { getIdentities } from '../../../../../services/context-service';
 import { openContext } from '../../../../../store/ducks/context';
-import ComboBox from '../../../../../components/common/ComboBox/ComboBox';
+import ComboBox from '../../../../../components/common/ComboBox/NewComboBox';
 import Input from '../../../../../components/common/Input/Input';
 import style from './SearchBox.css';
 
@@ -27,7 +27,8 @@ class SearchBox extends Component {
     this.setState(nextProps);
   }
 
-  onIdentityChange({ value: identityName }) {
+  onIdentityChange(input, selected) {
+    const identityName = selected ? selected.value : input;
     this.setState({ identityName });
   }
 
@@ -53,9 +54,9 @@ class SearchBox extends Component {
           <ComboBox
             className={style['context-type']}
             placeholder="Enter Identity Type"
-            options={identities}
+            value={identities.find(x => x.value === identityName)}
+            suggestions={identities}
             onChange={this.onIdentityChange.bind(this)}
-            selected={identities.filter(x => x.value === identityName)}
           />
         </div>
 
