eedad0e19805ce1409b75ffd4099821a00ab1887
YshayY <yshayy@gmail.com>
2017-05-29 23:29:46 +0300

    temp

--- services/editor/modules/hoc/with-loading.js
+++ services/editor/modules/hoc/with-loading.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { compose, lifecycle, pure, withState } from 'recompose';
 
-const withLoading = (loadingRenderer, loadingPromise) => Comp => compose(
+const withLoading = (loadingRenderer, loadingPromiseOrFactory) => Comp => compose(
     withState('isLoading', 'setIsLoading', true),
     pure,
     lifecycle({
       componentWillMount() {
-        loadingPromise.then(() => this.props.setIsLoading(false));
+        (loadingPromiseOrFactory.then ? loadingPromiseOrFactory : loadingPromiseOrFactory(this.props)).then(() => this.props.setIsLoading(false));
       },
     }))(props => (
         props.isLoading ? loadingRenderer() : <Comp {...props} />
