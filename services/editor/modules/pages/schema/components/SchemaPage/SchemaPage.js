440c02d117e4b4d7dd90d5cfd0db15d227f8910a
YshayY <yshayy@gmail.com>
2017-06-01 17:13:32 +0300

    more styling, removed directoryview

--- services/editor/modules/pages/schema/components/SchemaPage/SchemaPage.js
+++ services/editor/modules/pages/schema/components/SchemaPage/SchemaPage.js
@@ -5,36 +5,44 @@ import { connect } from 'react-redux';
 import style from './SchemaPage.css';
 import { compose, lifecycle } from 'recompose';
 import withLoading from '../../../../hoc/with-loading';
-import { refreshSchema } from "../../../../services/context-service";
-import DirectoryTreeView from "../../../keys/components/KeysList/DirectoryTreeView"
-import IdentityTypesItem from './IdentityTypesItem';
+import { refreshSchema } from '../../../../services/context-service';
+import { Link } from 'react-router';
 
-const isNode = new Function("try {return this===global;}catch(e){return false;}");
+const isNode = new Function('try {return this===global;}catch(e){return false;}');
+
+const LinkMenuItem = ({ path, name }) => (<li key={path} >
+  <Link to={`/schema/${path}`} >
+    {name}
+  </Link>
+</li>);
 
 export default compose(
   connect(state => ({}), { ...actions }),
-  withLoading(() => null, ({loadSchema}) => isNode() ? Promise.resolve() : refreshSchema().then(loadSchema)),
-  connect(state => ({identities: state.schema.identities})),
+  withLoading(() => null, ({ loadSchema }) => isNode() ? Promise.resolve() : refreshSchema().then(loadSchema)),
+  connect(state => ({ identities: state.schema.identities })),
   lifecycle({
-    componentDidMount(){
+    componentDidMount() {
       this.props.loadSchema();
-    }
+    },
   }),
-)
-( (props) => {
-      const { identities, children } = props;
-      console.log(identities);
-      return (
-        <div className={style['schema-page-container']}>
-          <div className={style['side-menu']} key="SideMenu">
-            <DirectoryTreeView paths={identities.map(x=> `identities/${x}`)}
-                renderItem={IdentityTypesItem}
-                expandByDefault={true} />
-          </div>
-          <div style={{display:"flex", flexGrow:1}} key="Page">
-            {children}
-          </div>
-        </div>
-      );
-    }
+)((props) => {
+  const { identities, children } = props;
+  return (
+    <div className={style['schema-page-container']}>
+      <ul className={style['side-menu']} key="SideMenu">
+        <li>
+          <div>Identities</div>
+          <ul>
+              {
+                identities.map(x => ({ path: `identities/${x}`, name: x })).map(LinkMenuItem)
+            }
+            </ul>
+        </li>
+      </ul>
+      <div style={{ display: 'flex', flexGrow: 1 }} key="Page">
+        {children}
+      </div>
+    </div>
+  );
+},
   );
