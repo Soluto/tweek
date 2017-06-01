2c16447dd5a52a092e90eefd5c6baae94f0f873c
YshayY <yshayy@gmail.com>
2017-06-01 14:41:48 +0300

    styled a bit

--- services/editor/modules/pages/schema/components/IdentityPage/IdentityPage.js
+++ services/editor/modules/pages/schema/components/IdentityPage/IdentityPage.js
@@ -2,17 +2,29 @@ import React from 'react';
 import style from './IdentityPage.css';
 import { connect } from "react-redux";
 import PropertyType from "./PropertyType/PropertyType";
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 const IdentityPage = ({params:{identityType}, allProperties}) => {
   const propertyTypes = allProperties.filter((property) => (property.identity === identityType));
 
   return (
-    <div className={style['property-types-list']}>
-      {
-          propertyTypes.map((property, i) =>
-            <PropertyType key={i} property={property} />
-          )
-      }
+    <div className={style['identity-page']}>
+      <h3 style={{textTransform:'capitalize'}}>{identityType}</h3>
+      <Tabs>
+        <TabList>
+            <Tab selected={true}>Properties</Tab>
+            <Tab disabled={true}>Permissions</Tab>
+        </TabList>
+        <TabPanel>
+          <div className={style['property-types-list']}>
+            {
+                propertyTypes.map((property, i) =>
+                  <PropertyType key={i} property={property} />
+                )
+            }
+          </div>
+        </TabPanel>
+      </Tabs>
     </div>);
 };
 
