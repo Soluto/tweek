fc9348fcc06df5e666ab0914a6600915eeef8f98
Nataly Shrits <nataly@soluto.com>
2017-06-04 16:53:05 +0300

    delete intermediate project

--- services/editor-new/server/api/search.js
+++
@@ -1,5 +0,0 @@
-import searchIndex from '../searchIndex';
-
-export function getSearchIndex(req, res) {
-    res.json(searchIndex.index);
-}
--- services/editor-new/src/pages/keys/components/KeyPage/MessageKeyPage/MessageKeyPage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import './MessageKeyPage.css';
-
-const MessageKeyPage = ({ message }) => <div className={'key-page-message'}>{message}</div>;
-export default MessageKeyPage;
