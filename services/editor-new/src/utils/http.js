c44570a00b0921e31273db9b53f88d72ca454b43
Nataly Shrits <nataly@soluto.com>
2017-06-04 16:53:05 +0300

    delete intermediate project

--- services/editor-new/src/utils/http.js
+++
@@ -1,9 +0,0 @@
-export function withJsonData(data) {
-  return {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  };
-}
