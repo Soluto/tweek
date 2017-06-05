9b456ef83f7352a2d168c249b7e68ef54810cce3
YshayY <yshayy@gmail.com>
2017-06-05 13:15:02 +0300

    refactor

--- services/editor/modules/pages/schema/components/IdentityPage/IdentityPage.js
+++ services/editor/modules/pages/schema/components/IdentityPage/IdentityPage.js
@@ -6,8 +6,10 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import SaveButton from '../../../../components/common/SaveButton/SaveButton';
 import R from 'ramda';
 import * as schemaActions from '../../../../store/ducks/schema';
+import { diff } from 'deep-diff';
 
-const IdentityPropertiesEditor = ({identityProperties, onPropertyUpdate}) => (
+
+const IdentityPropertiesEditor = ({ identityProperties, onPropertyUpdate }) => (
   <div className={style['property-types-list']}>
     {
                 R.toPairs(identityProperties).map(([name, type]) =>
@@ -17,20 +19,25 @@ const IdentityPropertiesEditor = ({identityProperties, onPropertyUpdate}) => (
   </div>
 );
 
-const IdentityPage = ({ params: { identityType }, identityProperties, updateIdentityProperty }) => (
-  <div className={style['identity-page']}>
-      <SaveButton data-comp="save-button" hasChanges={false} isSaving={false} onclick={() => {}} />
-      <h3 style={{ textTransform: 'capitalize' }}>{identityType}</h3>
-      <Tabs>
-        <TabList>
-          <Tab selected>Properties</Tab>
-          <Tab disabled>Permissions</Tab>
-        </TabList>
-        <TabPanel>
-          <IdentityPropertiesEditor identityProperties={identityProperties.local} onPropertyUpdate={updateIdentityProperty} />
-        </TabPanel>
-      </Tabs>
-    </div>);
+const IdentityPage = ({ params: { identityType }, identityProperties, updateIdentityProperty }) => {
+  
+  const changes = diff(identityProperties.local, identityProperties.remote);
+  const hasChanges = (changes || []).length > 0;
+
+  return (<div className={style['identity-page']}>
+    <SaveButton data-comp="save-button" hasChanges={hasChanges} isSaving={false} onclick={() => {}} />
+    <h3 style={{ textTransform: 'capitalize' }}>{identityType}</h3>
+    <Tabs>
+      <TabList>
+        <Tab selected>Properties</Tab>
+        <Tab disabled>Permissions</Tab>
+      </TabList>
+      <TabPanel>
+        <IdentityPropertiesEditor identityProperties={identityProperties.local} onPropertyUpdate={updateIdentityProperty} />
+      </TabPanel>
+    </Tabs>
+  </div>);
+};
 
 export default connect(state => ({
   schema: state.schema,
