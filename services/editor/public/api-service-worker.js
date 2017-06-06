7f715bffa90332a5d7415212968ac645eb2b1df6
Nataly Shrits <nataly@soluto.com>
2017-06-06 18:16:10 +0300

    add socket.io to notify clients data was updated

--- services/editor/public/api-service-worker.js
+++ services/editor/public/api-service-worker.js
@@ -1,3 +1,5 @@
+self.importScripts('/socket.io/socket.io.js');
+
 const CACHE_NAME = 'v1';
 const urlsToCache = ['/api/keys', '/api/search-index', '/api/types', '/api/context-schema'];
 
@@ -9,22 +11,33 @@ function getUrl(request) {
 async function install() {
   const cache = await caches.open(CACHE_NAME);
   await cache.addAll(urlsToCache);
+
+  const socket = io(self.origin, { jsonp: false });
+  socket.on('connect', () => console.log('connected to socket'));
+  socket.on('refresh', () => {
+    console.log('refreshing cache...');
+    refresh().then(() => console.log('cache refreshed'));
+  });
+
   self.skipWaiting();
 }
 
-async function loadFromCache(request) {
+async function refresh() {
+  console.log('refreshing cache...');
   const cache = await caches.open(CACHE_NAME);
-  const init = Object.assign({}, request);
-  delete init.url;
-  request = new Request(request.url.replace(/\/$/, ''), init);
+  const keys = await cache.keys();
+  await Promise.all(keys.map(key => cache.delete(key)));
+  await cache.addAll(urlsToCache);
+}
 
+async function loadFromCache(originalRequest) {
+  const cache = await caches.open(CACHE_NAME);
+
+  const request = new Request(originalRequest.url.replace(/\/$/, ''));
   const match = await cache.match(request);
   if (match) return match;
 
-  console.log('not found in cache, fetching...', request.url);
-  const response = await fetch(request.clone());
-  if (response.ok) cache.put(request, response.clone());
-  return response;
+  return await fetch(originalRequest);
 }
 
 async function activate() {
@@ -48,11 +61,6 @@ self.addEventListener('activate', (events) => {
 
 self.addEventListener('fetch', (event) => {
   if ('GET' === event.request.method) {
-    const url = getUrl(event.request);
-
-    if (urlsToCache.includes(url)) {
-      console.log('loading from cache', url);
-      event.respondWith(loadFromCache(event.request));
-    }
+    event.respondWith(loadFromCache(event.request));
   }
 });
