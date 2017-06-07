bf8182237d002ed45c8b05b10c9b2fc4152feebf
Michael Kruglos <MichaelK@soluto.com>
2017-06-07 20:55:56 +0300

    Added support for the following authentication schemes: Digest, Google and generic OAuth2

--- services/editor/server/index.js
+++ services/editor/server/index.js
@@ -14,7 +14,8 @@ import searchIndex from './searchIndex';
 
 const crypto = require('crypto');
 const passport = require('passport');
-const azureADAuthProvider = require('./auth/azuread');
+const selectAuthenticationProviders = require('./auth/providerSelector')
+  .selectAuthenticationProviders;
 
 nconf.argv().env().defaults({
   PORT: 3001,
@@ -69,9 +70,9 @@ function addAuthSupport(server) {
   server.use(passport.initialize());
   server.use(passport.session());
 
-  const authProviders = [azureADAuthProvider(server, nconf)];
+  const authProviders = selectAuthenticationProviders(server, nconf);
   server.use('/login', (req, res) => {
-    res.send(authProviders.map(x => `<a href="${x.url}">login with ${x.name}</a>`).join(''));
+    res.send(authProviders.map(x => `<a href="${x.url}">login with ${x.name}</a>`).join('<br/>'));
   });
 
   server.use('*', (req, res, next) => {
