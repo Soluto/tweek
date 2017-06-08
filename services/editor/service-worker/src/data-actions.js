a698014e26c308747ffc42227be8e05b18273215
Nataly Shrits <nataly@soluto.com>
2017-06-08 15:23:22 +0300

    redirectToLogin.js

--- services/editor/service-worker/src/data-actions.js
+++ services/editor/service-worker/src/data-actions.js
@@ -47,13 +47,3 @@ export async function refresh() {
 
   console.log('cache refreshed');
 }
-
-export async function redirectToLogin() {
-  await self.clients.claim();
-  const clients = await self.clients.matchAll({ type: 'window' });
-  clients.forEach((client) => {
-    if ('navigate' in client) {
-      client.navigate(urls.LOGIN);
-    }
-  });
-}
