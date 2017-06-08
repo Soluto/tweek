19004f35399b7dde91e8c33203b7d0ab8fe84826
Nataly Shrits <nataly@soluto.com>
2017-06-08 14:29:07 +0300

    loadFromCache.js

--- services/editor/service-worker/src/index.js
+++ services/editor/service-worker/src/index.js
@@ -1,46 +1,10 @@
-import idbKeyval from 'idb-keyval';
-import { CACHE_NAME, notificationTypes, urls } from './constants';
-import { testLogin, redirectToLogin } from './actions';
+import { notificationTypes } from './constants';
+import { redirectToLogin } from './actions';
 import install from './install';
 import activate from './activate';
-import getUrl from './getUrl';
+import loadFromCache from './loadFromCache';
 
 self.importScripts('/socket.io/socket.io.js');
-
-const replaceUrls = [
-  {
-    test: /^\/api\/keys\/?$/,
-    get: idbKeyval.keys,
-  },
-  {
-    test: /^\/api\/manifests\/(.+)/,
-    get: match => idbKeyval.get(match[1]),
-  },
-];
-
-async function loadFromCache(originalRequest) {
-  const url = getUrl(originalRequest);
-
-  const replace = replaceUrls.find(x => x.test.test(url));
-  if (replace) {
-    const result = await replace.get(url.match(replace.test));
-    return new Response(JSON.stringify(result), { status: result ? 200 : 404 });
-  }
-
-  const shouldCache = urls.CACHE.includes(url);
-  const request = new Request(originalRequest.url.replace(/\/$/, ''));
-
-  if (shouldCache) {
-    const match = await caches.match(request);
-    if (match) return match;
-  }
-  const response = await testLogin(originalRequest);
-  if (shouldCache && response.ok) {
-    const cache = await caches.open(CACHE_NAME);
-    cache.put(request, response.clone());
-  }
-  return response;
-}
 
 async function handleNotification(notification) {
   notification.close();
