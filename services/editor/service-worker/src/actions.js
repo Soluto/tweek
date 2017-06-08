77c60fedfdcc4a98942323dbdfe5eaf4ed19368c
Nataly Shrits <nataly@soluto.com>
2017-06-08 14:22:41 +0300

    move actions to separate file

---
+++ services/editor/service-worker/src/actions.js
@@ -0,0 +1,59 @@
+import idbKeyval from 'idb-keyval';
+import { CACHE_NAME, notificationTypes, urls } from './constants';
+let isLoggedIn = true;
+
+export async function testLogin(request, shouldLoadCache = true) {
+  const response = await fetch(request);
+
+  const wasLoggedIn = isLoggedIn;
+  isLoggedIn = response.status !== 403;
+
+  if (!isLoggedIn) {
+    if (Notification.permission === 'granted') {
+      self.registration.showNotification('Login expired\nPlease log in again', {
+        icon: '/tweek.png',
+        requireInteraction: true,
+        tag: notificationTypes.LOGIN,
+      });
+    }
+  } else {
+    const loginNotifications = await self.registration.getNotifications({
+      tag: notificationTypes.LOGIN,
+    });
+    loginNotifications.forEach(notification => notification.close());
+    if (!wasLoggedIn && shouldLoadCache) {
+      refresh();
+    }
+  }
+
+  return response;
+}
+
+export async function refresh() {
+  console.log('refreshing cache...');
+
+  const testLoginResponse = await testLogin(urls.IS_LOGGED_IN, false);
+  if (!testLoginResponse.ok) return;
+
+  const cache = await caches.open(CACHE_NAME);
+  await cache.addAll(urls.CACHE);
+
+  const localStorage = await fetch(urls.LOCAL_STORAGE);
+  if (localStorage.ok) {
+    const data = await localStorage.json();
+    idbKeyval.clear();
+    await Promise.all(data.map(manifest => idbKeyval.set(manifest.key_path, manifest)));
+  }
+
+  console.log('cache refreshed');
+}
+
+export async function redirectToLogin() {
+  await self.clients.claim();
+  const clients = await self.clients.matchAll({ type: 'window' });
+  clients.forEach((client) => {
+    if ('navigate' in client) {
+      client.navigate(urls.LOGIN);
+    }
+  });
+}
--- services/editor/service-worker/src/index.js
+++ services/editor/service-worker/src/index.js
@@ -1,5 +1,6 @@
 import idbKeyval from 'idb-keyval';
 import { CACHE_NAME, notificationTypes, urls } from './constants';
+import { testLogin, refresh, redirectToLogin } from './actions';
 import getUrl from './getUrl';
 
 self.importScripts('/socket.io/socket.io.js');
@@ -14,64 +15,6 @@ const replaceUrls = [
     get: match => idbKeyval.get(match[1]),
   },
 ];
-
-let isLoggedIn = true;
-
-async function testLogin(request, shouldLoadCache = true) {
-  const response = await fetch(request);
-
-  const wasLoggedIn = isLoggedIn;
-  isLoggedIn = response.status !== 403;
-
-  if (!isLoggedIn) {
-    if (Notification.permission === 'granted') {
-      self.registration.showNotification('Login expired\nPlease log in again', {
-        icon: '/tweek.png',
-        requireInteraction: true,
-        tag: notificationTypes.LOGIN,
-      });
-    }
-  } else {
-    const loginNotifications = await self.registration.getNotifications({
-      tag: notificationTypes.LOGIN,
-    });
-    loginNotifications.forEach(notification => notification.close());
-    if (!wasLoggedIn && shouldLoadCache) {
-      refresh();
-    }
-  }
-
-  return response;
-}
-
-async function refresh() {
-  console.log('refreshing cache...');
-
-  const testLoginResponse = await testLogin(urls.IS_LOGGED_IN, false);
-  if (!testLoginResponse.ok) return;
-
-  const cache = await caches.open(CACHE_NAME);
-  await cache.addAll(urls.CACHE);
-
-  const localStorage = await fetch(urls.LOCAL_STORAGE);
-  if (localStorage.ok) {
-    const data = await localStorage.json();
-    idbKeyval.clear();
-    await Promise.all(data.map(manifest => idbKeyval.set(manifest.key_path, manifest)));
-  }
-
-  console.log('cache refreshed');
-}
-
-async function redirectToLogin() {
-  await self.clients.claim();
-  const clients = await self.clients.matchAll({ type: 'window' });
-  clients.forEach((client) => {
-    if ('navigate' in client) {
-      client.navigate(urls.LOGIN);
-    }
-  });
-}
 
 async function install() {
   const socket = io(self.origin, { jsonp: false });
