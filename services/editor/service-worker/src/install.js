b5afc1e72cb2d2c9ef095ad9da2d529afc3f2123
YshayY <yshayy@gmail.com>
2017-06-12 18:45:30 +0300

    fix refresh activation

--- services/editor/service-worker/src/install.js
+++ services/editor/service-worker/src/install.js
@@ -1,18 +1,3 @@
-import { refresh } from './data-actions';
-
 export default async function install() {
-  const socket = io(self.origin, { jsonp: false });
-  socket.on('connect', () => console.log('connected to socket'));
-  socket.on('refresh', () => {
-    console.log('refreshing cache...');
-    refresh().catch(error => console.error('error while refreshing cache', error));
-  });
-
-  try {
-    await refresh();
-  } catch (error) {
-    console.error('error while loading cache', error);
-  }
-
   self.skipWaiting();
 }
