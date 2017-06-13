527e88dff86a8e1fb5d8f3c0047f2808f0e6cfb1
Michael Kruglos <MichaelK@soluto.com>
2017-06-13 17:01:13 +0300

    Reduced unnecessary allocations in redis driver

--- addons/Context/Tweek.Drivers.Redis/RedisDriver.cs
+++ addons/Context/Tweek.Drivers.Redis/RedisDriver.cs
@@ -17,7 +17,7 @@ namespace Tweek.Drivers.Redis
     public class RedisDriver : IContextDriver
     {
         private readonly ConnectionMultiplexer mRedisConnection;
-
+        private JsonSerializerSettings JSON_SERIALIZER = new JsonSerializerSettings { ContractResolver = new TweekContractResolver() };
         public string GetKey(Identity identity) => "identity_" + identity.Type + "_" + identity.Id;
 
         public RedisDriver(string connectionString)
@@ -53,7 +53,7 @@ namespace Tweek.Drivers.Redis
             var db = mRedisConnection.GetDatabase();
             var id = GetKey(identity);
             HashEntry[] redisContext = context.Select(item =>
-                new HashEntry(item.Key, JsonConvert.SerializeObject(item.Value, new JsonSerializerSettings{ContractResolver = new TweekContractResolver()}))
+                new HashEntry(item.Key, JsonConvert.SerializeObject(item.Value, JSON_SERIALIZER))
             ).ToArray();
 
             await db.HashSetAsync(id, redisContext);
