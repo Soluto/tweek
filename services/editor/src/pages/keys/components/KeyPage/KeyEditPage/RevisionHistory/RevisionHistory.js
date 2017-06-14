ed1e491a1b32b442e2b223b6d5e72344b41d7900
Nataly Shrits <nataly@soluto.com>
2017-06-14 10:28:50 +0300

    sort revision history by date

--- services/editor/src/pages/keys/components/KeyPage/KeyEditPage/RevisionHistory/RevisionHistory.js
+++ services/editor/src/pages/keys/components/KeyPage/KeyEditPage/RevisionHistory/RevisionHistory.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import moment from 'moment';
+import R from 'ramda';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
+import './RevisionHistory.css';
+
+const byDate = R.descend(R.prop('date'));
 
 const formatDate = date => `${moment(date).calendar(null, { sameElse: 'DD/MM/YYYY [at] HH:mm' })}`;
 
 const RevisionHistory = ({ revisionHistory, goToRevision, selectedKey, revision }) =>
-  <div>
-    <select value={revision} onChange={e => goToRevision(selectedKey, e.target.value)}>
-      {revisionHistory.map(item =>
-        <option key={item.sha} value={item.sha}>
-          {`${formatDate(item.date)} : ${item.author}`}
-        </option>,
-      )}
-    </select>
-  </div>;
+  <select
+    className="revision-history"
+    value={revision}
+    onChange={e => goToRevision(selectedKey, e.target.value)}
+  >
+    {R.sort(byDate, revisionHistory).map(item =>
+      <option key={item.sha} value={item.sha}>
+        {`${formatDate(item.date)} : ${item.author}`}
+      </option>,
+    )}
+  </select>;
 
 const goToRevision = (key, sha) =>
   push({
