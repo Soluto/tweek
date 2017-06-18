8f6397101a565ba7a99a83083ec59176499fca54
Nataly Shrits <nataly@soluto.com>
2017-06-18 12:16:09 +0300

    archive e2e tests tests

--- e2e/spec/keys/deleteKey.js
+++ e2e/spec/keys/deleteKey.js
@@ -1,5 +1,6 @@
 /* global describe, before, beforeEach, after, afterEach, it, browser */
 
+import assert from 'assert';
 import KeysAsserts from '../../KeysAsserts';
 import PageAsserts from '../../PageAsserts';
 import KeysPageObject from '../../utils/KeysPageObject';
@@ -23,35 +24,62 @@ describe('delete key', () => {
     keysPageObject.addEmptyKey(keyToDeleteFullPath);
   });
 
-  it('should not delete key if alert was not accepted', () => {
-    keysPageObject.goToKey(keyToDeleteFullPath);
+  describe('archive', () => {
 
-    browser.click(keySelectors.DELETE_KEY_BUTTON);
-    browser.waitForVisible(keySelectors.ALERT_CANCEL_BUTTON, 1000);
-    browser.leftClick(globalSelectors.ALERT_BACKGROUND, -200, -200);
-    pageAsserts.assertIsInPage(`${KeysPageObject.KEYS_PAGE_URL}/${keyToDeleteFullPath}`, 'should still be in key page');
-    keysAsserts.assertIsKeyExistsAfterTransaction(keyToDeleteFullPath, true, 'key should exist after cancel delete');
   });
 
-  it('should not delete key if alert was canceled', () => {
-    keysPageObject.goToKey(keyToDeleteFullPath);
+  describe('restore', () => {
+    beforeEach(() => {
+      keysPageObject.goToKey(keyToDeleteFullPath);
+      browser.click(keySelectors.ARCHIVE_KEY_BUTTON);
+      browser.waitForEnabled(keySelectors.RESTORE_KEY_BUTTON, 5000);
+    });
 
-    browser.click(keySelectors.DELETE_KEY_BUTTON);
-    browser.waitForVisible(keySelectors.ALERT_CANCEL_BUTTON, 1000);
-    browser.click(keySelectors.ALERT_CANCEL_BUTTON);
-    pageAsserts.assertIsInPage(`${KeysPageObject.KEYS_PAGE_URL}/${keyToDeleteFullPath}`, 'should still be in key page');
-    keysAsserts.assertIsKeyExistsAfterTransaction(keyToDeleteFullPath, true, 'key should exist after cancel delete');
+    it('should restore key', () => {
+      browser.click(keySelectors.RESTORE_KEY_BUTTON);
+
+      assert(!browser.isVisible(keySelectors.READONLY_KEY_MESSAGE), 'should not show key is readonly message');
+      
+
+      browser.waitUntil(() => browser.getText(keySelectors.KEY_DISPLAY_NAME) === testKeyFullPath, 5000);
+
+      browser.waitForEnabled(keySelectors.ARCHIVE_KEY_BUTTON, 5000);
+    })
   });
 
-  it('should succeed deleting key', () => {
-    keysPageObject.goToKey(keyToDeleteFullPath);
+  describe('delete', () => {
+    beforeEach(() => {
+      keysPageObject.goToKey(keyToDeleteFullPath);
+      browser.click(keySelectors.ARCHIVE_KEY_BUTTON);
+      browser.waitForEnabled(keySelectors.DELETE_KEY_BUTTON, 5000);
+    });
 
-    browser.click(keySelectors.DELETE_KEY_BUTTON);
-    browser.waitForVisible(keySelectors.ALERT_CANCEL_BUTTON, 1000);
-    keysPageObject.acceptRodalIfRaised();
+    it('should not delete key if alert was not accepted', () => {
+      browser.click(keySelectors.DELETE_KEY_BUTTON);
+      browser.waitForVisible(keySelectors.ALERT_CANCEL_BUTTON, 1000);
+      browser.leftClick(globalSelectors.ALERT_BACKGROUND, -200, -200);
 
-    pageAsserts.assertIsInPage(KeysPageObject.KEYS_PAGE_URL, 'should moves to keys page url');
-    keysAsserts.assertIsKeyExistsAfterTransaction(keyToDeleteFullPath, false, 'key should not exist after delete');
+      pageAsserts.assertIsInPage(`${KeysPageObject.KEYS_PAGE_URL}/${keyToDeleteFullPath}`, 'should still be in key page');
+      keysAsserts.assertIsKeyExistsAfterTransaction(keyToDeleteFullPath, true, 'key should exist after cancel delete');
+    });
+
+    it('should not delete key if alert was canceled', () => {
+      browser.click(keySelectors.DELETE_KEY_BUTTON);
+      browser.waitForVisible(keySelectors.ALERT_CANCEL_BUTTON, 1000);
+      browser.click(keySelectors.ALERT_CANCEL_BUTTON);
+
+      pageAsserts.assertIsInPage(`${KeysPageObject.KEYS_PAGE_URL}/${keyToDeleteFullPath}`, 'should still be in key page');
+      keysAsserts.assertIsKeyExistsAfterTransaction(keyToDeleteFullPath, true, 'key should exist after cancel delete');
+    });
+
+    it('should succeed deleting key', () => {
+      browser.click(keySelectors.DELETE_KEY_BUTTON);
+      browser.waitForVisible(keySelectors.ALERT_CANCEL_BUTTON, 1000);
+      keysPageObject.acceptRodalIfRaised();
+
+      pageAsserts.assertIsInPage(KeysPageObject.KEYS_PAGE_URL, 'should moves to keys page url');
+      keysAsserts.assertIsKeyExistsAfterTransaction(keyToDeleteFullPath, false, 'key should not exist after delete');
+    });
   });
 });
 
