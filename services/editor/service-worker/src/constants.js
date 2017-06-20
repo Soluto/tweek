a5e64406b66f18ee3b91a59e96f4db7faebe80d5
Nataly Shrits <nataly@soluto.com>
2017-06-20 15:54:40 +0300

    check revision when refreshing

--- services/editor/service-worker/src/constants.js
+++ services/editor/service-worker/src/constants.js
@@ -9,4 +9,5 @@ export const urls = {
   CACHE: ['/api/types', '/api/context-schema', '/api/tags'],
   MANIFESTS: '/api/manifests',
   SEARCH_INDEX: '/api/search-index',
+  REVISION: '/api/revision',
 };
