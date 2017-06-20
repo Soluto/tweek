af933244d921737069bac50861f8946251f9e59a
Michael Kruglos <michaelk@soluto.com>
2017-06-20 15:59:19 +0300

    Fixes after review

---
+++ services/api/Tweek.ApiService.NetCore/Security/CorsExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LanguageExt;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tweek.ApiService.NetCore.Security
+{
+    public static class CorsExtensions
+    {
+        public static void SetupCors(this IServiceCollection services, IConfiguration configuration)
+        {
+            if (!configuration.GetValue<bool>("CorsEnabled")) return;
+
+            var corsPolicies = configuration.GetSection("CorsPolicies");
+            if (corsPolicies.GetChildren().Length() == 0)
+            {
+                services.AddCors(options => options.AddPolicy("Keys",
+                    builder => builder
+                        .AllowCredentials()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowAnyOrigin()
+                ));
+            }
+            else
+            {
+                foreach (var policy in corsPolicies.GetChildren())
+                {
+                    Console.WriteLine($"CORS POLICY {policy.Key}");
+                    services.AddCors(options => options.AddPolicy(policy.Key, builder =>
+                        {
+                            builder.WithHeaders(policy.GetSection("Headers")?.Value?.Split(',') ?? new[] {""})
+                                .WithMethods(policy.GetSection("Methods")?.Value?.Split(',') ?? new[] {""})
+                                .WithOrigins(policy.GetSection("Origins")?.Value?.Split(',') ?? new[] {""})
+                                .WithExposedHeaders(
+                                    policy.GetSection("ExposedHeaders")?.Value?.Split(',') ?? new[] {""});
+
+                            if (policy.GetValue<bool>("AllowCredentials"))
+                            {
+                                builder.AllowCredentials();
+                            }
+                            else
+                            {
+                                builder.DisallowCredentials();
+                            }
+
+                            var maxPreflightAge = policy.GetValue<double>("MaxPreflightAge");
+                            if (maxPreflightAge != 0)
+                            {
+                                builder.SetPreflightMaxAge(TimeSpan.FromSeconds(maxPreflightAge));
+                            }
+                        }
+                    ));
+                }
+            }
+        }
+    }
+}
