f7018ce798e9d6170280fd78c7aac727e077fc80
Michael Kruglos <michaelk@soluto.com>
2017-06-21 15:33:41 +0300

    Fixes for Cors configuration implementation

--- services/api/Tweek.ApiService.NetCore/Security/CorsExtensions.cs
+++ services/api/Tweek.ApiService.NetCore/Security/CorsExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LanguageExt;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,14 +11,16 @@ namespace Tweek.ApiService.NetCore.Security
 {
     public static class CorsExtensions
     {
+        public const string DEFAULT_POLICY_NAME = "Keys";
+
         public static void SetupCors(this IServiceCollection services, IConfiguration configuration)
         {
             if (!configuration.GetValue<bool>("CorsEnabled")) return;
 
             var corsPolicies = configuration.GetSection("CorsPolicies");
-            if (corsPolicies.GetChildren().Length() == 0)
+            if (corsPolicies.GetChildren().All(section => section.Key != DEFAULT_POLICY_NAME))
             {
-                services.AddCors(options => options.AddPolicy("Keys",
+                services.AddCors(options => options.AddPolicy(DEFAULT_POLICY_NAME,
                     builder => builder
                         .AllowCredentials()
                         .AllowAnyHeader()
@@ -25,36 +28,42 @@ namespace Tweek.ApiService.NetCore.Security
                         .AllowAnyOrigin()
                 ));
             }
-            else
+
+            foreach (var policyConfiguration in corsPolicies.GetChildren())
             {
-                foreach (var policy in corsPolicies.GetChildren())
+                var headers = policyConfiguration.GetSection("Headers").Value?.Split(',');
+                var methods = policyConfiguration.GetSection("Methods").Value?.Split(',');
+                var origins = policyConfiguration.GetSection("Origins").Value?.Split(',');
+                var exposedHeaders = policyConfiguration.GetSection("ExposedHeaders").Value?.Split(',');
+
+                if (headers == null || methods == null || origins == null || exposedHeaders == null)
                 {
-                    Console.WriteLine($"CORS POLICY {policy.Key}");
-                    services.AddCors(options => options.AddPolicy(policy.Key, builder =>
-                        {
-                            builder.WithHeaders(policy.GetSection("Headers")?.Value?.Split(',') ?? new[] {""})
-                                .WithMethods(policy.GetSection("Methods")?.Value?.Split(',') ?? new[] {""})
-                                .WithOrigins(policy.GetSection("Origins")?.Value?.Split(',') ?? new[] {""})
-                                .WithExposedHeaders(
-                                    policy.GetSection("ExposedHeaders")?.Value?.Split(',') ?? new[] {""});
-
-                            if (policy.GetValue<bool>("AllowCredentials"))
-                            {
-                                builder.AllowCredentials();
-                            }
-                            else
-                            {
-                                builder.DisallowCredentials();
-                            }
-
-                            var maxPreflightAge = policy.GetValue<double>("MaxPreflightAge");
-                            if (maxPreflightAge != 0)
-                            {
-                                builder.SetPreflightMaxAge(TimeSpan.FromSeconds(maxPreflightAge));
-                            }
-                        }
-                    ));
+                    throw new InvalidOperationException(
+                        $"Missing configuration fields for {policyConfiguration.Key}, please specify Headers, Methods, Origins and ExposedHeaders");
                 }
+
+                var policyBuilder = new CorsPolicyBuilder(origins);
+                policyBuilder
+                    .WithHeaders(headers)
+                    .WithMethods(methods)
+                    .WithExposedHeaders(exposedHeaders);
+
+                if (policyConfiguration.GetValue<bool>("AllowCredentials"))
+                {
+                    policyBuilder.AllowCredentials();
+                }
+                else
+                {
+                    policyBuilder.DisallowCredentials();
+                }
+
+                var maxPreflightAge = policyConfiguration.GetValue<double>("MaxPreflightAge");
+                if (maxPreflightAge != 0)
+                {
+                    policyBuilder.SetPreflightMaxAge(TimeSpan.FromSeconds(maxPreflightAge));
+                }
+
+                services.AddCors(options => options.AddPolicy(policyConfiguration.Key, policyBuilder.Build()));
             }
         }
     }
