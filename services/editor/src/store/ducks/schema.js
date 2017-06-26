ab7c1632ecc015b6f7a4dba4cc47a6386a9c0f30
YshayY <yshayy@gmail.com>
2017-06-26 12:10:46 +0300

    update schema is working

--- services/editor/src/store/ducks/schema.js
+++ services/editor/src/store/ducks/schema.js
@@ -1,12 +1,15 @@
 import { handleActions } from 'redux-actions';
-import { getSchema } from '../../services/context-service';
+import { getSchema, refreshSchema } from '../../services/context-service';
 import R from 'ramda';
 import jsondiffpatch from 'jsondiffpatch';
 import { withJsonData } from '../../utils/http';
 
 const SCHEMA_LOADED = 'SCHEMA_LOADED';
-const UPDATE_SCHEMA_PROPERTY = 'SCHEMA_UPDATE_PROPERTY';
+const UPSERT_SCHEMA_PROPERTY = 'SCHEMA_UPSERT_PROPERTY';
+const REMOVE_SCHEMA_PROPERTY = 'SCHEMA_REMOVE_PROPERTY';
 const IDENTITY_SELECTED = 'IDENTITY_SELECTED';
+const SCHEMA_SAVED = 'SCHEMA_SAVED';
+const SAVING_SCHEMA = 'SAVING_SCHEMA';
 
 export function loadSchema() {
   return { type: SCHEMA_LOADED };
@@ -16,18 +19,23 @@ export function saveSchema(identityType) {
   return async (dispatch, getState) => {
     let identityState = getState().schema[identityType];
     let patch = jsondiffpatch.diff(identityState.remote, identityState.local);
-    await dispatch(
-      fetch(`/api/schema/${identityType}`, {
-        credentials: 'same-origin',
-        method: 'PATCH',
-        ...withJsonData(patch),
-      }),
-    );
+    dispatch({ type: SAVING_SCHEMA, value: { identity: identityType } });
+    await fetch(`/api/schema/${identityType}`, {
+      credentials: 'same-origin',
+      method: 'PATCH',
+      ...withJsonData(patch),
+    });
+    dispatch({ type: SCHEMA_SAVED, value: { identity: identityType } });
+    await refreshSchema();
   };
 }
 
-export function updateIdentityProperty(identity, prop, value) {
-  return { type: UPDATE_SCHEMA_PROPERTY, value: { identity, prop, value } };
+export function removeIdentityProperty(identity, prop) {
+  return { type: REMOVE_SCHEMA_PROPERTY, value: { identity, prop } };
+}
+
+export function upsertIdentityProperty(identity, prop, value) {
+  return { type: UPSERT_SCHEMA_PROPERTY, value: { identity, prop, value } };
 }
 
 function createRemoteAndLocalStates(state) {
@@ -42,13 +50,17 @@ export default handleActions(
   {
     [SCHEMA_LOADED]: (state, action) => createSchemaState(),
     [IDENTITY_SELECTED]: (state, action) => ({ selectedIdentity: action.identity, ...state }),
-    [UPDATE_SCHEMA_PROPERTY]: (state, { value: { identity, prop, value } }) => ({
-      ...state,
-      [identity]: {
-        ...state[identity],
-        local: { ...state[identity].local, [prop]: value },
-      },
-    }),
+    [REMOVE_SCHEMA_PROPERTY]: (state, { value: { identity, prop } }) =>
+      R.dissocPath([identity, 'local', prop])(state),
+    [UPSERT_SCHEMA_PROPERTY]: (state, { value: { identity, prop, value } }) =>
+      R.assocPath([identity, 'local', prop], value)(state),
+    [SCHEMA_SAVED]: (state, { value: { identity } }) =>
+      R.pipe(
+        R.assocPath([identity, 'remote'], state[identity].local),
+        R.assocPath([identity, 'isSaving'], false),
+      )(state),
+    [SAVING_SCHEMA]: (state, { value: { identity } }) =>
+      R.assocPath([identity, 'isSaving'], true)(state),
   },
   [],
 );
