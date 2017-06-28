846b08941ff62f596dc08c0ff6dbfcb8d8fb3adb
Nataly Shrits <nataly@soluto.com>
2017-06-28 15:47:18 +0300

    fix tests

--- e2e/selectors/globalSelectors.js
+++ e2e/selectors/globalSelectors.js
@@ -1,19 +1,9 @@
-import { getRelativeSelector, getSelectorByClassNames, _getSelectorByIndex } from './selectorUtils';
+import { getSelectorByClassNames } from './selectorUtils';
 
 const globalSelectors = {};
 
 globalSelectors.BACKGROUND = getSelectorByClassNames('header');
 globalSelectors.ALERT_BACKGROUND = '.rodal-mask';
 globalSelectors.ERROR_NOTIFICATION_TITLE = '.notifications-br .notification-error .notification-title';
-
-globalSelectors.typeaheadSuggestionByIndex = (suggestionIndex) => {
-  const selector =
-    getRelativeSelector([
-      getSelectorByClassNames('bootstrap-typeahead-menu'),
-      _getSelectorByIndex('li', suggestionIndex),
-    ]);
-
-  return selector;
-};
 
 export default globalSelectors;
