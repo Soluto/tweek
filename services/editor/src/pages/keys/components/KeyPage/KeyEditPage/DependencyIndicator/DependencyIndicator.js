2088fbe5b0ab1c3c52c01ca402a9ae0ee701d3c8
Michael Kruglos <MichaelK@soluto.com>
2017-06-28 14:16:16 +0300

    Improved styling of the DependencyIndicator

--- services/editor/src/pages/keys/components/KeyPage/KeyEditPage/DependencyIndicator/DependencyIndicator.js
+++ services/editor/src/pages/keys/components/KeyPage/KeyEditPage/DependencyIndicator/DependencyIndicator.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import classNames from 'classnames';
 import wrapComponentWithClass from '../../../../../../hoc/wrap-component-with-class';
+import './DependencyIndicator.css';
 
 const DependsOn = ({ dependencies }) =>
   dependencies && dependencies.length !== 0
-    ? <div>depends on: {dependencies.map(dep => <div>{dep}</div>)}</div>
+    ? <div className={classNames('depends-on')} data-comp={'depends-on'}>
+        Depends on:<br /><ul>{dependencies.map(dep => <li key={dep}>{dep}</li>)}</ul>
+      </div>
     : <div />;
 
 const UsedBy = ({ dependentKeys }) =>
   dependentKeys && dependentKeys.length !== 0
-    ? <div>used by: <ul>{dependentKeys.map(dep => <li key={dep}>{dep}</li>)}</ul></div>
+    ? <div className={classNames('depends-on')} data-comp={'depends-on'}>
+        Used by:<br /><ul>{dependentKeys.map(dep => <li key={dep}>{dep}</li>)}</ul>
+      </div>
     : <div />;
 
 const DependencyIndicator = ({ manifest, dependentKeys }) =>
-  <div>
+  <div className={classNames('dependency-indicator-container')}>
     <DependsOn {...manifest} />
     <UsedBy dependentKeys={dependentKeys} />
   </div>;
