704ff682c160c20fbf5f138a557b0599e6d3f023
Nataly Shrits <nataly@soluto.com>
2017-07-03 16:37:46 +0300

    manifests promise will not wait for search-index to finish

--- services/editor/server/api/search.js
+++ services/editor/server/api/search.js
@@ -2,14 +2,14 @@ import performSearch from '../common/perform-search';
 import searchIndex from '../searchIndex';
 
 export async function getSearchIndex(req, res) {
-  res.json(await searchIndex.index);
+  res.json(await searchIndex.indexPromise);
 }
 
-const createSearchEndpoint = field => async (req, res) => {
+const createSearchEndpoint = field => (req, res) => {
   const result = performSearch(req.query.q, {
     maxResults: req.query.count,
     field,
-    index: await searchIndex.index,
+    index: searchIndex.index,
   });
   res.json(result);
 };
