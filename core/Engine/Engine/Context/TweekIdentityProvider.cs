8e3a97bbd0003ae3c63272673c7ae9a9c3c8cecf
Nataly Shrits <nataly@soluto.com>
2017-07-05 15:02:40 +0300

    added TweekIdentityProvider

---
+++ core/Engine/Engine/Context/TweekIdentityProvider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Engine.DataTypes;
+using Engine.Drivers.Rules;
+
+namespace Engine.Context
+{
+    public class TweekIdentityProvider
+    {
+        private static readonly Regex IdentityRegex = new Regex(@"^@tweek\/context\/([^\/]+)", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        private HashSet<string> _identities;
+        private TweekIdentityProvider(IRulesDriver driver, HashSet<string> initialIdentities)
+        {
+            _identities = initialIdentities;
+
+            driver.OnRulesChange += (newRules) =>
+            {
+                _identities = GetIdentities(newRules.Keys);
+            };
+        }
+
+        public HashSet<string> GetIdentities() => _identities;
+
+        public static async Task<TweekIdentityProvider> Create(IRulesDriver driver)
+        {
+            var allRules = await driver.GetAllRules();
+            var initialIdentities = GetIdentities(allRules.Keys);
+            return new TweekIdentityProvider(driver, initialIdentities);
+        }
+
+        private static HashSet<string> GetIdentities(IEnumerable<string> keys)
+        {
+            var identities = keys.Select(key => IdentityRegex.Match(key))
+                .Select(x => x.Groups[1])
+                .Where(x => x.Success)
+                .Select(x => x.Value);
+
+            return new HashSet<string>(identities) {Identity.GlobalIdentityType};
+        }
+    }
+}
