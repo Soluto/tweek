fb7f391de26c0baf24325063c97a437abf7db283
Nataly Shrits <nataly@soluto.com>
2017-07-06 11:49:05 +0300

    remove logzio dependency

--- services/management/src/logger.js
+++ services/management/src/logger.js
@@ -1,16 +1,10 @@
 const winston = require('winston');
-// const LogzIO = require('winston-logzio');
 
 const logger = new winston.Logger({
   exitOnError: false,
 });
 
 logger.add(winston.transports.Console);
-/*
-logger.add(LogzIO, {
-    token: 'wsEabfWJZpXkwVqUVrVNvhSvUnnYtzbg',
-    type: 'tweek-management'
-});*/
 
 process.on('uncaughtException', (err) => {
   logger.error(err);
