b9c52e298d30ee3c50f6d3f6ba5078a66025349b
YshayY <yshayy@gmail.com>
2017-07-19 10:15:53 +0300

    add telemetry initializer for adding x-api-client

--- addons/ApplicationInsights/Tweek.Addons.ApplicationInsights/ApplicationInsightAddon.cs
+++ addons/ApplicationInsights/Tweek.Addons.ApplicationInsights/ApplicationInsightAddon.cs
@@ -1,15 +1,38 @@
 ﻿using System;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Tweek.ApiService.Addons;
 
 namespace Tweek.Addons.ApplicationInsights
 {
+    public class ApiClientTelemetryInitializer : ITelemetryInitializer
+    {
+        IHttpContextAccessor httpContextAccessor;
+
+        public ApiClientTelemetryInitializer(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (httpContextAccessor.HttpContext.Request.Query.TryGetValue("x-api-client", out var values)){
+                telemetry.Context.Properties["x-api-client"] = values.ToString();
+            }
+        }
+    }
+
     public class ApplicationInsightsAddon : ITweekAddon
     {
         public void Use(IApplicationBuilder builder, IConfiguration configuration)
         {
+            var initializer = new ApiClientTelemetryInitializer(builder.ApplicationServices.GetService<IHttpContextAccessor>());
+            var telemetryConfiguration= builder.ApplicationServices.GetService<TelemetryConfiguration>();
+            telemetryConfiguration.TelemetryInitializers.Add(initializer);
         }
 
         public void Configure(IServiceCollection services, IConfiguration configuration)
