3bfb20d107825046ca8958d63f38d03517adf027
Asaf Kotzer <asafkotzer@gmail.com>
2017-07-20 09:53:24 +0300

    Updated AI property name

--- addons/ApplicationInsights/Tweek.Addons.ApplicationInsights/ApiClientTelemetryInitializer.cs
+++ addons/ApplicationInsights/Tweek.Addons.ApplicationInsights/ApiClientTelemetryInitializer.cs
@@ -15,7 +15,7 @@ namespace Tweek.Addons.ApplicationInsights
         public void Initialize(ITelemetry telemetry)
         {
             if (httpContextAccessor.HttpContext?.Request?.Headers.TryGetValue("x-api-client", out var apiClient) ?? false){
-                telemetry.Context.Properties["x-api-client"] = apiClient.ToString();
+                telemetry.Context.Properties["ApiClientId"] = apiClient.ToString();
             }
         }
     }
