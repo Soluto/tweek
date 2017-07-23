a8cf83d5471c5ab4b565016eba9857823bdc9eac
Nataly Shrits <nataly@soluto.com>
2017-07-23 11:33:25 +0300

    include author

--- services/authoring/src/routes/index.js
+++ services/authoring/src/routes/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const { compose } = require('ramda');
 const requestErrorHandlingWrapper = require('../utils/request-error-handling-wrapper');
-
+const includeAuthor = require('../utils/include-author');
 const SearchRoutes = require('./search');
 
 function configureRoutes(config) {
   const app = express();
 
-  const addConfig = fn => requestErrorHandlingWrapper((req, res) => fn(req, res, config));
+  function addConfig(fn, isAuthorRequired = false) {
+    const wrapRequest = compose(
+      requestErrorHandlingWrapper,
+      fn => (req, res) => fn(req, res, config),
+      includeAuthor(isAuthorRequired),
+    );
+
+    return wrapRequest(fn);
+  }
 
   app.get('/search-index', addConfig(SearchRoutes.getSearchIndex));
   app.get('/search', addConfig(SearchRoutes.search));
