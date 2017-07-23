d20179666ab677fa9ba053b1c3b3e7c89e207d19
Nataly Shrits <nataly@soluto.com>
2017-07-23 11:50:17 +0300

    add keys routes

--- services/authoring/src/routes/index.js
+++ services/authoring/src/routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { compose } = require('ramda');
 const requestErrorHandlingWrapper = require('../utils/request-error-handling-wrapper');
 const includeAuthor = require('../utils/include-author');
+
+const KeysRoutes = require('./keys');
 const SearchRoutes = require('./search');
 
 function configureRoutes(config) {
@@ -16,6 +18,20 @@ function configureRoutes(config) {
 
     return wrapRequest(fn);
   }
+
+  app.get('/keys', addConfig(KeysRoutes.getAllKeys));
+  app
+    .route('/keys/*')
+    .get(addConfig(KeysRoutes.getKey))
+    .put(addConfig(KeysRoutes.updateKey, true))
+    .delete(addConfig(KeysRoutes.deleteKey, true));
+
+  app.get('/revision', addConfig(KeysRoutes.getRevision));
+  app.get('/revision-history/*', addConfig(KeysRoutes.getKeyRevisionHistory));
+
+  app.get('/manifests', addConfig(KeysRoutes.getAllManifests));
+  app.get('/manifests/*', addConfig(KeysRoutes.getManifest));
+  app.get('/dependents/*', addConfig(KeysRoutes.getDependents));
 
   app.get('/search-index', addConfig(SearchRoutes.getSearchIndex));
   app.get('/search', addConfig(SearchRoutes.search));
