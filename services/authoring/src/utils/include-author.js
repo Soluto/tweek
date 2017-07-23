ccdbf3e96f7c69f5544c1e2e3aee89998770f7ad
Nataly Shrits <nataly@soluto.com>
2017-07-23 13:19:15 +0300

    add schema and tags routes

--- services/authoring/src/utils/include-author.js
+++ services/authoring/src/utils/include-author.js
@@ -1,17 +1,15 @@
-function includeAuthor(isAuthorRequired) {
-  return function (handler) {
-    return (req, res, config = {}) => {
-      const { user, email } = req.query;
+function includeAuthor(handler) {
+  return (req, res, config = {}) => {
+    const { user, email } = req.query;
 
-      if (isAuthorRequired && (!user || !email)) {
-        res.status(400).send("Missing user and/or email");
-        return;
-      }
+    if (req.method.toLowerCase() !== 'get' && (!user || !email)) {
+      res.status(400).send("Missing user and/or email");
+      return;
+    }
 
-      const author = { user: user || 'unknown', email: email || 'unknown@tweek.com' };
+    const author = { user: user || 'unknown', email: email || 'unknown@tweek.com' };
 
-      return handler(req, res, Object.assign({}, config, { author }));
-    };
+    return handler(req, res, Object.assign({ author }, config));
   };
 }
 
