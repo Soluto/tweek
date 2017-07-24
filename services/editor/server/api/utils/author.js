92ec6b1a313fbebeaba5ab28de82216d15026960
YshayY <yshayy@gmail.com>
2017-07-24 17:49:32 +0300

    fix author params

--- services/editor/server/api/utils/author.js
+++ services/editor/server/api/utils/author.js
@@ -1,14 +1,14 @@
 export const UKNOWN_AUTHOR = {
-  name: 'unknown',
-  email: 'unknown@tweek.com',
+  "author.name": 'unknown',
+  "author.email": 'unknown@tweek.com',
 };
 
 export function getAuthor(req) {
   return (
     (req.user &&
     req.user.email && {
-      name: req.user.displayName || req.user.email,
-      email: req.user.email,
+      "author.name": req.user.displayName || req.user.email,
+      "author.email": req.user.email,
     }) ||
     UKNOWN_AUTHOR
   );
