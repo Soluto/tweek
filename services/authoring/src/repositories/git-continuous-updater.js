cf14dbd3fac1ad2c1b3346eedb26815129a5573b
Nataly Shrits <nataly@soluto.com>
2017-07-24 13:43:36 +0300

    improve git continuous updater

--- services/authoring/src/repositories/git-continuous-updater.js
+++ services/authoring/src/repositories/git-continuous-updater.js
@@ -1,17 +1,18 @@
-const Rx = require('rxjs');
+const { Observable } = require('rxjs');
 const { CONTINUOUS_UPDATER_INTERVAL } = require('../constants');
 
 module.exports = {
-  onUpdate: gitTransactionManager =>
-    Rx.Observable
+  onUpdate(gitTransactionManager) {
+    const updateRepo$ = Observable
       .defer(async () => {
         await gitTransactionManager.read(async gitRepo => await gitRepo.fetch());
         return await gitTransactionManager.write(async gitRepo => await gitRepo.mergeMaster());
-      })
+      });
+
+    return updateRepo$.concat(updateRepo$.delay(CONTINUOUS_UPDATER_INTERVAL).repeat())
       .do((_) => {}, err => console.error('Error pulling changes in git repo', err))
-      .delay(CONTINUOUS_UPDATER_INTERVAL)
-      .retry()
-      .repeat()
+      .retryWhen(Observable.of(1).delay(CONTINUOUS_UPDATER_INTERVAL))
       .distinctUntilChanged()
-      .do(sha => console.log('Updated git repo', sha)),
+      .do(sha => console.log('Updated git repo', sha));
+  },
 };
