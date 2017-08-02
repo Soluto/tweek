749c8dba1ca9cc881cb6e2d60f519224b300f500
Nataly Shrits <nataly87s@gmail.com>
2017-08-02 20:18:16 +0300

    make max search results count configurable (#527)

--- services/editor/src/services/search-service.js
+++ services/editor/src/services/search-service.js
@@ -1,11 +1,22 @@
 /* global fetch */
 
+let maxResults;
+
 const createSearchFunction = endpoint =>
   async function suggestions(query) {
     if (!query || query === '') return [];
-    const response = await fetch(`/api/${endpoint}?q=${encodeURIComponent(query)}`, {
-      credentials: 'same-origin',
-    });
+
+    if (!maxResults) {
+      const response = await fetch('/api/editor-configuration/search/max_results');
+      maxResults = (await response.json()) || 25;
+    }
+
+    const response = await fetch(
+      `/api/${endpoint}?q=${encodeURIComponent(query)}&count=${maxResults}`,
+      {
+        credentials: 'same-origin',
+      },
+    );
     return await response.json();
   };
 
