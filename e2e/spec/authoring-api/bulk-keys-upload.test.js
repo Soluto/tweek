16e3e763d2582b19b28f735ec49d76359d03c0f1
Dvir Shoham <dvir@soluto.com>
2017-08-03 10:54:56 +0300

    Trying to debug codefresh build

--- e2e/spec/authoring-api/bulk-keys-upload.test.js
+++ e2e/spec/authoring-api/bulk-keys-upload.test.js
@@ -10,7 +10,7 @@ nconf.argv().env().defaults({
   TWEEK_API_URL: 'http://localhost:4003',
   GIT_PRIVATE_KEY_PATH: '../services/git-service/ssh/tweekgit'
 });
-console.log("authoring api url ", nconf.get(AUTHORING_URL));
+console.log("authoring api url ", nconf.get('AUTHORING_URL'));
 let should = chai.should();
 const authoringApiRequest = supertest(nconf.get('AUTHORING_URL'));
 const tweekApiRequest = supertest(nconf.get('TWEEK_API_URL'));
