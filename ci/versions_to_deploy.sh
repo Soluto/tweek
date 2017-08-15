719db473822e42a3d7ff4392737fd65c6e2589a2
Yshayy <yshayy@gmail.com>
2017-08-15 19:26:14 +0300

    add exp suffix

--- ci/versions_to_deploy.sh
+++ ci/versions_to_deploy.sh
@@ -4,7 +4,7 @@ TWEEK_API_TAG="tweek-api-$TWEEK_API_VERSION"
 TWEEK_API_SHOULD_UPDATE=$([[ "$(git tag | grep -c $TWEEK_API_TAG)" == "0" ]] && echo "true" || echo "false")
 if [ "$TWEEK_API_SHOULD_UPDATE" == 'true' ]; then
     echo 'tagging $TWEEK_API_TAG'
-    git tag $TWEEK_API_TAG
+    git tag $TWEEK_API_TAG-exp
 fi
 
 echo checking management version
@@ -13,7 +13,7 @@ TWEEK_MANAGEMENT_TAG="tweek-management-$TWEEK_MANAGEMENT_VERSION"
 TWEEK_MANAGEMENT_SHOULD_UPDATE=$([[ "$(git tag | grep -c $TWEEK_MANAGEMENT_TAG)" == "0" ]] && echo "true" || echo "false")
 if [ "$TWEEK_MANAGEMENT_SHOULD_UPDATE" == 'true' ]; then
     echo 'tagging $TWEEK_MANAGEMENT_TAG'
-    git tag $TWEEK_MANAGEMENT_TAG
+    git tag $TWEEK_MANAGEMENT_TAG-exp
 fi
 
 echo checking authoring version
@@ -22,7 +22,7 @@ TWEEK_AUTHORING_TAG="tweek-authoring-$TWEEK_MANAGEMENT_VERSION"
 TWEEK_AUTHORING_SHOULD_UPDATE=$([[ "$(git tag | grep -c $TWEEK_AUTHORING_TAG)" == "0" ]] && echo "true" || echo "false")
 if [ "$TWEEK_AUTHORING_SHOULD_UPDATE" == 'true' ]; then
     echo 'tagging $TWEEK_AUTHORING_TAG'
-    git tag $TWEEK_AUTHORING_TAG
+    git tag $TWEEK_AUTHORING_TAG-exp
 fi
 
 echo  checking editor version
@@ -31,7 +31,7 @@ TWEEK_EDITOR_TAG="tweek-editor-$TWEEK_EDITOR_VERSION"
 TWEEK_EDITOR_SHOULD_UPDATE=$([[ "$(git tag | grep -c $TWEEK_EDITOR_TAG)" == "0" ]] && echo "true" || echo "false")
 if [ "$TWEEK_EDITOR_SHOULD_UPDATE" == 'true' ]; then
     echo 'tagging $TWEEK_EDITOR_TAG'
-    git tag $TWEEK_EDITOR_TAG
+    git tag $TWEEK_EDITOR_TAG-exp
 fi
 
 echo $(set | grep TWEEK)
