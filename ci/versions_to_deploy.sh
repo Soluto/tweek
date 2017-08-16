8afcd57f70eead19316ec20dd8ad449ebfc26c86
Yshayy <yshayy@gmail.com>
2017-08-16 18:08:16 +0300

    fix

--- ci/versions_to_deploy.sh
+++ ci/versions_to_deploy.sh
@@ -5,11 +5,10 @@ TWEEK_API_DOCKER_TAG="latest-exp"
 if [[ "$(git tag | grep -c $TWEEK_API_TAG)" == "0" ]]; then
     TWEEK_API_DOCKER_TAG=$TWEEK_API_VERSION-exp
 fi
-echo $TWEEK_API_DOCKER_TAG
 
 TWEEK_API_SHOULD_UPDATE=$([[ "$(git tag | grep -c $TWEEK_API_TAG)" == "0" ]] && echo "true" || echo "false")
 if [ "$TWEEK_API_SHOULD_UPDATE" == 'true' ]; then
-    echo 'tagging $TWEEK_API_TAG'
+    echo tagging $TWEEK_API_TAG
     git tag $TWEEK_API_TAG-exp
 fi
 
@@ -18,7 +17,7 @@ TWEEK_MANAGEMENT_VERSION=$(cat ./services/management/package.json | grep -E "\"v
 TWEEK_MANAGEMENT_TAG="tweek-management-$TWEEK_MANAGEMENT_VERSION"
 TWEEK_MANAGEMENT_SHOULD_UPDATE=$([[ "$(git tag | grep -c $TWEEK_MANAGEMENT_TAG)" == "0" ]] && echo "true" || echo "false")
 if [ "$TWEEK_MANAGEMENT_SHOULD_UPDATE" == 'true' ]; then
-    echo 'tagging $TWEEK_MANAGEMENT_TAG'
+    echo tagging $TWEEK_MANAGEMENT_TAG
     git tag $TWEEK_MANAGEMENT_TAG-exp
 fi
 
@@ -27,7 +26,7 @@ TWEEK_AUTHORING_VERSION=$(cat ./services/authoring/package.json | grep -E "\"ver
 TWEEK_AUTHORING_TAG="tweek-authoring-$TWEEK_MANAGEMENT_VERSION"
 TWEEK_AUTHORING_SHOULD_UPDATE=$([[ "$(git tag | grep -c $TWEEK_AUTHORING_TAG)" == "0" ]] && echo "true" || echo "false")
 if [ "$TWEEK_AUTHORING_SHOULD_UPDATE" == 'true' ]; then
-    echo 'tagging $TWEEK_AUTHORING_TAG'
+    echo tagging $TWEEK_AUTHORING_TAG
     git tag $TWEEK_AUTHORING_TAG-exp
 fi
 
@@ -36,7 +35,7 @@ TWEEK_EDITOR_VERSION=$(cat ./services/editor/package.json | grep -E "\"version\"
 TWEEK_EDITOR_TAG="tweek-editor-$TWEEK_EDITOR_VERSION"
 TWEEK_EDITOR_SHOULD_UPDATE=$([[ "$(git tag | grep -c $TWEEK_EDITOR_TAG)" == "0" ]] && echo "true" || echo "false")
 if [ "$TWEEK_EDITOR_SHOULD_UPDATE" == 'true' ]; then
-    echo 'tagging $TWEEK_EDITOR_TAG'
+    echo tagging $TWEEK_EDITOR_TAG
     git tag $TWEEK_EDITOR_TAG-exp
 fi
 
