6a24d99a4332e4805234614e59a427d9d2a2641e
Nataly Shrits <nataly87s@gmail.com>
2017-08-24 15:14:50 +0300

    fix ValidationIcon tooltip

--- services/editor/src/components/common/ValidationIcon.js
+++ services/editor/src/components/common/ValidationIcon.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactTooltip from 'react-tooltip';
+import Chance from 'chance';
 import alertIconSrc from '../../resources/alert-icon.svg';
 
-const ValidationIcon = ({ show, hint }) =>
-  <div
-    className="validation-icon-wrapper"
-    data-comp="validation-icon"
-    data-is-shown={show}
-    style={{ opacity: show ? 1 : 0 }}
-  >
-    <img className="validation-icon" data-tip={hint} src={alertIconSrc} />
-    <ReactTooltip disable={!show || !hint} effect="solid" place="top" delayHide={500} />
-  </div>;
+const chance = new Chance();
 
-ValidationIcon.propTypes = {
-  show: PropTypes.bool.isRequired,
-  hint: PropTypes.string,
-};
+export default class ValidationIcon extends Component {
+  static propTypes = {
+    show: PropTypes.bool.isRequired,
+    hint: PropTypes.string,
+  };
 
-export default ValidationIcon;
+  componentWillMount() {
+    this.tooltipId = chance.guid();
+  }
+
+  render() {
+    const { show, hint } = this.props;
+    return (
+      <div
+        className="validation-icon-wrapper"
+        data-comp="validation-icon"
+        data-is-shown={show}
+        style={{ opacity: show ? 1 : 0 }}
+      >
+        <img
+          className="validation-icon"
+          src={alertIconSrc}
+          data-tip={hint}
+          data-for={this.tooltipId}
+        />
+        <ReactTooltip
+          id={this.tooltipId}
+          disable={!show || !hint}
+          effect="solid"
+          place="top"
+          delayHide={500}
+        />
+      </div>
+    );
+  }
+}
