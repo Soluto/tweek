a3791fa1e1c2c30431a0b9f0e6fde9e948f86f07
Michael Kruglos <MichaelK@soluto.com>
2017-08-31 18:17:32 +0300

    Turned authentication back on

--- services/authoring/src/server.ts
+++ services/authoring/src/server.ts
@@ -52,13 +52,7 @@ const keysRepository = new KeysRepository(gitTransactionManager);
 const tagsRepository = new TagsRepository(gitTransactionManager);
 const appsRepository = new AppsRepository(gitTransactionManager);
 
-// const auth = passport.authenticate(['tweek-internal', 'apps-credentials'], { session: false });
-const auth: express.Handler = (req, res, next) => {
-  req.user = { isTweekService: true, name: 'tweek' };
-  let t: any = passport;
-  t = 1;
-  return next();
-};
+const auth = passport.authenticate(['tweek-internal', 'apps-credentials'], { session: false });
 
 async function startServer() {
   await appsRepository.refresh();
