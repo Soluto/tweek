cbf910f6588af53b1f578db53b499626a5b35bc4
Omer Levi Hevroni <omerl@soluto.com>
2017-11-02 18:28:03 +0200

    Fix API findings - ignore all false positive

--- services/api/Tweek.ApiService.SmokeTests/test.sh
+++ services/api/Tweek.ApiService.SmokeTests/test.sh
@@ -6,7 +6,16 @@ set -e
 ./wait-for-it.sh api:80 -t 4000
 ./wait-for-it.sh zap:8090 -t 4000
 
+# Disable X-Content-Type scanner - not relevant for API
+curl --fail http://zap:8090/JSON/pscan/action/disableScanners/?zapapiformat=JSON\&formMethod=GET\&ids=10021
+
+#Disable Cache control scanner - not relevant for this API
+curl --fail http://zap:8090/JSON/pscan/action/disableScanners/?zapapiformat=JSON\&formMethod=GET\&ids=10049
+
 dotnet test
+
+mkdir /usr/src/wrk/
+cp glue.json /usr/src/wrk/
 
 ruby /usr/bin/glue/bin/glue \
   -t zap \
