86169a9a4ba26aff161346cb58cde04dcb52cf35
Nataly Shrits <nataly87s@gmail.com>
2017-11-06 15:45:56 +0200

    revert pollUntil

--- e2e/integration/utils/utils.js
+++ e2e/integration/utils/utils.js
@@ -1,15 +1,12 @@
 const delay = duration => new Promise(resolve => setTimeout(resolve, duration));
 
-module.exports.pollUntil = async (action, assert, timeout = 14000, delayDuration = 0) => {
-  const startTime = new Date();
-  while (startTime > new Date() - timeout) {
+module.exports.pollUntil = async (action, assert, delayDuration = 0) => {
+  while (true) {
     let result = await action();
     try {
       assert(result);
-      return;
+      break;
     } catch (ex) {}
     await delay(delayDuration);
   }
-  let result = await action();
-  assert(result);
 };
