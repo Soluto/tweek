61ccd2f7c4c8bbad02ac9ec60b77867a78e022c2
Nataly Shrits <nataly87s@gmail.com>
2017-11-06 20:23:46 +0200

    make e2e ui tests wait for services

---
+++ e2e/ui/utils/client-utils.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const nconf = require('nconf');
+
+async function waitUntil(action, timeout = 15000, delayDuration = 25) {
+  let shouldStop = false;
+  const timeoutRef = setTimeout(() => (shouldStop = true), timeout);
+  let error;
+  while (!shouldStop) {
+    try {
+      await action();
+      clearTimeout(timeoutRef);
+      return;
+    } catch (ex) {
+      error = ex;
+    }
+    delayDuration && (await delay(delayDuration));
+  }
+  throw error;
+}
+
+function waitForClient(baseURL, timeout = 60000, delayDuration = 1000) {
+  const client = axios.create({ baseURL });
+  return waitUntil(() => client.get('/health'), timeout, delayDuration);
+}
+
+module.exports.waitForAllClients = async function() {
+  const services = [
+    nconf.get('EDITOR_URL'),
+    nconf.get('TWEEK_API_URL'),
+    nconf.get('AUTHORING_URL'),
+  ];
+
+  await Promise.all(services.map(waitForClient));
+};
