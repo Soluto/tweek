408652cc532c1cf500ff79779f4ce00256016fe2
Nataly Shrits <nataly87s@gmail.com>
2017-11-06 10:23:34 +0200

    increase test timeout

--- e2e/integration/utils/utils.js
+++ e2e/integration/utils/utils.js
@@ -1,6 +1,6 @@
 const delay = duration => new Promise(resolve => setTimeout(resolve, duration));
 
-module.exports.pollUntil = async (action, assert, timeout = 10000, delayDuration = 0) => {
+module.exports.pollUntil = async (action, assert, timeout = 14000, delayDuration = 0) => {
   const startTime = new Date();
   while (startTime + timeout > new Date()) {
     let result = await action();
