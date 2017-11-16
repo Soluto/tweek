0d248702eb2d69f8e3acaab7cd39c60097b8a7cc
Nataly Shrits <nataly87s@gmail.com>
2017-11-16 19:14:30 +0200

    add an implementation for RulesDriver

---
+++ core/Engine/Engine.Drivers/Rules/RulesDriver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Engine.Drivers.Rules
+{
+    public class RulesDriver : IRulesDriver, IDisposable
+    {
+        private readonly IDisposable _subscription;
+        private readonly IConnectableObservable<Dictionary<string, RuleDefinition>> _pipeline;
+
+        public RulesDriver(IRulesProvider rulesProvider, TimeSpan failureDelay,
+            ILogger logger = null, IScheduler scheduler = null)
+        {
+            logger = logger ?? NullLogger.Instance;
+            scheduler = scheduler ?? DefaultScheduler.Instance;
+
+            _pipeline = Observable.Defer(rulesProvider.OnVersion)
+                .Do(_ => LastCheckTime = scheduler.Now.UtcDateTime)
+                .DistinctUntilChanged()
+                .Select(version => Observable.FromAsync(ct => rulesProvider.GetRuleset(version, ct)).Do(_ => CurrentLabel = version))
+                .Switch()
+                .SubscribeOn(scheduler)
+                .Catch((Exception exception) =>
+                {
+                    logger.LogWarning(exception, "Failed to update rules");
+                    return Observable.Empty<Dictionary<string, RuleDefinition>>()
+                        .Delay(failureDelay);
+                })
+                .Repeat()
+                .Replay(1);
+
+            _subscription = new CompositeDisposable(
+                _pipeline.Subscribe(rules => OnRulesChange?.Invoke(rules)),
+                _pipeline.Connect()
+            );
+        }
+
+        public event Action<IDictionary<string, RuleDefinition>> OnRulesChange;
+
+        public async Task<Dictionary<string, RuleDefinition>> GetAllRules() => await _pipeline.FirstAsync();
+
+        public string CurrentLabel { get; private set; }
+
+        public DateTime LastCheckTime { get; private set; } = DateTime.MinValue;
+
+        public void Dispose()
+        {
+            _subscription.Dispose();
+        }
+    }
+}
