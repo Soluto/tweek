2486e629a72d6bb916f5c82fde3d9ec2a5c4e248
benny daon <bennydaon@gmail.com>
2017-11-20 21:24:04 +0200

    add AUTH_DIGEST_CREDENTIALS for user:pass pairs

--- services/editor/server/auth/httpDigest.js
+++ services/editor/server/auth/httpDigest.js
@@ -4,21 +4,27 @@ import { DigestStrategy } from 'passport-http';
 module.exports = function (server, config) {
   const givenUser = config.get('AUTH_DIGEST_USER');
   const givenPassword = config.get('AUTH_DIGEST_PASSWORD');
+  const givenCreds = config.get('AUTH_DIGEST_CREDENTIALS');
   const strategyParams = {
     realm: 'Tweek Editor',
     qop: 'auth', // Reference: https://github.com/jaredhanson/passport-http/blob/f66dfce9538a302e8c4706c77dd82374c5bfac22/lib/passport-http/strategies/digest.js#L31
   };
 
+  const creds = new Map(
+          [...(givenUser ? [[givenUser, givenPassword]] : []),
+           ...(givenCreds || []).split(';').map(x=> x.split(":"))
+          ])
+
   const validate = (user, done) => {
-    if (user === givenUser) {
+    if (creds.has(user)) {
       const userObject = {
-        id: givenUser,
-        sub: givenUser,
-        name: givenUser,
-        email: givenUser,
-        displayName: givenUser,
+        id: user,
+        sub: user,
+        name: user,
+        email: user,
+        displayName: user,
       };
-      done(null, userObject, givenPassword);
+      done(null, userObject, creds.get(user));
     } else {
       done(null, false);
     }
