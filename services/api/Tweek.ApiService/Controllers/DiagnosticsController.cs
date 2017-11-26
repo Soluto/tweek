6e363bdf08b0c4207ba827a68904ed1982073d96
Nataly Shrits <nataly87s@gmail.com>
2017-11-26 16:13:15 +0200

    remove status and isalive endpoints from api

--- services/api/Tweek.ApiService/Controllers/DiagnosticsController.cs
+++ services/api/Tweek.ApiService/Controllers/DiagnosticsController.cs
@@ -1,38 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Reflection;
-using Tweek.ApiService.Addons;
 
 namespace Tweek.ApiService.Controllers
 {
     public class DiagnosticsController : Controller
     {
         private string _version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
-        private IEnumerable<IDiagnosticsProvider> _diagnosticsProviders;
-
-        public DiagnosticsController(IEnumerable<IDiagnosticsProvider> diagnosticsProviders)
-        {
-            _diagnosticsProviders = diagnosticsProviders;
-        }
 
         [HttpGet("version")]
         public string Version()=> _version;
-
-        [HttpGet("isalive")]
-        [ProducesResponseType(typeof(Int32), (int) HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(Int32), (int) HttpStatusCode.ServiceUnavailable)]
-        public IActionResult IsAlive() => _diagnosticsProviders.All(x => x.IsAlive())
-            ? (IActionResult) Ok(HttpStatusCode.OK)
-            : StatusCode((int) HttpStatusCode.ServiceUnavailable);
-
-        [HttpGet("status")]
-        [ProducesResponseType(typeof(object), (int)HttpStatusCode.OK)]
-        [ApiExplorerSettings(IgnoreApi = true)]
-        public dynamic Status()
-            => _diagnosticsProviders.ToDictionary(provider => provider.Name, provider => provider.GetDetails());
 
         [HttpGet("gc")]
         [ApiExplorerSettings(IgnoreApi = true)]
