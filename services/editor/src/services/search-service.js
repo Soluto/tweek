d249f98e0882a3759f8da4244d6f25c3c233f426
Bryan Hunter <bryan.hunter@asurion.com>
2017-11-30 10:35:31 +0200

    Hide Internal Keypaths (#665)
    * moved filtering of internal keys (@tweek) to search-service.  Will now filter internal keys on all 'suggestion' searches.  Will not filter on 'search' searches since people still need to alter @tweek keys.
    
    Added filtering to NewKeyInput so @tweek doesn't show.
    
    Modified KeysList to call the filterInternalKeys from service instead of inline.
    
    * wrapped getSuggestions in filter instead of checking text for search or suggestions

--- services/editor/src/services/search-service.js
+++ services/editor/src/services/search-service.js
@@ -2,6 +2,22 @@
 
 import fetch from '../utils/fetch';
 let maxResults;
+let showInternalKeys;
+
+export const filterInternalKeys = async list =>
+  list && !await shouldShowInternalKeys() ? list.filter(x => !/^@tweek\//.test(x)) : list;
+
+const shouldShowInternalKeys = async () => {
+  if (showInternalKeys === undefined) {
+    try {
+      const response = await fetch('/api/editor-configuration/show_internal_keys');
+      showInternalKeys = await response.json();
+    } catch (err) {
+      console.error("unable to get 'show_internal_keys' configuration", err);
+    }
+  }
+  return showInternalKeys;
+};
 
 const createSearchFunction = endpoint =>
   async function suggestions(query) {
@@ -25,6 +41,8 @@ const createSearchFunction = endpoint =>
     return await response.json();
   };
 
-export const getSuggestions = createSearchFunction('suggestions');
+const suggestionFn = createSearchFunction('suggestions');
+
+export const getSuggestions = async query => filterInternalKeys(await suggestionFn(query));
 
 export const search = createSearchFunction('search');
