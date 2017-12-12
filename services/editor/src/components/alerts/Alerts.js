908ec10249b9eae6e22d38c0e628820da30306ac
Nataly Shrits <nataly87s@gmail.com>
2017-12-12 14:46:40 +0200

    [editor] added validation and component data to Alert component

--- services/editor/src/components/alerts/Alerts.js
+++ services/editor/src/components/alerts/Alerts.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withState, setDisplayName, compose } from 'recompose';
 import Rodal from 'rodal';
 import './Alerts.css';
+
+const addState = compose(setDisplayName('Alert'), withState('componentData', 'setComponentData'));
 
 const reactify = (Content, props) =>
   typeof Content === 'string' ? (
@@ -10,32 +13,41 @@ const reactify = (Content, props) =>
     <Content {...props} />
   );
 
-const Alert = ({
-  title,
-  message,
-  component: Component,
-  buttons,
-  onClose,
-  showCloseButton = false,
-}) => (
-  <Rodal
-    closeOnEsc={true}
-    visible
-    showCloseButton={showCloseButton}
-    onClose={onClose}
-    className={'rodal-container'}
-  >
-    {title && reactify(title, { className: 'rodal-header' })}
-    {message && reactify(message, { className: 'rodal-body' })}
-    {Component && <Component />}
-    <div className={'rodal-button-container'}>
-      {buttons.map(({ text, ...props }, i) => (
-        <button key={i} {...props}>
-          {text}
-        </button>
-      ))}
-    </div>
-  </Rodal>
+const Alert = addState(
+  ({
+    title,
+    message,
+    component: Component,
+    buttons,
+    onClose,
+    showCloseButton = false,
+    componentData,
+    setComponentData,
+  }) => (
+    <Rodal
+      closeOnEsc={true}
+      visible
+      showCloseButton={showCloseButton}
+      onClose={onClose}
+      className={'rodal-container'}
+    >
+      {title && reactify(title, { className: 'rodal-header' })}
+      {message && reactify(message, { className: 'rodal-body' })}
+      {Component && <Component onChange={setComponentData} componentData={componentData} />}
+      <div className={'rodal-button-container'}>
+        {buttons.map(({ text, validate, onClick, ...props }, i) => (
+          <button
+            key={i}
+            disabled={validate && !validate(componentData)}
+            onClick={() => onClick(componentData)}
+            {...props}
+          >
+            {text}
+          </button>
+        ))}
+      </div>
+    </Rodal>
+  ),
 );
 
 export default connect(state => state)(({ alerts }) => (
