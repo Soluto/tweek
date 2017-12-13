744d008ee965134f4ccf35cfb37050b3e314be02
Nataly Shrits <nataly87s@gmail.com>
2017-12-13 11:50:59 +0200

    [e2e-integration] rename test key

--- e2e/integration/spec/authoring-api/add-app.test.js
+++ e2e/integration/spec/authoring-api/add-app.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 chai.should();
-const {init:initClients} = require("../../utils/clients");
-const delay = (duration)=> new Promise(resolve=>setTimeout(resolve,duration));
+const { init: initClients } = require('../../utils/clients');
+const delay = duration => new Promise(resolve => setTimeout(resolve, duration));
 
 describe('authoring api', () => {
   let clients;
@@ -11,35 +11,32 @@ describe('authoring api', () => {
 
   describe('/posts /apps/new', () => {
     it('allow creating new app with keys-read permission', async () => {
-      const response = await clients.authoring.post('/api/apps?author.name=test&author.email=test@soluto.com')
-                                              .send({ name: 'my-app', permissions: ['keys-read'] })
-                                              .expect(200);
-      
-      let {appId, appSecret} = response.body;
+      const response = await clients.authoring
+        .post('/api/apps?author.name=test&author.email=test@soluto.com')
+        .send({ name: 'my-app', permissions: ['keys-read'] })
+        .expect(200);
+
+      let { appId, appSecret } = response.body;
 
       let appClient = await clients.authoring.with(client =>
-                      client.set( {"x-client-id": appId, "x-client-secret": appSecret } )
-                            .unset("Authorization")
-                    );
+        client.set({ 'x-client-id': appId, 'x-client-secret': appSecret }).unset('Authorization'),
+      );
 
-      await appClient.get('/api/keys/@integration_tests/some_key')
-                    .expect(200)
+      await appClient.get('/api/keys/integration_tests/some_key').expect(200);
 
-      await appClient.get('/api/keys')
-                            .expect(403)
+      await appClient.get('/api/keys').expect(403);
     });
 
-     it('allow creating new app with invalid permission', async () => {
-      await clients.authoring.post('/api/apps?author.name=test&author.email=test@soluto.com')
-                             .send({ name: 'my-app', permissions: ['admin'] })
-                             .expect(400);
+    it('allow creating new app with invalid permission', async () => {
+      await clients.authoring
+        .post('/api/apps?author.name=test&author.email=test@soluto.com')
+        .send({ name: 'my-app', permissions: ['admin'] })
+        .expect(400);
 
-      await clients.authoring.post('/api/apps?author.name=test&author.email=test@soluto.com')
-                             .send({ name: 'my-app', permissions: ['my-permission'] })
-                             .expect(400);
-      
+      await clients.authoring
+        .post('/api/apps?author.name=test&author.email=test@soluto.com')
+        .send({ name: 'my-app', permissions: ['my-permission'] })
+        .expect(400);
     });
-
   });
-
 });
