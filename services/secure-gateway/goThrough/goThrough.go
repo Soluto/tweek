1c3e1e173ab02ee289d27d37c6e95b2125dae1dd
Alex Fux <AlexanderFux83@gmail.com>
2017-12-26 13:42:10 +0200

    v1 conttoller adde

---
+++ services/secure-gateway/goThrough/goThrough.go
@@ -0,0 +1,37 @@
+package goThrough
+
+import (
+	"log"
+	"net/url"
+
+	"github.com/Soluto/tweek/services/secure-gateway/config"
+	"github.com/Soluto/tweek/services/secure-gateway/proxy"
+	"github.com/gorilla/mux"
+	"github.com/urfave/negroni"
+)
+
+func Mount(upstreams *config.Upstreams, middleware *negroni.Negroni, router *mux.Router) {
+	// URLs
+	api := parseUpstreamOrPanic(upstreams.API)
+	// authoring := parseUpstreamOrPanic(upstreams.Authoring)
+	// management := parseUpstreamOrPanic(upstreams.Management)
+
+	// Proxy forwarders
+	apiForwarder := proxy.New(api)
+	// authoringForwarder := proxy.New(authoring)
+	// managementForwarder := proxy.New(t.management)
+
+	// Mounting handlers
+	router.Methods("GET").PathPrefix("/values").Handler(middleware.With(apiForwarder))
+
+	// router.Methods("GET").PathPrefix("/tags").Handler(middleware.With(NewTagsGet(authoring), authoringForwarder))
+	// router.Methods("PUT").PathPrefix("/tags").Handler(middleware.With(NewTagsSave(authoring), authoringForwarder))
+}
+
+func parseUpstreamOrPanic(u string) *url.URL {
+	result, err := url.Parse(u)
+	if err != nil {
+		log.Panicln("Invalid upstream", u)
+	}
+	return result
+}
--- services/secure-gateway/router.go
+++ services/secure-gateway/router.go
@@ -16,34 +16,45 @@ import (
 
 type Router interface {
 	http.Handler
+	V1Router() *mux.Router
 	V2Router() *mux.Router
 }
 
 type router struct {
-	router         *mux.Router
-	basePathRouter *mux.Router
+	router   *mux.Router
+	v1Router *mux.Router
+	v2Router *mux.Router
 }
 
 // NewRouter creates a new transformation middleware
 func NewRouter(upstreams *config.Upstreams, enforcer *casbin.SyncedEnforcer) Router {
 	mainRouter := mux.NewRouter()
-	basePathRouter := mainRouter.PathPrefix("/api/v2/").Subrouter()
+
 	apiURL, err := url.Parse(upstreams.API)
 	if err != nil {
 		log.Panicln("Invalid upstream", upstreams.API)
 	}
 	apiForwarder := proxy.New(apiURL)
-	basePathRouter.Methods("GET").PathPrefix("/models").Handler(negroni.New(handlers.NewModelsRead(enforcer), apiForwarder))
-	basePathRouter.Methods("POST").PathPrefix("/models").Handler(negroni.New(handlers.NewModelsWrite(enforcer), apiForwarder))
+
+	v1Router := mainRouter.PathPrefix("/api/v1").Subrouter()
+	v1Router.Methods("GET").Handler(negroni.New(apiForwarder))
+
+	v2Router := mainRouter.PathPrefix("/api/v2/").Subrouter()
+
+	v2Router.Methods("GET").PathPrefix("/models").Handler(negroni.New(handlers.NewModelsRead(enforcer), apiForwarder))
+	v2Router.Methods("POST").PathPrefix("/models").Handler(negroni.New(handlers.NewModelsWrite(enforcer), apiForwarder))
 
 	return &router{
-		router:         mainRouter,
-		basePathRouter: basePathRouter,
+		router:   mainRouter,
+		v1Router: v1Router,
+		v2Router: v2Router,
 	}
 }
 
+func (t *router) V1Router() *mux.Router { return t.v1Router }
+
 // V2Router - returns the mux router for the base path (`/api/v2/`)
-func (t *router) V2Router() *mux.Router { return t.basePathRouter }
+func (t *router) V2Router() *mux.Router { return t.v2Router }
 
 func (t *router) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	t.router.ServeHTTP(rw, r)
