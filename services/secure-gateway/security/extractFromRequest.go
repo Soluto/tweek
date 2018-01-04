0b875a24808427c6829ce1bc1e8a4e80f44b58e2
Alex Fux <AlexanderFux83@gmail.com>
2018-01-04 13:28:21 +0200

    added act mapping

--- services/secure-gateway/security/extractFromRequest.go
+++ services/secure-gateway/security/extractFromRequest.go
@@ -1,6 +1,7 @@
 package security
 
 import "net/http"
+import "strings"
 
 // ExtractFromRequest extracts object and action from request
 func ExtractFromRequest(r *http.Request) (obj string, sub string, act string, err string) {
@@ -11,8 +12,26 @@ func ExtractFromRequest(r *http.Request) (obj string, sub string, act string, er
 
 		// this is work in progress
 		sub = user.Email()
+		switch {
+		case r.Method == "DELETE":
+		case r.Method == "PUT":
+		case r.Method == "POST":
+		case r.Method == "PATCH":
+			act = "write"
+		case r.Method == "GET" && strings.HasPrefix(r.RequestURI, "/manifests"):
+		case r.Method == "GET" && strings.HasPrefix(r.RequestURI, "/keys"):
+			act = "list"
+		case r.Method == "GET" && strings.HasPrefix(r.RequestURI, "/search"):
+		case r.Method == "GET" && strings.HasPrefix(r.RequestURI, "/suggestions"):
+			act = "search"
+		case r.Method == "GET" && strings.HasPrefix(r.RequestURI, "/search-index"):
+			act = "get search index"
+		case r.Method == "GET" && strings.HasPrefix(r.RequestURI, "/revision-history"):
+			act = "history"
+		default:
+			act = "read"
+		}
 		obj = r.RequestURI
-		act = r.Method
 	}
 	return obj, sub, act, err
 }
