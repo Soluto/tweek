78d369500ae02dbd42805714fc78be2bb35d91fd
Michael Kruglos <michaelk@soluto.com>
2018-01-07 19:42:26 +0200

    Added support for groups using casbin

--- services/secure-gateway/handlers/models_test.go
+++ services/secure-gateway/handlers/models_test.go
@@ -26,7 +26,7 @@ func TestNewModelsRead(t *testing.T) {
 		{
 			name: "Read",
 			args: args{request: httptest.NewRequest("GET", "/api/v2/models", nil)},
-			want: `[{"PType":"p","V0":"allow@security.test","V1":"/target","V2":"read","V3":"allow","V4":"","V5":""},{"PType":"p","V0":"deny@security.test","V1":"/target","V2":"read","V3":"deny","V4":"","V5":""}]`,
+			want: `[{"PType":"p","V0":"allow@security.test","V1":"/target","V2":"read","V3":"allow","V4":"","V5":""},{"PType":"p","V0":"role_users","V1":"/target","V2":"read","V3":"allow","V4":"","V5":""},{"PType":"p","V0":"deny@security.test","V1":"/target","V2":"read","V3":"deny","V4":"","V5":""},{"PType":"g","V0":"allow@security.test","V1":"role_users","V2":"","V3":"","V4":"","V5":""},{"PType":"g","V0":"deny@security.test","V1":"role_users","V2":"","V3":"","V4":"","V5":""}]`,
 		},
 	}
 
@@ -63,7 +63,7 @@ func TestNewModelsWrite(t *testing.T) {
 		want     bool
 	}{
 		{
-			name: "Write allowed",
+			name: "Add user with allow permissions",
 			args: args{
 				request:  httptest.NewRequest("PUT", "/api/v2/models", bytes.NewBufferString(`[{"PType":"p","V0":"allow1@security.test","V1":"/target","V2":"GET","V3":"allow","V4":"","V5":""}]`)),
 				user:     "allow1@security.test",
@@ -74,7 +74,7 @@ func TestNewModelsWrite(t *testing.T) {
 			want:     true,
 		},
 		{
-			name: "Write denied",
+			name: "Add user with deny permissions",
 			args: args{
 				request:  httptest.NewRequest("PUT", "/api/v2/models", bytes.NewBufferString(`[{"PType":"p","V0":"allow1@security.test","V1":"/target","V2":"GET","V3":"deny","V4":"","V5":""}]`)),
 				user:     "allow1@security.test",
@@ -83,6 +83,17 @@ func TestNewModelsWrite(t *testing.T) {
 			},
 			enforcer: casbin.NewSyncedEnforcer("../security/testdata/policy.conf", "../security/testdata/model.csv"),
 			want:     false,
+		},
+		{
+			name: "Add user to group with allow permissions",
+			args: args{
+				request:  httptest.NewRequest("PUT", "/api/v2/models", bytes.NewBufferString(`[{"PType":"g","V0":"allow1@security.test","V1":"role_users","V2":"","V3":"","V4":"","V5":""},{"PType":"p","V0":"role_users","V1":"/target","V2":"GET","V3":"deny","V4":"","V5":""}]`)),
+				user:     "allow1@security.test",
+				resource: "/target",
+				action:   "GET",
+			},
+			enforcer: casbin.NewSyncedEnforcer("../security/testdata/policy.conf", "../security/testdata/model.csv"),
+			want:     true,
 		},
 	}
 
