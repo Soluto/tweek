fafd4760972698767bbc440922a4f04ff995c401
AleF83 <AlexanderFux83@gmail.com>
2018-01-18 16:41:14 +0200

    .

--- e2e/integration/utils/clients.js
+++ e2e/integration/utils/clients.js
@@ -28,7 +28,7 @@ nconf
   .defaults({
     AUTHORING_URL: 'http://localhost:4005',
     API_URL: 'http://localhost:4003',
-    GATEWAY_URL: 'http://localhost:80',
+    GATEWAY_URL: 'http://localhost:4006',
     GIT_PRIVATE_KEY_PATH: '../../deployments/dev/ssh/tweekgit',
   });
 
