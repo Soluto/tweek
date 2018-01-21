383ddd9772e383ffbcd5e1d12633c12b62ec9637
Michael Kruglos <michaelk@soluto.com>
2018-01-21 15:59:40 +0200

    Some fixes for review

--- services/secure-gateway/audit/logAuditor.go
+++ services/secure-gateway/audit/logAuditor.go
@@ -11,44 +11,44 @@ const (
 	actionAuditFmt = "%v : %q %q %q"
 )
 
-type logAudit struct {
+type logAuditor struct {
 	log *log.Logger
 }
 
 // New creates a new Auditor, which logs to io.Writer, with prefix 'AUDIT'
 func New(out io.Writer) (Auditor, error) {
-	return NewLog(log.New(out, "AUDIT", log.LstdFlags|log.LUTC))
+	return NewLogger(log.New(out, "AUDIT", log.LstdFlags|log.LUTC))
 }
 
-// NewLog creates a new logger based Auditor
-func NewLog(logger *log.Logger) (Auditor, error) {
-	return &logAudit{log: logger}, nil
+// NewLogger creates a new logger based Auditor
+func NewLogger(logger *log.Logger) (Auditor, error) {
+	return &logAuditor{log: logger}, nil
 }
 
-func (a *logAudit) Allowed(subject, object, action string) {
+func (a *logAuditor) Allowed(subject, object, action string) {
 	a.log.Printf(actionAuditFmt, subject, object, action, "ACCESS ALLOWED")
 }
 
-func (a *logAudit) Denied(subject, object, action string) {
+func (a *logAuditor) Denied(subject, object, action string) {
 	a.log.Printf(actionAuditFmt, subject, object, action, "ACCESS DENIED")
 }
 
-func (a *logAudit) EnforcerError(subject, object, action string, err error) {
+func (a *logAuditor) EnforcerError(subject, object, action string, err error) {
 	a.log.Printf(actionAuditFmt, subject, object, action, fmt.Sprintf("ERROR: %v", err))
 }
 
-func (a *logAudit) TokenError(err error) {
+func (a *logAuditor) TokenError(err error) {
 	a.log.Printf("TOKEN ERROR: %q", err)
 }
 
-func (a *logAudit) EnforcerEnabled() {
+func (a *logAuditor) EnforcerEnabled() {
 	a.log.Printf("ENFORCER ENABLED")
 }
 
-func (a *logAudit) EnforcerDisabled() {
+func (a *logAuditor) EnforcerDisabled() {
 	a.log.Printf("ENFORCER DISABLED")
 }
 
-func (a *logAudit) RunningInTestMode() {
+func (a *logAuditor) RunningInTestMode() {
 	a.log.Printf("RUNNING IN TEST MODE")
 }
