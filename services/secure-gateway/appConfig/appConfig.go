bb3975e0273b4f95078e8f4a03ae23fab2865cf1
AleF83 <AlexanderFux83@gmail.com>
2018-01-25 17:29:15 +0200

    .

--- services/secure-gateway/appConfig/appConfig.go
+++ services/secure-gateway/appConfig/appConfig.go
@@ -60,7 +60,7 @@ func InitConfig() *Configuration {
 	tweekConfigor := configor.New(&configor.Config{ENVPrefix: "TWEEKGATEWAY"})
 
 	// Loading default config file
-	tweekConfigor.Load(conf, "config/gateway.json")
+	tweekConfigor.Load(conf, "/config/gateway.json")
 
 	// Loading provided config file
 	if configFilePath, exists := os.LookupEnv("TWEEK_GATEWAY_CONFIG_FILE_PATH"); exists {
