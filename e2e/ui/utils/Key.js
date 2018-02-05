b28c34e05fa31a18103cef44562006ea0da7b842
Yshayy <yshayy@gmail.com>
2018-02-05 11:48:12 +0200

    attempt to fix test on CF

--- e2e/ui/utils/Key.js
+++ e2e/ui/utils/Key.js
@@ -135,7 +135,6 @@ class Key {
 
   commitChanges(selector = saveChangesButton) {
     if (selector === saveChangesButton) assert.ok(this.hasChanges, 'no changes to commit');
-    browser.waitForEnabled(selector, timeout);
     browser.click(selector);
     browser.waitUntil(() => !this.hasChanges && !this.isSaving, timeout, 'changes were not saved');
     return this;
