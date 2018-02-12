c5bf0603578e2e0acbf16ebccdc7187f465cfc1c
Nataly Shrits <nataly87s@gmail.com>
2018-02-12 11:44:22 +0200

    resharper utils

--- services/publishing/Tweek.Publishing.Service/Utils/ConfigurationHelper.cs
+++ services/publishing/Tweek.Publishing.Service/Utils/ConfigurationHelper.cs
@@ -5,15 +5,20 @@ namespace Tweek.Publishing.Service.Utils
 {
     public static class ConfigurationExtensions
     {
-        public static string GetValueInlineOrFile(this IConfiguration configuration, string keyPrefix) {
+        public static string GetValueInlineOrFile(this IConfiguration configuration, string keyPrefix)
+        {
             var inline = configuration[$"{keyPrefix}"];
-            if ( inline != null) {
+            if (inline != null)
+            {
                 return inline;
             }
+
             var file = configuration[$"{keyPrefix}Path"];
-            if ( file != null) {
+            if (file != null)
+            {
                 return File.ReadAllText(file);
             }
+
             return null;
         }
     }
