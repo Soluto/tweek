833337b809cc84902008826b24be48a5b58a68b4
AleF83 <AlexanderFux83@gmail.com>
2018-02-21 12:02:59 +0200

    .

--- e2e/integration/spec/authoring-api/policy.spec.js
+++ e2e/integration/spec/authoring-api/policy.spec.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
-const { init: initClients, pollUntil } = require('../../utils/clients');
+const { init: initClients } = require('../../utils/clients');
+const { pollUntil } = require('../../utils/utils');
 const { getObjectContentFromMinio } = require('../../utils/minio');
 
 describe('authoring api policy', () => {
