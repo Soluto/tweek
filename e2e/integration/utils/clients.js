5accb9aedf000a510f15b72aa84c6230f6453308
AleF83 <AlexanderFux83@gmail.com>
2018-02-21 15:29:35 +0200

    .

--- e2e/integration/utils/clients.js
+++ e2e/integration/utils/clients.js
@@ -31,7 +31,6 @@ nconf
   .defaults({
     AUTHORING_URL: 'http://authoring.localtest.me:4009',
     API_URL: 'http://api.localtest.me:4009',
-    MANAGEMENT_URL: 'http://management.localtest.me:4009',
     GATEWAY_URL: 'http://localhost:4009',
     GIT_PRIVATE_KEY_PATH: '../../deployments/dev/ssh/tweekgit',
   });
@@ -47,7 +46,6 @@ module.exports.init = async function() {
   return {
     api: createClient(nconf.get('API_URL'), setBearerToken),
     authoring: createClient(nconf.get('AUTHORING_URL'), setBearerToken),
-    management: createClient(nconf.get('MANAGEMENT_URL'), setBearerToken),
     gateway: createClient(nconf.get('GATEWAY_URL'), setBearerToken),
   };
 };
--- services/secure-gateway/transformation/health.go
+++ services/secure-gateway/transformation/health.go
@@ -15,20 +15,16 @@ import (
 func NewHealthHandler(upstreams *appConfig.Upstreams, token security.JWTToken, middleware *negroni.Negroni) http.Handler {
 	api := parseUpstreamOrPanic(upstreams.API)
 	authoring := parseUpstreamOrPanic(upstreams.Authoring)
-	management := parseUpstreamOrPanic(upstreams.Management)
 
 	// Proxy forwarders
 	apiForwarder := middleware.With(proxy.New(api, token))
 	authoringForwarder := middleware.With(proxy.New(authoring, token))
-	managementForwarder := middleware.With(proxy.New(management, token))
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		switch r.Host {
 		case "api":
 			apiForwarder.ServeHTTP(rw, r)
 		case "authoring":
 			authoringForwarder.ServeHTTP(rw, r)
-		case "management":
-			managementForwarder.ServeHTTP(rw, r)
 		default:
 			rw.WriteHeader(http.StatusOK)
 			io.WriteString(rw, "OK")
