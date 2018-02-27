55ccb49fae59912f322bf78181aba1fb19004eac
AleF83 <AlexanderFux83@gmail.com>
2018-02-27 15:06:26 +0200

    .

--- services/secure-gateway/security/authorization.go
+++ services/secure-gateway/security/authorization.go
@@ -16,28 +16,37 @@ import (
 func AuthorizationMiddleware(enforcer *casbin.SyncedEnforcer, auditor audit.Auditor) negroni.HandlerFunc {
 	enforcer.AddFunction("matchResources", MatchResources)
 	return negroni.HandlerFunc(func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-		sub, act, ctxs, err := ExtractFromRequest(r)
-		if err != nil {
-			log.Println("Failed to extract from request", err)
-			auditor.EnforcerError(sub, fmt.Sprintf("%q", ctxs), act, err)
-			http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		} else {
-			res, err := enforcer.EnforceSafe(sub, ctxs, act)
-			if err != nil {
-				log.Println("Failed to validate request", err)
-				auditor.EnforcerError(sub, fmt.Sprintf("%q", ctxs), act, err)
-				http.Error(rw, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-				return
-			}
-
-			if !res {
-				auditor.Denied(sub, fmt.Sprintf("%q", ctxs), act)
-				http.Error(rw, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-				return
-			}
-
-			auditor.Allowed(sub, fmt.Sprintf("%q", ctxs), act)
+		user, ok := r.Context().Value(UserInfoKey).(UserInfo)
+		if !ok {
+			log.Panic("Authentication failed")
+		}
+		if user.Issuer() == "tweek-internal" {
 			next(rw, r)
+			log.Print("Issuer is tweek-internal - ACCESS ALLOWED")
+		} else {
+			sub, act, ctxs, err := ExtractFromRequest(r)
+			if err != nil {
+				log.Println("Failed to extract from request", err)
+				auditor.EnforcerError(sub, fmt.Sprintf("%q", ctxs), act, err)
+				http.Error(rw, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			} else {
+				res, err := enforcer.EnforceSafe(sub, ctxs, act)
+				if err != nil {
+					log.Println("Failed to validate request", err)
+					auditor.EnforcerError(sub, fmt.Sprintf("%q", ctxs), act, err)
+					http.Error(rw, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+					return
+				}
+
+				if !res {
+					auditor.Denied(sub, fmt.Sprintf("%q", ctxs), act)
+					http.Error(rw, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+					return
+				}
+
+				auditor.Allowed(sub, fmt.Sprintf("%q", ctxs), act)
+				next(rw, r)
+			}
 		}
 	})
 }
