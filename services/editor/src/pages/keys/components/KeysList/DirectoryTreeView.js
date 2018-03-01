0cfa87a83b416a8c863abb64e7c915e3696fd46a
Yshayy <yshayy@gmail.com>
2018-03-01 10:53:08 +0200

    Update DirectoryTreeView.js
    use localcompare instead of >

--- services/editor/src/pages/keys/components/KeysList/DirectoryTreeView.js
+++ services/editor/src/pages/keys/components/KeysList/DirectoryTreeView.js
@@ -9,7 +9,7 @@ const leaf = Symbol();
 const compsPathSorter = (l, r) => {
   if (l.props.node === leaf && r.props.node !== leaf) return 1;
   if (r.props.node === leaf && l.props.node !== leaf) return -1;
-  return l.props.name > r.props.name;
+  return l.props.name.localeCompare(r.props.name);
 };
 
 export default function DirectoryTreeView({ paths, renderItem, expandByDefault }) {
