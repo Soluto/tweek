bf6e58b438cddbaa02317b8ad0ddb272f6d039d1
Michael Kruglos <3526988+michaelkruglos@users.noreply.github.com>
2018-05-07 15:22:11 +0300

    805 context multi driver (#808)
    * Initial setup for multi driver
    
    * WIP
    
    * Restored redis integration tests
    
    * Implemented multi context driver
    
    * Fixes for multi context integration driver
    
    * Some fixes for multi driver
    
    * Trying to fix couchbase related issue in circleci
    
    * Fixes for multi driver
    
    * Work in progress
    
    * WIP
    
    * Implementation of multi context driver
    
    * Added multidriver unit tests and fixed a few bugs
    
    * Running unit tests as part of the build
    
    * Fixes for review
    
    * Removed unnecessary attribute
    
    * Fixes for review
    
    * removed multi integration test from source, since it is not needed anymore
    
    * Small refactor to improve code reuse
    
    * Removed unnecessary integration test code
    
    * Fixed naming, following review
    
    * Fixes for review
    
    * Fixes for review
    
    * Fixes for solution file
    
    * small fix for project file
    
    * small fixes for project files
    
    * Made unit tests async
    
    * Added remove non existent property test
    
    * Fixes for integration tests
    
    * Fixes for couchbase driver
    
    * Fixes for review
    
    * Removed multi context section from appsettings.json

---
+++ services/api/Tweek.ApiService/MultiContext/MultiContextAddon.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tweek.ApiService.Addons;
+using Tweek.Engine.Drivers.Context;
+
+namespace Tweek.ApiService.MultiContext
+{
+    public class MultiContextAddon: ITweekAddon
+    {
+        public void Use(IApplicationBuilder builder, IConfiguration configuration)
+        {
+        }
+
+        public void Configure(IServiceCollection services, IConfiguration configuration)
+        {
+            var readersNames = configuration.GetValue<string>("MultiContext:Readers").Split(',').Select(n=>n.Trim()).ToArray();
+            var writersNames = configuration.GetValue<string>("MultiContext:Writers").Split(',').Select(n=>n.Trim()).ToArray();
+            
+            var drivers = services.SetupAllContextDrivers(configuration, readersNames.Concat(writersNames));
+            services.RemoveAllContextDrivers();
+            
+            var readers = GetContextDrivers(drivers, readersNames);
+            var writers = GetContextDrivers(drivers, writersNames);
+            services.AddSingleton<IContextDriver>(new MultiContextDriver(readers, writers));
+        }
+
+        private static IEnumerable<IContextDriver> GetContextDrivers(IDictionary<string,IContextDriver> drivers, IEnumerable<string> names)
+        {
+            return drivers.Where(kvPair => names.Contains(kvPair.Key)).Select(kvPair => kvPair.Value);
+        }
+
+    }
+}
---
+++ services/api/Tweek.ApiService/MultiContext/MultiContextExtensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection;
+using Couchbase.Search;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyModel;
+using Tweek.ApiService.Addons;
+using Tweek.ApiService.Utils;
+using Tweek.Engine.Drivers.Context;
+
+namespace Tweek.ApiService.MultiContext
+{
+    public static class MultiContextExtensions
+    {
+        public static IDictionary<string, IContextDriver> SetupAllContextDrivers(this IServiceCollection services,
+            IConfiguration configuration, IEnumerable<string> names)
+        {
+            var addonConfiguration = configuration.GetSection("Addons");
+
+            var selectedAddons = new HashSet<string>(
+                names
+                    .Where(x => !string.IsNullOrEmpty(x))
+                    .Select(addon => addonConfiguration.GetSection(addon))
+                    .Select(x => Assembly.CreateQualifiedName(x["AssemblyName"], x["ClassName"]))
+            );
+
+            var drivers = AddonsListExtensions.GetSelectedAddons(selectedAddons);
+
+            foreach (var driver in drivers)
+            {
+                driver.Configure(services, configuration);
+            }
+
+            var serviceDescriptors = services.GetDescriptorsByType(typeof(IContextDriver)).ToArray();
+            var assemblyNamesToAddonName = addonConfiguration.GetChildren()
+                .ToDictionary(addon => addon.GetValue<string>("AssemblyName"), addon => addon.Key);
+
+            return serviceDescriptors.ToDictionary(
+                descriptor => assemblyNamesToAddonName[descriptor.ImplementationInstance.GetType().GetTypeInfo().Assembly.GetName().Name],
+                descriptor => (IContextDriver) descriptor.ImplementationInstance);
+        }
+
+        public static void RemoveAllContextDrivers(this IServiceCollection services)
+        {
+            var serviceDescriptors = services.GetDescriptorsByType(typeof(IContextDriver)).ToArray();
+            foreach (var descriptor in serviceDescriptors)
+            {
+                services.Remove(descriptor);
+            }
+        }
+
+        private static IEnumerable<ServiceDescriptor> GetDescriptorsByType(this IServiceCollection services, Type type)
+        {
+            return services.Where(descriptor => descriptor.ServiceType == type);
+        }
+    }
+}
