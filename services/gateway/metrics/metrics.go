d07262d6107eb32db4fe599101195de4147d293a
Michael Kruglos <3526988+michaelkruglos@users.noreply.github.com>
2018-05-30 16:01:05 +0300

    Added metrics per upstream for passthrough (#840)
    * Added metrics per upstream for passthrough
    
    * Fixes for review

---
+++ services/gateway/metrics/metrics.go
@@ -0,0 +1,30 @@
+package metrics
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/urfave/negroni"
+)
+
+// NewMetricsVar creates the new metrics histogram
+func NewMetricsVar(subsystem string) *prometheus.SummaryVec {
+	var metrics = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Subsystem: subsystem,
+		Name:      "request_duration_seconds",
+		Help:      "Total time spent serving requests.",
+	}, []string{"upstream", "method"})
+
+	prometheus.MustRegister(metrics)
+	return metrics
+}
+
+// NewMetricsMiddleware creates a new metrics middleware
+func NewMetricsMiddleware(label string, metric *prometheus.SummaryVec) negroni.HandlerFunc {
+
+	return func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		defer func(begin time.Time) { metric.WithLabelValues(label, r.Method).Observe(time.Since(begin).Seconds()) }(time.Now())
+		next(rw, r)
+	}
+}
--- services/gateway/passThrough/passThrough.go
+++ services/gateway/passThrough/passThrough.go
@@ -4,14 +4,17 @@ import (
 	"log"
 	"net/url"
 
+	"github.com/prometheus/client_golang/prometheus"
+
 	"github.com/Soluto/tweek/services/gateway/appConfig"
+	"github.com/Soluto/tweek/services/gateway/metrics"
 	"github.com/Soluto/tweek/services/gateway/proxy"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
 )
 
 // Mount - mounts the request passThrough handlers and middleware
-func Mount(upstreams *appConfig.Upstreams, v1Hosts *appConfig.V1Hosts, middleware *negroni.Negroni, router *mux.Router) {
+func Mount(upstreams *appConfig.Upstreams, v1Hosts *appConfig.V1Hosts, middleware *negroni.Negroni, metricsVar *prometheus.SummaryVec, router *mux.Router) {
 	// URLs
 	api := parseUpstreamOrPanic(upstreams.API)
 	authoring := parseUpstreamOrPanic(upstreams.Authoring)
@@ -20,12 +23,16 @@ func Mount(upstreams *appConfig.Upstreams, v1Hosts *appConfig.V1Hosts, middlewar
 	apiForwarder := proxy.New(api, nil)
 	authoringForwarder := proxy.New(authoring, nil)
 
+	// metrics
+	apiMetricsMiddleware := metrics.NewMetricsMiddleware("api", metricsVar)
+	authoringMetricsMiddleware := metrics.NewMetricsMiddleware("authoring", metricsVar)
+
 	// Mounting handlers
 	for _, host := range v1Hosts.API {
-		router.Host(host).Handler(middleware.With(apiForwarder))
+		router.Host(host).Handler(middleware.With(apiMetricsMiddleware, apiForwarder))
 	}
 	for _, host := range v1Hosts.Authoring {
-		router.Host(host).Handler(middleware.With(authoringForwarder))
+		router.Host(host).Handler(middleware.With(authoringMetricsMiddleware, authoringForwarder))
 	}
 }
 
