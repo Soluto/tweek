8c74d5008a4fe44a6c7c127c3c1a3b5e6bddef4a
Yshayy <yshayy@gmail.com>
2018-06-26 14:28:33 +0300

    updated api service

--- services/api/Tweek.ApiService/Program.cs
+++ services/api/Tweek.ApiService/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 using Serilog;
 using System.IO;
 
@@ -8,9 +9,7 @@ namespace Tweek.ApiService
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel(opts=> opts.Limits.MaxRequestLineSize = 128 * 1024)
-                .UseContentRoot(Directory.GetCurrentDirectory())
+            var host = WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .UseSerilog()
                 .Build();
