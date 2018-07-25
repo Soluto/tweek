a96820919590b69e64bc9d4fa7227a18ae498f63
AleF83 <AlexanderFux83@gmail.com>
2018-07-25 17:13:19 +0300

    Make http calls parallel

--- services/gateway/handlers/statusHandler.go
+++ services/gateway/handlers/statusHandler.go
@@ -5,20 +5,30 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"sync"
 
 	"github.com/Soluto/tweek/services/gateway/appConfig"
 )
 
 // NewStatusHandler - handler function that returns versions for all services
 func NewStatusHandler(config *appConfig.Upstreams) http.HandlerFunc {
+	services := map[string]string{
+		"api":        config.API,
+		"authoring":  config.Authoring,
+		"publishing": config.Publishing,
+	}
 	return func(w http.ResponseWriter, r *http.Request) {
-		versions := map[string]interface{}{
-			"api":        getServiceStatus(config.API),
-			"authoring":  getServiceStatus(config.Authoring),
-			"publishing": getServiceStatus(config.Publishing),
-		}
+		var wg sync.WaitGroup
+		wg.Add(len(services))
 
-		js, err := json.Marshal(versions)
+		serviceStatuses := map[string]interface{}{}
+
+		for serviceName, serviceHost := range services {
+			go checkServiceStatus(serviceName, serviceHost, serviceStatuses, &wg)
+		}
+		wg.Wait()
+
+		js, err := json.Marshal(serviceStatuses)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -28,23 +38,27 @@ func NewStatusHandler(config *appConfig.Upstreams) http.HandlerFunc {
 	}
 }
 
-func getServiceStatus(serviceHost string) map[string]interface{} {
+func checkServiceStatus(serviceName string, serviceHost string, statuses map[string]interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
 	resp, err := http.Get(fmt.Sprintf("%s/health", serviceHost))
 	if err != nil || resp.StatusCode != http.StatusOK {
-		return nil
+		fmt.Printf("Service health request for %s failed with error: %v\n", serviceName, err)
+		statuses[serviceName] = "Service health request failed"
+		return
 	}
-
 	defer resp.Body.Close()
 	contents, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return nil
+		fmt.Printf("Service health request for %s read failed with error: %v\n", serviceName, err)
+		statuses[serviceName] = "Service health request cannot be read"
+		return
 	}
-
 	var status map[string]interface{}
 	err = json.Unmarshal(contents, &status)
 	if err != nil {
-		return nil
+		fmt.Printf("Service health request for %s JSON parse failed with error: %v\n", serviceName, err)
+		statuses[serviceName] = "Service health request responded with bad format"
+		return
 	}
-
-	return status
+	statuses[serviceName] = status
 }
