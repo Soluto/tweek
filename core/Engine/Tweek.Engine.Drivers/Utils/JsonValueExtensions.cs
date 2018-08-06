4c1b530330e5b7d9eceaac48d1abbea38887164c
Yshayy <yshayy@gmail.com>
2018-08-06 19:04:33 +0300

    cosmetics, fix default version def

--- core/Engine/Tweek.Engine.Drivers/Utils/JsonValueExtensions.cs
+++ core/Engine/Tweek.Engine.Drivers/Utils/JsonValueExtensions.cs
@@ -8,8 +8,7 @@ namespace Tweek.Engine.Drivers {
         public static Option<JsonValue> GetPropertyOption(this JsonValue json, string propName)
         {
             var prop = json.TryGetProperty(propName);
-
-            return (prop == null || prop.Value == null) ? None : Some(prop.Value);
+            return Optional(prop?.Value);
         }
     }
 }
