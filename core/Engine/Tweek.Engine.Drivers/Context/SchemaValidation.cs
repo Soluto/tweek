e10ae76e2b90e50a83b2027903d4fddd7a399c95
Yshayy <yshayy@gmail.com>
2018-08-07 11:28:58 +0300

    cosmetics

--- core/Engine/Tweek.Engine.Drivers/Context/SchemaValidation.cs
+++ core/Engine/Tweek.Engine.Drivers/Context/SchemaValidation.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using FSharpUtils.Newtonsoft;
 using LanguageExt;
-using static Tweek.Engine.Drivers.Context.InputValidationContextDriver;
 using Unit = LanguageExt.Unit;
 using ValidationResult = LanguageExt.Validation<string, LanguageExt.Unit>;
 using static LanguageExt.Prelude;
@@ -21,54 +20,58 @@ namespace Tweek.Engine.Drivers.Context
 
         public delegate Option<Validator> Provider(string identityType);
         public delegate ValidationResult Validator(string property, JsonValue value);
-        private delegate ValidationResult PropertyValidator(JsonValue value);
         public delegate Option<CustomTypeDefinition> CustomTypeDefinitionProvider(string typeName);
+        private delegate ValidationResult PropertyValidator(JsonValue value);
 
         public static Validation<string, Unit> Valid = Success<string, Unit>(Unit.Default);
         public static Validation<string, Unit> Error(string error) => Fail<string, Unit>(error);
-        public static Provider Create(Dictionary<string, JsonValue> identitySchemas, IReadOnlyDictionary<string, CustomTypeDefinition> customTypeProvider = null, Mode mode = Mode.Strict) {
-                customTypeProvider = customTypeProvider ?? new Dictionary<string, CustomTypeDefinition>();
-                IReadOnlyDictionary<string, Validator> schemaValidators = identitySchemas.ToDictionary(x => x.Key, x=> 
-                    {
-                        IReadOnlyDictionary<string, PropertyValidator> propValidators = x.Value.Properties()
-                        .ToDictionary(prop => prop.Item1, 
-                        prop => prop.Item2.GetPropertyOption("type").Match(
-                            type=>CreateSinglePropertyValidator(type, customTypeProvider.TryGetValue ),
-                            ()=> new PropertyValidator((JsonValue _)=> Error($"Invalid property schema  \"{prop.Item1}\" -  missing type"))));
+        public static Provider Create(IReadOnlyDictionary<string, JsonValue> identitySchemas, IReadOnlyDictionary<string, CustomTypeDefinition> customTypes = null, Mode mode = Mode.Strict)
+        {
+            customTypes = customTypes ?? new Dictionary<string, CustomTypeDefinition>();
+            IReadOnlyDictionary<string, Validator> schemaValidators =
+                identitySchemas.ToDictionary(x => x.Key, x => CreateSchemaValidator(x.Value, customTypes.TryGetValue, mode));
+            return schemaValidators.TryGetValue;
+        }
+        private static PropertyValidator InvalidPropValidator(string propName) => (JsonValue _) => Error($"Invalid property schema  \"{propName}\" -  missing type");
+        public static Validator CreateSchemaValidator(JsonValue schema, CustomTypeDefinitionProvider customTypesProvider, Mode mode)
+        {
+            IReadOnlyDictionary<string, PropertyValidator> propValidators = schema.Properties()
+                    .Map(x=> (name: x.Item1, def: x.Item2 ))
+                    .ToDictionary( prop => prop.name,
+                    prop => prop.def.GetPropertyOption("type").Match(
+                        type => CreateSinglePropertyValidator(type, customTypesProvider),
+                        () => InvalidPropValidator(prop.name)));
 
-                        return new Validator((string propName, JsonValue propValue) => 
-                             propValidators.TryGetValue(propName)
-                                           .Match(validator => validator(propValue), ()=> 
-                                           mode == Mode.AllowUndefinedProperties ?
-                                           Valid :
-                                           Error($"property \"{propName}\" not found" )));
-                    }
-                );
-                return schemaValidators.TryGetValue;
-            }
-        
+            return new Validator((string propName, JsonValue propValue) =>
+                 propValidators.TryGetValue(propName)
+                               .Match(validator => validator(propValue), () =>
+                               mode == Mode.AllowUndefinedProperties ?
+                               Valid :
+                               Error($"property \"{propName}\" not found")));
+        }
+
         private static PropertyValidator CreateSinglePropertyValidator(JsonValue schema, CustomTypeDefinitionProvider provider)
         {
             switch (schema)
             {
-                case JsonValue.String baseTypeName:
-                    var baseType = baseTypeName.Item;
-                    
-                    var validator = fun((Func<JsonValue, bool> fn) => 
-                        new PropertyValidator(x=>  fn(x) ? Valid : Error($"value is not a {baseType}")));
-                    
-                    switch (baseType)
+                case JsonValue.String typeName:
+                    var type = typeName.Item;
+
+                    var validator = fun((Func<JsonValue, bool> fn) =>
+                        new PropertyValidator(x => fn(x) ? Valid : Error($"value is not a {type}")));
+
+                    switch (type)
                     {
-                        case "number":  return validator( x=>x.IsNumber);
-                        case "date": return validator(x=> DateTime.TryParse(x.AsString(), out var _));
+                        case "number": return validator(x => x.IsNumber);
+                        case "date": return validator(x => DateTime.TryParse(x.AsString(), out var _));
                         case "string": return validator(x => x.IsString);
-                        case "boolean": return validator(x=> x.IsBoolean);
-                        case "array": return validator(x=> x.IsArray);
-                        case "object": return validator(x=> x.IsRecord);
+                        case "boolean": return validator(x => x.IsBoolean);
+                        case "array": return validator(x => x.IsArray);
+                        case "object": return validator(x => x.IsRecord);
                         default:
-                            return provider(baseType)
-                           .Map(type => CreateCustomTypeValidator(type))
-                           .IfNone(() => (JsonValue value) => Error($"custom type \"{baseType}\" not exit"));
+                            return provider(type)
+                           .Map(customType => CreateCustomTypeValidator(customType))
+                           .IfNone(() => (JsonValue value) => Error($"custom type \"{type}\" not exists"));
                     }
                 case JsonValue.Record customTypeRaw:
                     return CreateCustomTypeValidator(CustomTypeDefinition.FromJsonValue(customTypeRaw));
@@ -84,6 +87,7 @@ namespace Tweek.Engine.Drivers.Context
             {
                 validators = validators.Append((JsonValue property) => !typeDefinition.AllowedValues.Any(v => v.Equals(property)) ? Error("value not in the allowed values") : Valid);
             }
+            
             var regexValidation = Optional(typeDefinition.Validation)
                          .Map(validation => fun((JsonValue property) =>
                          {
@@ -93,7 +97,7 @@ namespace Tweek.Engine.Drivers.Context
 
             validators = validators.Append(regexValidation);
 
-            return property => validators.Aggregate(Valid, (a, b) => a | b(property));
+            return property => validators.Aggregate(Valid, (acc, validator) => acc | validator(property));
         }
     }
 }
