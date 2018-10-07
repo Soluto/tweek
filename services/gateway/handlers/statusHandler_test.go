40409c55e9118c3cad161c8f31b089616ecb8f85
Yshayy <yshayy@gmail.com>
2018-10-07 21:33:49 +0300

    change status check, patch compose files

--- services/gateway/handlers/statusHandler_test.go
+++ services/gateway/handlers/statusHandler_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"reflect"
 	"testing"
 
@@ -11,10 +12,11 @@ const HOST = "http://myservice.com"
 
 func Test_checkServiceStatus(t *testing.T) {
 	type args struct {
-		serviceName      string
-		responseCode     int
-		responseBody     map[string]interface{}
-		expectedStatuses map[string]interface{}
+		serviceName       string
+		responseCode      int
+		responseBody      map[string]interface{}
+		expectedStatus    map[string]interface{}
+		expectedIsHealthy bool
 	}
 
 	tests := []struct {
@@ -24,34 +26,39 @@ func Test_checkServiceStatus(t *testing.T) {
 		{
 			name: "HTTP Response Status 200",
 			args: args{
-				serviceName:  "Healthy Server",
-				responseCode: 200,
-				responseBody: map[string]interface{}{},
-				expectedStatuses: map[string]interface{}{
-					"Healthy Server": map[string]interface{}{},
-				},
+				serviceName:       "Healthy Server",
+				responseCode:      200,
+				responseBody:      map[string]interface{}{},
+				expectedStatus:    map[string]interface{}{},
+				expectedIsHealthy: true,
 			},
 		},
 		{
 			name: "HTTP Response Status 500",
 			args: args{
-				serviceName:  "Unhealthy Server",
-				responseCode: 500,
-				responseBody: map[string]interface{}{"unhealthy": "I'm unhealthy"},
-				expectedStatuses: map[string]interface{}{
-					"Unhealthy Server": map[string]interface{}{"unhealthy": "I'm unhealthy"},
-				},
+				serviceName:       "Unhealthy Server",
+				responseCode:      500,
+				responseBody:      map[string]interface{}{"unhealthy": "I'm unhealthy"},
+				expectedStatus:    map[string]interface{}{"unhealthy": "I'm unhealthy"},
+				expectedIsHealthy: false,
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+
 			gock.New(HOST).Get("/health").Reply(tt.args.responseCode).JSON(tt.args.responseBody)
 			defer gock.Off()
-			statuses := map[string]interface{}{}
-			checkServiceStatus(tt.args.serviceName, HOST, statuses)
+			status, isHealthy := checkServiceStatus(tt.args.serviceName, HOST)
+			println(isHealthy)
+			val, err := json.Marshal(status)
+			if err == nil {
+				println(string(val))
+			} else {
+				println(err)
+			}
 
-			if !reflect.DeepEqual(statuses, tt.args.expectedStatuses) {
+			if !reflect.DeepEqual(status, tt.args.expectedStatus) || isHealthy != tt.args.expectedIsHealthy {
 				t.Fatal()
 			}
 		})
