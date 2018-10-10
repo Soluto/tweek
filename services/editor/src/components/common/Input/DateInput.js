889e8eec836ede1e4f956991f5371cb4dfaf13b6
Michael Kruglos <michaelk@soluto.com>
2018-10-10 15:27:03 +0300

    Refactor datepicker to be a functional component

--- services/editor/src/components/common/Input/DateInput.js
+++ services/editor/src/components/common/Input/DateInput.js
@@ -2,51 +2,50 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Calendar from 'react-calendar';
 import moment from 'moment';
+import { withState, compose } from 'recompose';
 import Input from './Input';
 
 import './DateInput.css';
 
-//const DateInput = ({ onChange, value, ...props }) => {
-class DateInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showCalendar: false };
-    this.onFocus = this.onFocus.bind(this);
-    this.onBlur = this.onBlur.bind(this);
-  }
 
-  onFocus(evt) {
+const DateInput = compose(
+  withState('showCalendar', 'setShowCalendar', false)
+)(({ onChange, value, showCalendar, setShowCalendar, ...props }) => {
+  // const { onChange, value, ...props } = this.props;
+  const calendarDate = value ? new Date(value) : new Date();
+
+  const onFocus = (evt) => {
     evt.preventDefault();
-    this.setState({ showCalendar: true });
-  }
-
-  onBlur(evt) {
+    setShowCalendar(true);
+  };
+  const onBlur = (evt) => {
     evt.preventDefault();
     // We need this timeout, when calendar is clicked, so it can call onChange
-    setTimeout(() => this.setState({ showCalendar: false }), 100);
-  }
+    setTimeout(() => setShowCalendar(false), 100);
+  };
 
-  render() {
-    const { onChange, value, ...props } = this.props;
-    const calendarDate = value ? new Date(value) : new Date();
-    return (
-      <div>
-        <Input {...props} onChange={onChange} onFocus={this.onFocus} onBlur={this.onBlur} value={value} />
-        {
-          this.state.showCalendar && !isNaN(calendarDate) ?
-            (<div className="calendar wrapper">
-              <div className="calendar container">
-                <Calendar
-                  className="calendar"
-                  onChange={newDate => onChange(moment(newDate).format("L"))}
-                  value={calendarDate}
-                />
-              </div>
-            </div>) : null
-        }
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Input {...props} onChange={onChange} onFocus={onFocus} onBlur={onBlur} value={value} />
+      {
+        showCalendar && !isNaN(calendarDate) ?
+          (<div className="calendar wrapper">
+            <div className="calendar container">
+              <Calendar
+                className="calendar"
+                onChange={newDate => onChange(moment(newDate).format("L"))}
+                value={calendarDate}
+              />
+            </div>
+          </div>) : null
+      }
+    </div>
+  );
+});
+
+DateInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+};
 
 export default DateInput;
