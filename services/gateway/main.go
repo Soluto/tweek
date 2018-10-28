79838b5749cc39af016a98369cbec79449397895
Michael Kruglos <michaelk@soluto.com>
2018-10-28 11:13:24 +0200

    Moved the endpoint to /api/v2

--- services/gateway/main.go
+++ services/gateway/main.go
@@ -92,7 +92,7 @@ func newApp(config *appConfig.Configuration) http.Handler {
 
 	router.MainRouter().PathPrefix("/metrics").Handler(prometheus.Handler())
 
-	router.MainRouter().PathPrefix("/user-info").HandlerFunc(security.NewUserInfoHandler(&config.Security, userInfoExtractor))
+	router.V2Router().PathPrefix("/user-info").HandlerFunc(security.NewUserInfoHandler(&config.Security, userInfoExtractor))
 
 	app := negroni.New(recovery)
 
