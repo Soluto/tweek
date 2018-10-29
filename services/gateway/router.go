c9333a561635c891444d7c4acf99bfefcd80a83e
Michael Kruglos <michaelk@soluto.com>
2018-10-29 10:29:01 +0200

    Changed how /configurations/ path is mounted

--- services/gateway/router.go
+++ services/gateway/router.go
@@ -11,7 +11,6 @@ import (
 // Router struct contains all subrouters
 type Router interface {
 	http.Handler
-	Configurations() *mux.Router
 	V1Router() *mux.Router
 	V2Router() *mux.Router
 	LegacyNonV1Router() *mux.Router
@@ -21,7 +20,6 @@ type Router interface {
 
 type router struct {
 	router            *mux.Router
-	configurations    *mux.Router
 	v1Router          *mux.Router
 	v2Router          *mux.Router
 	legacyNonV1Router *mux.Router
@@ -40,8 +38,6 @@ func NewRouter(configuration *appConfig.Configuration) Router {
 		return result
 	}).Subrouter()
 
-	configurations := mainRouter.PathPrefix("/configurations/").Subrouter()
-
 	v1Router := mainRouter.PathPrefix("/api/v1/").Subrouter()
 
 	v2Router := mainRouter.PathPrefix("/api/v2/").Subrouter()
@@ -49,7 +45,6 @@ func NewRouter(configuration *appConfig.Configuration) Router {
 	authRouter := mainRouter.PathPrefix("/auth").Subrouter()
 
 	return &router{
-		configurations:    configurations,
 		router:            mainRouter,
 		v1Router:          v1Router,
 		v2Router:          v2Router,
@@ -59,8 +54,6 @@ func NewRouter(configuration *appConfig.Configuration) Router {
 }
 
 func (t *router) MainRouter() *mux.Router { return t.router }
-
-func (t *router) Configurations() *mux.Router { return t.configurations }
 
 func (t *router) V1Router() *mux.Router { return t.v1Router }
 
