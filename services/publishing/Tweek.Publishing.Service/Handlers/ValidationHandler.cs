aafdd940e5d156137f149bfddad670948f1068a8
Michael Kruglos <michaelk@soluto.com>
2018-12-05 18:11:31 +0200

    Added metrics to publishing

--- services/publishing/Tweek.Publishing.Service/Handlers/ValidationHandler.cs
+++ services/publishing/Tweek.Publishing.Service/Handlers/ValidationHandler.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using App.Metrics;
+using App.Metrics.Counter;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Logging;
@@ -10,7 +12,13 @@ namespace Tweek.Publishing.Service.Handlers
 {
     public class ValidationHandler
     {
-        public static Func<HttpRequest, HttpResponse, RouteData, Task> Create(ShellExecutor executor, GitValidationFlow gitValidationFlow, ILogger logger) =>
+        private static readonly CounterOptions Validation = new CounterOptions
+            {Context = "publishing", Name = "validation"};
+
+        private static readonly MetricTags Success = new MetricTags("Status", "Successful");
+        private static readonly MetricTags Failure = new MetricTags("Status", "Failure");
+
+        public static Func<HttpRequest, HttpResponse, RouteData, Task> Create(ShellExecutor executor, GitValidationFlow gitValidationFlow, ILogger logger, IMetrics metrics) =>
             async (req, res, routedata) =>
             {
                 var oldCommit = req.Query["oldrev"].ToString().Trim();
@@ -26,9 +34,11 @@ namespace Tweek.Publishing.Service.Handlers
                 try
                 {
                     await gitValidationFlow.Validate(oldCommit, newCommit, gitExecutor);
+                    metrics.Measure.Counter.Increment(Validation, Success);
                 }
                 catch (Exception ex)
                 {
+                    metrics.Measure.Counter.Increment(Validation, Failure);
                     res.StatusCode = 400;
                     logger.LogWarning("Validation failed, {error}", ex.Message);
                     await res.WriteAsync(ex.Message);
