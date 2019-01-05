121cd0218545da815ed1f724e8509456a41ed921
Gabi Nir <gabinir@gmail.com>
2019-01-05 22:06:43 +0200

    withSecurityPageIsEnabled.js -> ../withSecurityPageIsEnabled.js

---
+++ services/editor/src/pages/security/withSecurityPageIsEnabled.js
@@ -0,0 +1,29 @@
+import { mapPropsStream } from 'recompose';
+import Rx from 'rxjs';
+import { getConfiguration } from '../../utils/fetch';
+const getSecurityPageIsEnabled = async () => {
+  try {
+    const response = await getConfiguration('security_page/is_enabled');
+
+    return await response.json();
+  } catch (e) {
+    console.warn('Failed fetching security_page/is_enabled from Tweek', e);
+    return false;
+  }
+};
+
+export default securityPageIsEnabledPropName =>
+  mapPropsStream((props$) => {
+    const securityPageIsEnabled$ = props$
+      .flatMap(() => {
+        const futureValue = Rx.Observable.fromPromise(
+          getSecurityPageIsEnabled().then(value => value, () => false),
+        );
+        return Rx.Observable.of(futureValue);
+      })
+      .switch();
+    return props$.combineLatest(securityPageIsEnabled$, (props, securityPageIsEnabled) => ({
+      ...props,
+      [securityPageIsEnabledPropName]: securityPageIsEnabled,
+    }));
+  });
