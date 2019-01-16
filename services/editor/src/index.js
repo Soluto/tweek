cb1c41f8d9d3be2c1fec59fa416fb379d054f5dc
Michael Kruglos <michaelk@soluto.com>
2019-01-16 11:50:27 +0200

    Upgraded editor node to version 10

--- services/editor/src/index.js
+++ services/editor/src/index.js
@@ -2,12 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import configureStore from './store/configureStore';
 import Routes from './Routes';
 import 'papp-polyfill';
-
-injectTapEventPlugin();
 
 const store = configureStore({});
 
