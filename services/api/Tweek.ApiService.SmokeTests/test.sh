57c80010132eb62612f60f747c45f7efa9dfefb6
Yshayy <yshayy@gmail.com>
2019-01-21 06:04:23 +0200

    Update test.sh
    attempt to fix smoke tests

--- services/api/Tweek.ApiService.SmokeTests/test.sh
+++ services/api/Tweek.ApiService.SmokeTests/test.sh
@@ -3,5 +3,6 @@
 # Abort script on error
 set -e
 
-wget -O /dev/null --retry-on-http-error=502 --tries=20 --timeout=15 --read-timeout=20 --waitretry=30 --retry-connrefused http://api/status
+wget -O /dev/null  --tries=20 --timeout=15 --read-timeout=20 --waitretry=30 --retry-connrefused http://gateway/status
+sleep 3
 dotnet test --no-build
