7b4a23604a3eed9bd80461cad20fd59353977dfe
Yshayy <yshayy@gmail.com>
2019-01-21 06:17:45 +0200

    Update test.sh

--- services/api/Tweek.ApiService.SmokeTests/test.sh
+++ services/api/Tweek.ApiService.SmokeTests/test.sh
@@ -5,5 +5,5 @@
 wget -O /dev/null  --tries=20 --timeout=15 --read-timeout=20 --waitretry=30 --retry-connrefused http://gateway/status
 set -e
 sleep 10
-wget -O /dev/null  --tries=20 --timeout=15 --read-timeout=20 --waitretry=30 --retry-connrefused http://api/status
+wget -O /dev/null  --tries=20 --timeout=15 --read-timeout=20 --waitretry=30 --retry-connrefused http://api/health
 dotnet test --no-build
