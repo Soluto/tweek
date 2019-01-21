902375dabc519625bb91f6a04a33afee874a6f87
Yshayy <yshayy@gmail.com>
2019-01-21 05:45:33 +0200

    Update test.sh
    fix smoketests entrypoint

--- services/api/Tweek.ApiService.SmokeTests/test.sh
+++ services/api/Tweek.ApiService.SmokeTests/test.sh
@@ -3,8 +3,5 @@
 # Abort script on error
 set -e
 
-function run_tests()
-{
-  wget -O /dev/null --tries 20 --timeout=15 --read-timeout=20 --waitretry=30 --retry-connrefused http://api/status
-  dotnet test --no-build
-}
+wget -O /dev/null --tries 20 --timeout=15 --read-timeout=20 --waitretry=30 --retry-connrefused http://api/status
+dotnet test --no-build
