aaf95de012b428bf97f20ee1b309be30ef06b5af
Nataly Shrits <nataly87s@gmail.com>
2019-01-30 19:00:58 +0200

    readonly key

---
+++ e2e/testcafe/spec/keys/readonly-key.js
@@ -0,0 +1,17 @@
+import { editorUrl } from '../../utils/constants';
+import { credentials, login } from '../../utils/auth-utils';
+import EditKey from '../../pages/Keys/EditKey';
+
+const testKeyFullPath = 'behavior_tests/read_only';
+
+fixture`Readonly Key`.page`${editorUrl}/keys`.httpAuth(credentials).beforeEach(login);
+
+test('should open the key as readonly', async (t) => {
+  const editKey = await EditKey.open(testKeyFullPath);
+
+  await t
+    .expect(editKey.messageText.visible)
+    .ok()
+    .expect(editKey.jpad.container.withAttribute('disabled').exists)
+    .ok();
+});
