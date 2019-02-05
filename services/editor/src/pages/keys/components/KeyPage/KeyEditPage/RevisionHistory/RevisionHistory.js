b687598fd6fb3e057bebcffbfd41b52f5cecfaef
Nataly Shrits <nataly87s@gmail.com>
2019-02-06 00:57:53 +0200

    upgrade redux

--- services/editor/src/pages/keys/components/KeyPage/KeyEditPage/RevisionHistory/RevisionHistory.js
+++ services/editor/src/pages/keys/components/KeyPage/KeyEditPage/RevisionHistory/RevisionHistory.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import moment from 'moment';
 import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
+import { push } from 'connected-react-router';
 import styled from '@emotion/styled';
 
 const RevisionHistorySelect = styled.select`
--- services/editor/src/store/rootReducer.js
+++ services/editor/src/store/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import { connectRouter } from 'connected-react-router';
 import keysReducer from './ducks/keys';
 import selectedKeyReducer from './ducks/selectedKey';
 import tagsReducer from './ducks/tags';
@@ -8,13 +8,14 @@ import alertsReducer from './ducks/alerts';
 import schemaReducer from './ducks/schema';
 import contextReducer from './ducks/context';
 
-export default combineReducers({
-  keys: keysReducer,
-  selectedKey: selectedKeyReducer,
-  routing: routerReducer,
-  tags: tagsReducer,
-  notifications: notificationsReducer,
-  alerts: alertsReducer,
-  context: contextReducer,
-  schema: schemaReducer,
-});
+export default (history) =>
+  combineReducers({
+    keys: keysReducer,
+    selectedKey: selectedKeyReducer,
+    router: connectRouter(history),
+    tags: tagsReducer,
+    notifications: notificationsReducer,
+    alerts: alertsReducer,
+    context: contextReducer,
+    schema: schemaReducer,
+  });
