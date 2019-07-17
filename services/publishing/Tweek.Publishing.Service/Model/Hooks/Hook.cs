21182ae03b4d9abfe195719dceec0b3887547198
Tomer Eskenazi <tomeresk@gmail.com>
2019-07-17 09:46:17 +0300

    Hooks changes Make the If-Match header optional fix TriggerNotificationHooksForCommit to only trigger notification hooks

--- services/publishing/Tweek.Publishing.Service/Model/Hooks/Hook.cs
+++ services/publishing/Tweek.Publishing.Service/Model/Hooks/Hook.cs
@@ -1,47 +1,26 @@
-using System.Text;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System;
-using Tweek.Publishing.Service.Utils;
 using Newtonsoft.Json;
 
 namespace Tweek.Publishing.Service.Model.Hooks {
   public class Hook {
     [JsonProperty("type")]
-    private string _type;
+    public string Type { get; }
     [JsonProperty("url")]
-    private string _url;
+    public string Url { get; }
 
     public Hook(string type, string url) {
-      this._type = type;
-      this._url = url;
-    }
-
-    public async Task Trigger(string payload) {
-      switch (_type) {
-        case "notification_webhook":
-          await TriggerNotificationWebhook(payload);
-          break;
-        default:
-          throw new Exception($"Failed to trigger hook, invalid type: {_type}");
-      }
-    }
-
-    private async Task TriggerNotificationWebhook(string payload) {
-      var qq = new StringContent(payload, Encoding.UTF8, "application/json");
-      var response = await Http.GetClient().PostAsync(_url, new StringContent(payload, Encoding.UTF8, "application/json"));
-      response.EnsureSuccessStatusCode();
+      this.Type = type;
+      this.Url = url;
     }
 
     public override bool Equals(object obj) {
       if (obj == null || this.GetType() != obj.GetType()) return false;
       
       var otherHook = (Hook)obj;
-      return this._type == otherHook._type && this._url == otherHook._url;
+      return this.Type == otherHook.Type && this.Url == otherHook.Url;
     }
 
     public override int GetHashCode() {
-      return $"{_type}-${_url}".GetHashCode();
+      return $"{Type}-${Url}".GetHashCode();
     }
   }
 }
