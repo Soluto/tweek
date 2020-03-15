26fda929e931ca7ba412e09310f66bf176155a70
yshay <yshayy@gmail.com>
2020-03-15 12:57:53 +0200

    fix build errors

--- addons/Context/Tweek.Drivers.Context.Couchbase/CouchBaseServiceAddon.cs
+++ addons/Context/Tweek.Drivers.Context.Couchbase/CouchBaseServiceAddon.cs
@@ -44,8 +44,8 @@ namespace Tweek.Drivers.Context.Couchbase
             var contextBucketPassword = couchbaseConfig["Password"];
             var serverUrl = couchbaseConfig.GetSection("Url").Get<string>();
             var serverUrls = couchbaseConfig.GetSection("Urls").Get<List<string>>();
-            if (serverUrls == null || serverUrls.Length == 0){
-                serverUrls = new List<string>(){ serverUrl }
+            if (serverUrls == null || serverUrls.Count == 0){
+                serverUrls = new List<string>(){ serverUrl };
             }
             var healthCheckMaxLatency = Optional(couchbaseConfig["HealthCheck:MaxLatencyMilliseconds"]).Map(x=> TimeSpan.FromMilliseconds(int.Parse(x))).IfNone(TimeSpan.FromSeconds(1));
             var healthCheckRetry = Optional(couchbaseConfig["HealthCheck:RetryCount"]).Map(x=> int.Parse(x)).IfNone(3);
