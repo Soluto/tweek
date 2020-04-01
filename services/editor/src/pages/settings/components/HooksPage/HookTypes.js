3f8b9a61bc4124e507a22d9f0c73d1a94b5dd219
Noam Okman <noamokman@gmail.com>
2020-04-01 14:49:40 +0300

    Add to ui

--- services/editor/src/pages/settings/components/HooksPage/HookTypes.js
+++ services/editor/src/pages/settings/components/HooksPage/HookTypes.js
@@ -1,4 +1,7 @@
-export const hookTypes = [{ label: 'Notification Webhook', value: 'notification_webhook' }];
+export const hookTypes = [
+  { label: 'Notification Webhook', value: 'notification_webhook' },
+  { label: 'Slack Webhook', value: 'slack_webhook' },
+];
 
 export const hookLabelsByType = hookTypes.reduce((labelsByType, typeObject) => {
   labelsByType[typeObject.value] = typeObject.label;
