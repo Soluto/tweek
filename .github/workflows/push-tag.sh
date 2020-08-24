292dfe3649bf814ba8909fc835f4f8545116c619
Yshay Yaacobi <yshayy@gmail.com>
2020-08-24 17:21:53 +0300

    final?

--- .github/workflows/push-tag.sh
+++ .github/workflows/push-tag.sh
@@ -1,7 +1,7 @@
 set -e
 git config --local user.email "$(git log --format='%ae' HEAD^!)"
 git config --local user.name "$(git log --format='%an' HEAD^!)"  
-docker tag soluto/tweek-$1 soluto/tweek-api:gh-test-$2
-docker push soluto/tweek-$1:gh-test-$2
-git tag -m "Version created: $1-$2" gh-tests-tweek-$1-$2
+docker tag soluto/tweek-$1 soluto/tweek-api:$2
+docker push soluto/tweek-$1:$2
+git tag -m "Version created: $1-$2" tweek-$1-$2
 git push --follow-tags
