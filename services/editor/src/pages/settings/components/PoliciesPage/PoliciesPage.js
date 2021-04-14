31789863278e8d2c94b3699c7c2722fd1ea650ff
Natalya Shrits <natalya.shrits@asurion.com>
2021-04-14 15:25:22 -0500

    add policies tab navigation

--- services/editor/src/pages/settings/components/PoliciesPage/PoliciesPage.js
+++ services/editor/src/pages/settings/components/PoliciesPage/PoliciesPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { useHistory } from 'react-router';
 import AclPolicies from './ACL';
 import JWTExtraction from './JWTExtraction';
 import './PoliciesPage.css';
@@ -9,20 +10,32 @@ const Title = styled.h3`
   text-transform: capitalize;
 `;
 
-export default () => (
-  <div className="policies-page">
-    <Title>Policies</Title>
-    <Tabs>
-      <TabList>
-        <Tab selected>ACL</Tab>
-        <Tab>JWT Extraction</Tab>
-      </TabList>
-      <TabPanel>
-        <AclPolicies />
-      </TabPanel>
-      <TabPanel>
-        <JWTExtraction />
-      </TabPanel>
-    </Tabs>
-  </div>
-);
+export default () => {
+  const history = useHistory();
+  const params = new URLSearchParams(history.location.search);
+  const selectedTab = Number(params.get('tab') || 0);
+
+  return (
+    <div className="policies-page">
+      <Title>Policies</Title>
+      <Tabs
+        selectedIndex={selectedTab}
+        onSelect={(index) => {
+          params.set('tab', index);
+          history.replace({ ...history.location, search: params.toString() });
+        }}
+      >
+        <TabList>
+          <Tab>ACL</Tab>
+          <Tab>JWT Extraction</Tab>
+        </TabList>
+        <TabPanel>
+          <AclPolicies />
+        </TabPanel>
+        <TabPanel>
+          <JWTExtraction />
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+};
