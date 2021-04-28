9116622af935e4bcb727436353f744f33b5adfbd
Natalya Shrits <natalya.shrits@asurion.com>
2021-04-28 11:24:48 -0500

    EditableText

--- services/editor/src/hoc/wrap-component-with-class.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const wrapComponentWithClass = (Comp) => ({ className, ...props }) => (
-  <div className={className}>
-    <Comp {...props} />
-  </div>
-);
-
-export default wrapComponentWithClass;
