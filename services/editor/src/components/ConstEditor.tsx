ed1e6f2cd655ce62766dc358af197b2b71dc8840
Natalya Shrits <natalya.shrits@asurion.com>
2021-04-30 20:23:48 -0500

    ConstEditor

---
+++ services/editor/src/components/ConstEditor.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ValueType } from '../contexts/TypesService';
+import TypedInput from './common/Input/TypedInput';
+
+export type ConstEditorProps = {
+  value: any;
+  valueType: string | ValueType;
+  onChange: (value: any) => void;
+};
+
+const ConstEditor = ({ value, valueType, onChange }: ConstEditorProps) => (
+  <div data-comp="const-editor" style={{ display: 'flex', width: '100%' }}>
+    <TypedInput value={value} valueType={valueType} onChange={onChange} />
+  </div>
+);
+
+export default ConstEditor;
