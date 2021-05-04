218c1117a8281d02649a66e1ebe0387b77ebfbf0
Natalya Shrits <natalya.shrits@asurion.com>
2021-05-04 11:13:06 -0500

    DefaultValue

--- services/editor/src/components/JPadFullEditor/JPadVisualEditor/Rule/DefaultValue.tsx
+++ services/editor/src/components/JPadFullEditor/JPadVisualEditor/Rule/DefaultValue.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { TypedInput } from '../../../common';
+import { TypedInputProps } from '../../../common/Input/TypedInput';
 import './DefaultValue.css';
 
-const DefaultValue = (props) => (
+const DefaultValue = (props: TypedInputProps) => (
   <div className="default-value-container">
     <label className="default-value-label">Default Value:</label>
     <TypedInput {...props} data-comp="default-value" />
