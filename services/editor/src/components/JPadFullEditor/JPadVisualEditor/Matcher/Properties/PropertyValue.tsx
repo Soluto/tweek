03c9cb72de4b6c3b4432288292131300756c0a47
Natalya Shrits <natalya.shrits@asurion.com>
2021-05-03 19:34:29 -0500

    PropertyValue

--- services/editor/src/components/JPadFullEditor/JPadVisualEditor/Matcher/Properties/PropertyValue.tsx
+++ services/editor/src/components/JPadFullEditor/JPadVisualEditor/Matcher/Properties/PropertyValue.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { ValueType } from 'tweek-client';
 import { inOp, within } from '../../../../../services/operators-provider';
 import { types } from '../../../../../services/types-service';
 import { TypedInput } from '../../../../common';
 import './styles.css';
 
-const chooseValueTypeByOperator = (operator, valueType) => {
+const chooseValueTypeByOperator = (operator: string, valueType: ValueType) => {
   if (operator === inOp.operatorValue) {
     return { ...types.array, ofType: valueType };
   }
@@ -15,7 +16,14 @@ const chooseValueTypeByOperator = (operator, valueType) => {
   return valueType;
 };
 
-const PropertyValue = ({ selectedOperator, valueType, ...props }) => (
+export type PropertyValueProps = {
+  selectedOperator: string;
+  value: any;
+  valueType: ValueType;
+  onChange: (value: any) => void;
+};
+
+const PropertyValue = ({ selectedOperator, valueType, ...props }: PropertyValueProps) => (
   <div className="property-value-wrapper">
     <TypedInput
       data-comp="property-value"
