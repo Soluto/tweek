a8d4fb66ebd1ab56d28d5e35c17af2a32b3db87c
Natalya Shrits <natalya.shrits@asurion.com>
2021-05-05 10:16:21 -0500

    MessageKeyPage

--- services/editor/src/pages/keys/components/KeyPage/MessageKeyPage/MessageKeyPage.tsx
+++ services/editor/src/pages/keys/components/KeyPage/MessageKeyPage/MessageKeyPage.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './MessageKeyPage.css';
 
-const MessageKeyPage = ({ message, ...props }) => (
+export type MessageKeyPageProps = {
+  message: string;
+  'data-comp': string;
+};
+
+const MessageKeyPage = ({ message, ...props }: MessageKeyPageProps) => (
   <div className="key-page-message" {...props}>
     {message}
   </div>
 );
+
 export default MessageKeyPage;
